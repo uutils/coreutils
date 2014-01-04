283448eb62d3ded4d2fc0e434bee44c1bf7469ad
Jordy Dickinson <jordy.dickinson@gmail.com>
2014-01-04 13:46:02 -0500

    Move option parsing into main

--- cp/cp.rs
+++ cp/cp.rs
@@ -14,18 +14,64 @@ extern mod extra;
 use std::os;
 use std::io::fs;
 
-use conf::Conf;
-
-mod conf;
 mod util;
 
-fn main() {
-    let conf = Conf::new(os::args());
+pub enum Mode {
+    Copy,
+    Help,
+    Version,
+}
 
-    match conf.mode {
-        conf::Copy    => copy(&conf),
-        conf::Help    => help(&conf),
-        conf::Version => version(),
+fn main() {
+    use extra::getopts::groups::{
+        getopts,
+        optflag,
+        usage,
+    };
+
+    let args = os::args();
+    let opts = ~[
+        optflag("h", "help", "display this help and exit"),
+        optflag("", "version", "output version information and exit"),
+    ];
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(e) => {
+            error!("error: {:s}", e.to_err_msg());
+            fail!()
+        },
+    };
+
+    let progname = args[0].clone();
+    let usage = usage("Copy SOURCE to DEST, or multiple SOURCE(s) to \
+                       DIRECTORY.", opts);
+    let mode = if matches.opt_present("version") {
+        Version
+    } else if matches.opt_present("help") {
+        Help
+    } else {
+        Copy
+    };
+    // For now we assume that the first free argument is SOURCE and the
+    // second free argument is DEST.
+    let sources = if matches.free.len() < 1 {
+        error!("error: Missing SOURCE argument. Try --help.");
+        fail!()
+    } else {
+        ~[~Path::new(matches.free[0].clone())]
+    };
+    let dest = if matches.free.len() < 2 {
+        error!("error: Missing DEST argument. Try --help.");
+        fail!()
+    } else {
+        ~Path::new(matches.free[1].clone())
+    };
+    // Any other free arguments are ignored for now.
+
+    match mode {
+        Copy    => copy(sources, dest),
+        Help    => help(progname, usage),
+        Version => version(),
     }
 }
 
@@ -33,18 +79,17 @@ fn version() {
     println("cp 1.0.0");
 }
 
-fn help(conf: &Conf) {
-    println!("Usage: {:s} SOURCE DEST", conf.progname);
+fn help(progname: &str, usage: &str) {
+    println!("Usage: {:s} SOURCE DEST", progname);
     println("");
-    println(conf.usage);
+    println(usage);
 }
 
-fn copy(conf: &Conf) {
+fn copy(sources: &[~Path], dest: &Path) {
     // We assume there is only one source for now.
-    let source = &conf.sources[0];
-    let dest = &conf.dest;
+    let source = sources[0].clone();
 
-    if util::paths_refer_to_same_file(*source, *dest) {
+    if util::paths_refer_to_same_file(source, dest) {
         error!("error: \"{:s}\" and \"{:s}\" are the same file",
                source.display().to_str(),
                dest.display().to_str());
@@ -53,5 +98,5 @@ fn copy(conf: &Conf) {
 
     // In the case of only one source and one destination, it's a simple file
     // copy operation.
-    fs::copy(*source, *dest);
+    fs::copy(source, dest);
 }
