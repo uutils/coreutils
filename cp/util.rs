694e4c529efec2227af9f52c4aaf854516c6aeb4
Jordy Dickinson <jordy.dickinson@gmail.com>
2014-01-04 14:36:48 -0500

    Add copyright header to util.rs

--- cp/util.rs
+++ cp/util.rs
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the uutils coreutils package.
+ *
+ * (c) Jordy Dickinson <jordy.dickinson@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 use std::io;
 use std::io::fs;
 use std::os;
