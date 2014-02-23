ca383e0b3931abc651eaf517556700fb367448c4
Jordy Dickinson <jordy.dickinson@gmail.com>
2014-02-23 15:29:51 -0500

    Update cp for changes in rust

--- cp/cp.rs
+++ cp/cp.rs
@@ -1,4 +1,5 @@
 #[crate_id(name="cp", vers="1.0.0", author="Jordy Dickinson")];
+#[feature(macro_rules)];
 
 /*
  * This file is part of the uutils coreutils package.
@@ -9,13 +10,17 @@
  * that was distributed with this source code.
  */
 
-extern mod extra;
+extern crate getopts;
 
 use std::os;
 use std::io;
 use std::io::fs;
 
-mod util;
+use getopts::{
+    getopts,
+    optflag,
+    usage,
+};
 
 pub enum Mode {
     Copy,
@@ -24,12 +29,6 @@ pub enum Mode {
 }
 
 fn main() {
-    use extra::getopts::groups::{
-        getopts,
-        optflag,
-        usage,
-    };
-
     let args = os::args();
     let opts = ~[
         optflag("h", "help", "display this help and exit"),
@@ -93,34 +92,83 @@ fn copy(sources: &[~Path], dest: &Path) {
 
     if sources.len() == 1 {
         let source = sources[0].clone();
+        let same_file = match paths_refer_to_same_file(source, dest) {
+            Ok(b)  => b,
+            Err(e) => if e.kind == io::FileNotFound {
+                false
+            } else {
+                error!("error: {:s}", e.to_str());
+                fail!()
+            }
+        };
 
-        if util::paths_refer_to_same_file(source, dest) {
+        if same_file {
             error!("error: \"{:s}\" and \"{:s}\" are the same file",
                 source.display().to_str(),
                 dest.display().to_str());
             fail!();
         }
 
-        fs::copy(source, dest);
+        let io_result = fs::copy(source, dest);
+
+        if io_result.is_err() {
+            let err = io_result.unwrap_err();
+            error!("error: {:s}", err.to_str());
+            fail!();
+        }
     } else {
-        if fs::stat(dest).kind != io::TypeDirectory {
+        if fs::stat(dest).unwrap().kind != io::TypeDirectory {
             error!("error: TARGET must be a directory");
             fail!();
         }
 
         for source in sources.iter() {
-            if fs::stat(*source).kind != io::TypeFile {
+            if fs::stat(*source).unwrap().kind != io::TypeFile {
                 error!("error: \"{:s}\" is not a file", source.display().to_str());
                 continue;
             }
 
             let mut full_dest = dest.clone();
-            
+
             full_dest.push(source.filename_str().unwrap());
 
             println!("{:s}", full_dest.display().to_str());
 
-            fs::copy(*source, &full_dest);
+            let io_result = fs::copy(*source, full_dest);
+
+            if io_result.is_err() {
+                let err = io_result.unwrap_err();
+                error!("error: {:s}", err.to_str());
+                fail!()
+            }
         }
     }
+}
+
+pub fn paths_refer_to_same_file(p1: &Path, p2: &Path) -> io::IoResult<bool> {
+    let mut raw_p1 = ~p1.clone();
+    let mut raw_p2 = ~p2.clone();
+
+    let p1_lstat = match fs::lstat(raw_p1) {
+        Ok(stat) => stat,
+        Err(e)   => return Err(e),
+    };
+
+    let p2_lstat = match fs::lstat(raw_p2) {
+        Ok(stat) => stat,
+        Err(e)   => return Err(e),
+    };
+
+    // We have to take symlinks and relative paths into account.
+    if p1_lstat.kind == io::TypeSymlink {
+        raw_p1 = ~fs::readlink(raw_p1).unwrap();
+    }
+    raw_p1 = ~os::make_absolute(raw_p1);
+
+    if p2_lstat.kind == io::TypeSymlink {
+        raw_p2 = ~fs::readlink(raw_p2).unwrap();
+    }
+    raw_p2 = ~os::make_absolute(raw_p2);
+
+    Ok(raw_p1 == raw_p2)
 }
