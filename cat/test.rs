25926bab677f9901b8f8f709b36b4076d6d5d3bf
Michael Gehring <mg@ebfe.org>
2014-05-17 15:47:44 +0200

    Update tests to use std::process:Command

--- cat/test.rs
+++ cat/test.rs
@@ -1,32 +1,41 @@
-use std::io::process::Process;
+use std::io::process::Command;
 use std::str;
 
 #[test]
 fn test_output_multi_files_print_all_chars() {
-    let prog = Process::output("build/cat",
-                               ["cat/fixtures/alpha.txt".to_owned(), "cat/fixtures/256.txt".to_owned(),
-                                "-A".to_owned(), "-n".to_owned()]).unwrap();
-    let out = str::from_utf8_owned(prog.output.as_slice().to_owned()).unwrap();
+    let po = match Command::new("build/cat")
+                                .arg("cat/fixtures/alpha.txt")
+                                .arg("cat/fixtures/256.txt")
+                                .arg("-A")
+                                .arg("-n").output() {
+
+        Ok(p) => p,
+        Err(err) => fail!("{}", err),
+    };
+
+    let out = str::from_utf8(po.output.as_slice()).unwrap();
     assert_eq!(out,
-               "     1\tabcde$\n     2\tfghij$\n     3\tklmno$\n     4\tpqrst$\n     5\tuvwxyz$\n     6\t^@^A^B^C^D^E^F^G^H^I$\n     7\t^K^L^M^N^O^P^Q^R^S^T^U^V^W^X^Y^Z^[^\\^]^^^_ !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~^?M-^@M-^AM-^BM-^CM-^DM-^EM-^FM-^GM-^HM-^IM-^JM-^KM-^LM-^MM-^NM-^OM-^PM-^QM-^RM-^SM-^TM-^UM-^VM-^WM-^XM-^YM-^ZM-^[M-^\\M-^]M-^^M-^_M- M-!M-\"M-#M-$M-%M-&M-\'M-(M-)M-*M-+M-,M--M-.M-/M-0M-1M-2M-3M-4M-5M-6M-7M-8M-9M-:M-;M-<M-=M->M-?M-@M-AM-BM-CM-DM-EM-FM-GM-HM-IM-JM-KM-LM-MM-NM-OM-PM-QM-RM-SM-TM-UM-VM-WM-XM-YM-ZM-[M-\\M-]M-^M-_M-`M-aM-bM-cM-dM-eM-fM-gM-hM-iM-jM-kM-lM-mM-nM-oM-pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?".to_owned());
+               "     1\tabcde$\n     2\tfghij$\n     3\tklmno$\n     4\tpqrst$\n     5\tuvwxyz$\n     6\t^@^A^B^C^D^E^F^G^H^I$\n     7\t^K^L^M^N^O^P^Q^R^S^T^U^V^W^X^Y^Z^[^\\^]^^^_ !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~^?M-^@M-^AM-^BM-^CM-^DM-^EM-^FM-^GM-^HM-^IM-^JM-^KM-^LM-^MM-^NM-^OM-^PM-^QM-^RM-^SM-^TM-^UM-^VM-^WM-^XM-^YM-^ZM-^[M-^\\M-^]M-^^M-^_M- M-!M-\"M-#M-$M-%M-&M-\'M-(M-)M-*M-+M-,M--M-.M-/M-0M-1M-2M-3M-4M-5M-6M-7M-8M-9M-:M-;M-<M-=M->M-?M-@M-AM-BM-CM-DM-EM-FM-GM-HM-IM-JM-KM-LM-MM-NM-OM-PM-QM-RM-SM-TM-UM-VM-WM-XM-YM-ZM-[M-\\M-]M-^M-_M-`M-aM-bM-cM-dM-eM-fM-gM-hM-iM-jM-kM-lM-mM-nM-oM-pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?");
 }
 
 #[test]
 fn test_stdin_squeeze() {
-    let mut prog = Process::new("build/cat", ["-A".to_owned()]).unwrap();
+    let mut process= Command::new("build/cat").arg("-A").spawn().unwrap();
 
-    prog.stdin.take_unwrap().write(bytes!("\x00\x01\x02"));
+    process.stdin.take_unwrap().write(bytes!("\x00\x01\x02")).unwrap();
+    let po = process.wait_with_output().unwrap();
+    let out = str::from_utf8(po.output.as_slice()).unwrap();
 
-    let out = str::from_utf8_owned(prog.wait_with_output().output.as_slice().to_owned()).unwrap();
-    assert_eq!(out, "^@^A^B".to_owned());
+    assert_eq!(out, "^@^A^B");
 }
 
 #[test]
 fn test_stdin_number_non_blank() {
-    let mut prog = Process::new("build/cat", ["-b".to_owned(), "-".to_owned()]).unwrap();
+    let mut process = Command::new("build/cat").arg("-b").arg("-").spawn().unwrap();
 
-    prog.stdin.take_unwrap().write(bytes!("\na\nb\n\n\nc"));
+    process.stdin.take_unwrap().write(bytes!("\na\nb\n\n\nc")).unwrap();
+    let po = process.wait_with_output().unwrap();
+    let out =  str::from_utf8(po.output.as_slice()).unwrap();
 
-    let out = str::from_utf8_owned(prog.wait_with_output().output.as_slice().to_owned()).unwrap();
-    assert_eq!(out, "\n     1\ta\n     2\tb\n\n\n     3\tc".to_owned());
+    assert_eq!(out, "\n     1\ta\n     2\tb\n\n\n     3\tc");
 }
--- truncate/test.rs
+++ truncate/test.rs
@@ -1,5 +1,5 @@
 use std::io;
-use std::io::process::Process;
+use std::io::process::Command;
 
 static PROG: &'static str = "build/truncate";
 static TFILE1: &'static str = "truncate_test_1";
@@ -15,27 +15,27 @@ fn make_file(name: &str) -> io::File {
 #[test]
 fn test_increase_file_size() {
     let mut file = make_file(TFILE1);
-    if !Process::status(PROG, ["-s".to_owned(), "+5K".to_owned(), TFILE1.to_owned()]).unwrap().success() {
+    if !Command::new(PROG).args(["-s", "+5K", TFILE1]).status().unwrap().success() {
         fail!();
     }
-    file.seek(0, io::SeekEnd);
+    file.seek(0, io::SeekEnd).unwrap();
     if file.tell().unwrap() != 5 * 1024 {
         fail!();
     }
-    io::fs::unlink(&Path::new(TFILE1));
+    io::fs::unlink(&Path::new(TFILE1)).unwrap();
 }
 
 #[test]
 fn test_decrease_file_size() {
     let mut file = make_file(TFILE2);
-    file.write(bytes!("1234567890"));
-    if !Process::status(PROG, ["--size=-4".to_owned(), TFILE2.to_owned()]).unwrap().success() {
+    file.write(bytes!("1234567890")).unwrap();
+    if !Command::new(PROG).args(["--size=-4", TFILE2]).status().unwrap().success() {
         fail!();
     }
-    file.seek(0, io::SeekEnd);
+    file.seek(0, io::SeekEnd).unwrap();
     if file.tell().unwrap() != 6 {
         println!("{}", file.tell());
         fail!();
     }
-    io::fs::unlink(&Path::new(TFILE2));
+    io::fs::unlink(&Path::new(TFILE2)).unwrap();
 }
