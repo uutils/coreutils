3bdef139c64b7df34974270e2434acf9b688a917
Ben Eggers <beggerss@cs.washington.edu>
2014-06-19 22:58:50 -0700

    gave up, but i have some stuff written

--- tsort/tsort.rs
+++ tsort/tsort.rs
@@ -55,44 +55,9 @@ fn uumain(args: Vec<String>) -> int {
         Err(_) => { return 1; }
     };
 
-	// let mut nodes = build_graph(reader);
-	// let sorted = tsort(&nodes);
-
-	// print_nodes(sorted, /* stdout */);
-
-	// if nodes.length() > 0 {
-	// 	// Print error message to stderr
-	// 	print_nodes(nodes, /* stderr */);
-	// 	return 1;  // error because there were extras
-	// }
 
 	return 0
 }
-
-// // Build the graph from the passed file. Nodes are defined as strings in the file
-// // separated by whitespace (space, tab, or newline). Nodes are read as pairs. For
-// // each pair, there will be an edge pointing from the first node to the second node.
-// // If there are an odd number of nodes, the last node means nothing (but it will
-// // be in the graph, so if it only appears at the end of the file it will still
-// // appear in the output).
-// fn build_graph(...) -> Box<Vec<node>> {
-
-// }
-
-// // Topological sort the passed nodes. This is done by removing nodes, one by one,
-// // from the vector and placed into the return vector. Only nodes with in-degree
-// // 0 will be removed. If no nodes have in-degree 0, none will be removed.
-// // Thus, if this function returns and the passed vector still has nodes in it
-// // (i.e. if the returned vector is shorter than the initial vector was), there
-// // is a cycle.
-// fn tsort(nodes: &mut Box<Vec<node>>) -> Box<Vec<node>> {
-
-// }
-
-// // Prints all the nodes in the passed vector to the passed stream.
-// fn print_nodes(nodes: Box<Vec<Node>>, /* somehow a stream */) {
-
-// }
 
 // Print out the program usage
 fn usage(prog_name: String, opts: [getopts::OptGroup, ..2]) {
@@ -106,7 +71,7 @@ fn usage(prog_name: String, opts: [getopts::OptGroup, ..2]) {
 }
 
 fn open(path: String) -> StdResult<BufferedReader<Box<Reader>>, int> {
-    if "-" == path.as_slice() {
+    if  path.as_slice == "-" {
         let reader = box stdin() as Box<Reader>;
         return Ok(BufferedReader::new(reader));
     }
@@ -121,8 +86,3 @@ fn open(path: String) -> StdResult<BufferedReader<Box<Reader>>, int> {
         }
     }
 }
-
-// // How we represent nodes
-// struct node {
-
-// }
