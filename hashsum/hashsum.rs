c4ad21d881f94ed98d26b43dfb9cc351cc14f658
Gil Cottle <redcape@users.noreply.github.com>
2014-06-26 02:21:11 -0400

    Last '\r' must be written

--- hashsum/hashsum.rs
+++ hashsum/hashsum.rs
@@ -304,6 +304,7 @@ fn digest_reader(digest: &mut Box<Digest>, reader: &mut Reader, binary: bool) ->
     if cfg!(windows) && looking_for_newline {
         vec.push('\r' as u8);
     }
+    digest.input(vec.as_slice());
 
     Ok(digest.result_str())
 }
