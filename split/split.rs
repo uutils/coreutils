71f80aa3ad39f7d71dd9d43f9cdcc409033628df
Akira Hayakawa <ruby.wktk@gmail.com>
2014-07-01 23:25:23 +0900

    split: fix crate_id
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- split/split.rs
+++ split/split.rs
@@ -1,4 +1,4 @@
-#![crate_id(name="mkdir", vers="1.0.0", author="Akira Hayakawa")]
+#![crate_id(name="split", vers="1.0.0", author="Akira Hayakawa")]
 
 /*
  * This file is part of the uutils coreutils package.
