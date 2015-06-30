0ed57b3896c21f0fe3a8b43ac385db2626ddcd86
Remi Rampin <remirampin@gmail.com>
2015-06-30 15:21:40 -0400

    Fix simple typo in mv.rs

--- src/mv/mv.rs
+++ src/mv/mv.rs
@@ -165,12 +165,11 @@ pub fn uumain(args: Vec<String>) -> i32 {
 }
 
 fn help(usage: &str) {
-    let msg = format!("a0{} {1}\n\n \
+    println!("a0{} {1}\n\n \
                        Usage: {0} SOURCE DEST\n  \
                          or:  {0} SOURCE... DIRECTORY \
                        \n\
                        {2}", NAME, VERSION, usage);
-    println!("{}", msg);
 }
 
 fn exec(files: &[PathBuf], b: Behaviour) -> i32 {
