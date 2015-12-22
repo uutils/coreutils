1fc698a17f50ed22ed91f3a31ee93614c8d16c2f
Michael Gehring <mg@ebfe.org>
2015-12-22 12:55:51 +0100

    tests/stdbuf: disable on non-linux os

--- tests/stdbuf.rs
+++ tests/stdbuf.rs
@@ -7,10 +7,12 @@ static UTIL_NAME: &'static str = "stdbuf";
 
 #[test]
 fn test_stdbuf_unbuffered_stdout() {
-    let (_, mut ucmd) = testing(UTIL_NAME);
-    // This is a basic smoke test
-    let result = ucmd.args(&["-o0", "head"])
-                     .run_piped_stdin("The quick brown fox jumps over the lazy dog.");
-    assert_eq!(result.stdout,
-               "The quick brown fox jumps over the lazy dog.");
+    if cfg!(target_os="linux") {
+        let (_, mut ucmd) = testing(UTIL_NAME);
+        // This is a basic smoke test
+        let result = ucmd.args(&["-o0", "head"])
+                         .run_piped_stdin("The quick brown fox jumps over the lazy dog.");
+        assert_eq!(result.stdout,
+                   "The quick brown fox jumps over the lazy dog.");
+    }
 }
