c04371897bfbf0f25393543473dd2e3d082646cb
Nathan Ross <nross.se@gmail.com>
2016-02-19 20:09:46 -0500

    dirname: in tests, change to test for different OsStr behavior on different platforms

--- tests/dirname.rs
+++ tests/dirname.rs
@@ -5,32 +5,41 @@ use common::util::*;
 
 static UTIL_NAME: &'static str = "dirname";
 
+#[cfg(windows)]
+static FS_ROOT_EXAMPLE: &'static str = "a:\\";
+#[cfg(not(windows))]
+static FS_ROOT_EXAMPLE: &'static str = "/";
+
+#[cfg(windows)]
+static DIRNAME_SEPARATOR: &'static str = "\\";
+#[cfg(not(windows))]
+static DIRNAME_SEPARATOR: &'static str = "/";
 
 #[test]
 fn test_path_with_trailing_slashes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/root/alpha/beta/gamma/delta/epsilon/omega//";
+    let dir = format!("{0}root{1}alpha{1}beta{1}gamma{1}delta{1}epsilon{1}omega{1}{1}", FS_ROOT_EXAMPLE, DIRNAME_SEPARATOR);
     let out = ucmd.arg(dir).run().stdout;
 
-    assert_eq!(out.trim_right(), "/root/alpha/beta/gamma/delta/epsilon");
+    assert_eq!(out.trim_right(), format!("{0}root{1}alpha{1}beta{1}gamma{1}delta{1}epsilon", FS_ROOT_EXAMPLE, DIRNAME_SEPARATOR));
 }
 
 #[test]
 fn test_path_without_trailing_slashes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/root/alpha/beta/gamma/delta/epsilon/omega";
+    let dir = format!("{0}root{1}alpha{1}beta{1}gamma{1}delta{1}epsilon{1}omega", FS_ROOT_EXAMPLE, DIRNAME_SEPARATOR);
     let out = ucmd.arg(dir).run().stdout;
 
-    assert_eq!(out.trim_right(), "/root/alpha/beta/gamma/delta/epsilon");
+    assert_eq!(out.trim_right(), format!("{0}root{1}alpha{1}beta{1}gamma{1}delta{1}epsilon", FS_ROOT_EXAMPLE, DIRNAME_SEPARATOR));
 }
 
 #[test]
 fn test_root() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/";
+    let dir = FS_ROOT_EXAMPLE;
     let out = ucmd.arg(dir).run().stdout;
 
-    assert_eq!(out.trim_right(), "/");
+    assert_eq!(out.trim_right(), FS_ROOT_EXAMPLE);
 }
 
 #[test]
