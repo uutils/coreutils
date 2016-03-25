491320747bea420083cd4475cdd3ef836af77663
Joseph Crail <jbcrail@gmail.com>
2016-03-25 00:50:51 -0400

    sort: remove explicit enum values

--- src/sort/sort.rs
+++ src/sort/sort.rs
@@ -217,19 +217,19 @@ fn human_numeric_size_compare(a: &String, b: &String) -> Ordering {
 
 #[derive(Eq, Ord, PartialEq, PartialOrd)]
 enum Month {
-    Unknown     = 0,
-    January     = 1,
-    February    = 2,
-    March       = 3,
-    April       = 4,
-    May         = 5,
-    June        = 6,
-    July        = 7,
-    August      = 8,
-    September   = 9,
-    October     = 10,
-    November    = 11,
-    December    = 12,
+    Unknown,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
 }
 
 /// Parse the beginning string into a Month, returning Month::Unknown on errors.
