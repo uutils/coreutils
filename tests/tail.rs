9a5209a7a4728aa95fcd5d4e4b1e4f624efbca22
Nick Fitzgerald <fitzgen@gmail.com>
2016-03-27 14:41:56 -0700

    tests/tail: Rename `INPUT` to `FOOBAR_TXT` as there are more than one inputs

--- tests/tail.rs
+++ tests/tail.rs
@@ -7,19 +7,19 @@ use common::util::*;
 
 static UTIL_NAME: &'static str = "tail";
 
-static INPUT: &'static str = "foobar.txt";
+static FOOBAR_TXT: &'static str = "foobar.txt";
 
 #[test]
 fn test_stdin_default() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.run_piped_stdin(at.read(INPUT));
+    let result = ucmd.run_piped_stdin(at.read(FOOBAR_TXT));
     assert_eq!(result.stdout, at.read("foobar_stdin_default.expected"));
 }
 
 #[test]
 fn test_single_default() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.arg(INPUT).run();
+    let result = ucmd.arg(FOOBAR_TXT).run();
     assert_eq!(result.stdout, at.read("foobar_single_default.expected"));
 }
 
@@ -51,14 +51,14 @@ fn test_single_big_args() {
 #[test]
 fn test_bytes_single() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.arg("-c").arg("10").arg(INPUT).run();
+    let result = ucmd.arg("-c").arg("10").arg(FOOBAR_TXT).run();
     assert_eq!(result.stdout, at.read("foobar_bytes_single.expected"));
 }
 
 #[test]
 fn test_bytes_stdin() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.arg("-c").arg("13").run_piped_stdin(at.read(INPUT));
+    let result = ucmd.arg("-c").arg("13").run_piped_stdin(at.read(FOOBAR_TXT));
     assert_eq!(result.stdout, at.read("foobar_bytes_stdin.expected"));
 }
 
