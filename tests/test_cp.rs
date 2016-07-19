9458ffcf93f66dbf46ed4876cec1197db9a5c77a
Jeremy Neptune <jerenept@gmail.com>
2016-07-18 23:16:08 -0400

    cp: added test for the -n/--no-clobber option, and fixtures that allow testing this option

--- tests/test_cp.rs
+++ tests/test_cp.rs
@@ -6,6 +6,13 @@ static TEST_HELLO_WORLD_DEST: &'static str = "copy_of_hello_world.txt";
 static TEST_COPY_TO_FOLDER: &'static str = "hello_dir/";
 static TEST_COPY_TO_FOLDER_FILE: &'static str = "hello_dir/hello_world.txt";
 static TEST_COPY_FROM_FOLDER_FILE: &'static str = "hello_dir_with_file/hello_world.txt";
+static TEST_NOT_HELLO_WORLD: &'static str = "not_hello_world.txt";
+static HELLO_WORLD_TEXT: &'static str = "Hello, World!\n";
+static NOT_HELLO_WORLD_TEXT: &'static str = "This is not a Hello World!\n";
+static HELLO_WORLD_IN_DIRECTORY: &'static str = "hello_dir_with_file/not_hello_world.txt";
+//this file is misleadingly named - since it tests a particular situation (copying with noclobber
+//into a directory with a file that has the same name as the source file)
+static HELLO_FOLDER_WITH_FILES: &'static str = "hello_dir_with_file";
 
 #[test]
 fn test_cp_cp() {
@@ -20,7 +27,7 @@ fn test_cp_cp() {
     assert!(exit_success);
 
     // Check the content of the destination file that was copied.
-    assert_eq!(at.read(TEST_HELLO_WORLD_DEST), "Hello, World!\n");
+    assert_eq!(at.read(TEST_HELLO_WORLD_DEST), HELLO_WORLD_TEXT);
 }
 
 #[test]
@@ -35,7 +42,7 @@ fn test_cp_with_dirs_t() {
         .arg(TEST_HELLO_WORLD_SOURCE)
         .run();
     assert!(result_to_dir_t.success);
-    assert_eq!(at.read(TEST_COPY_TO_FOLDER_FILE), "Hello, World!\n");
+    assert_eq!(at.read(TEST_COPY_TO_FOLDER_FILE), HELLO_WORLD_TEXT);
 }
 
 #[test]
@@ -49,12 +56,47 @@ fn test_cp_with_dirs() {
         .arg(TEST_COPY_TO_FOLDER)
         .run();
     assert!(result_to_dir.success);
-    assert_eq!(at.read(TEST_COPY_TO_FOLDER_FILE), "Hello, World!\n");
+    assert_eq!(at.read(TEST_COPY_TO_FOLDER_FILE), HELLO_WORLD_TEXT);
 
     let result_from_dir = ts.util_cmd()
         .arg(TEST_COPY_FROM_FOLDER_FILE)
         .arg(TEST_HELLO_WORLD_DEST)
         .run();
     assert!(result_from_dir.success);
-    assert_eq!(at.read(TEST_HELLO_WORLD_DEST), "Hello, World!\n");
+    assert_eq!(at.read(TEST_HELLO_WORLD_DEST), HELLO_WORLD_TEXT);
+}
+
+#[test]
+fn test_cp_no_clobber() {
+    let ts = TestSet::new(UTIL_NAME);
+    let at = &ts.fixtures;
+
+    //using -n option
+    let result = ts.util_cmd()
+        .arg("-n")
+        .arg(TEST_HELLO_WORLD_SOURCE)
+        .arg(TEST_NOT_HELLO_WORLD)
+        .run();
+    assert!(result.success);
+    assert_eq!(at.read(TEST_NOT_HELLO_WORLD), NOT_HELLO_WORLD_TEXT);
+    assert_eq!(at.read(TEST_HELLO_WORLD_SOURCE), HELLO_WORLD_TEXT);
+
+    //this time, copying to a directory
+    let result_dir = ts.util_cmd()
+        .arg("-n")
+        .arg(TEST_NOT_HELLO_WORLD)
+        .arg(TEST_COPY_FROM_FOLDER_FILE)
+        .run();
+    assert!(result_dir.success);
+    assert_eq!(at.read(TEST_COPY_FROM_FOLDER_FILE), HELLO_WORLD_TEXT);
+    assert_eq!(at.read(TEST_NOT_HELLO_WORLD), NOT_HELLO_WORLD_TEXT);
+
+    let result_implicit_dir = ts.util_cmd()
+        .arg("-n")
+        .arg(TEST_NOT_HELLO_WORLD)
+        .arg(HELLO_FOLDER_WITH_FILES)
+        .run();
+    assert!(result_implicit_dir.success);
+    assert_eq!(at.read(HELLO_WORLD_IN_DIRECTORY), HELLO_WORLD_TEXT);
+    assert_eq!(at.read(TEST_NOT_HELLO_WORLD), NOT_HELLO_WORLD_TEXT);
 }
