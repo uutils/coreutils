270290efe6497b0f0be8e7b3b09dd600a6154a93
Knight <anonymousknight96@gmail.com>
2016-08-14 13:53:42 +0800

    whoami: use uucore::entries

--- src/whoami/platform/unix.rs
+++ src/whoami/platform/unix.rs
@@ -2,31 +2,18 @@
  * This file is part of the uutils coreutils package.
  *
  * (c) Jordi Boggiano <j.boggiano@seld.be>
+ * (c) Jian Zeng <anonymousknight96 AT gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-use std::io::{Result, Error};
-use ::libc;
-use uucore::c_types::{c_passwd, getpwuid};
-
-extern {
-    pub fn geteuid() -> libc::uid_t;
-}
+use std::io::Result;
+use uucore::libc::geteuid;
+use uucore::entries::uid2usr;
 
 pub unsafe fn getusername() -> Result<String> {
     // Get effective user id
     let uid = geteuid();
-
-    // Try to find username for uid
-    let passwd: *const c_passwd = getpwuid(uid);
-    if passwd.is_null() {
-        return Err(Error::last_os_error())
-    }
-
-    // Extract username from passwd struct
-    let pw_name: *const libc::c_char = (*passwd).pw_name;
-    let username = String::from_utf8_lossy(::std::ffi::CStr::from_ptr(pw_name).to_bytes()).to_string();
-    Ok(username)
+    uid2usr(uid)
 }
