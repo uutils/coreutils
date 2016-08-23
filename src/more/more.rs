a90aec3f1bdd9380eb71342ab2104947bc641b05
Martin Kysel <mkysel@nuodb.com>
2016-08-23 10:50:45 +0100

    add author name

--- src/more/more.rs
+++ src/more/more.rs
@@ -3,7 +3,7 @@
 /*
  * This file is part of the uutils coreutils package.
  *
- * (c) Jordy Dickinson <jordy.dickinson@gmail.com>
+ * (c) Martin Kysel <code@martinkysel.com>
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
