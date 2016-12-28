32f509a7895234af4391d1f7a1b0c5e8d885ce65
Jeremy Neptune <jerenept@gmail.com>
2016-12-28 02:08:43 +0100

    cp: added options: no-clobber, recursive, link, interactive Additionally: changed all show_error!/panic! combos to crash!

--- src/cp/cp.rs
+++ src/cp/cp.rs
@@ -4,21 +4,26 @@
  * This file is part of the uutils coreutils package.
  *
  * (c) Jordy Dickinson <jordy.dickinson@gmail.com>
+ * (c) Jeremy Neptune <jerenept@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
+ *
+ * Some parts of this program are modeled after Orvar Segerström and Sokovikov Evgeniy's work on
+ * mv.rs .
  */
 
 extern crate getopts;
-
+extern crate walkdir;
 #[macro_use]
 extern crate uucore;
 
 use getopts::Options;
 use std::fs;
-use std::io::{ErrorKind, Result, Write};
+use std::io::{BufReader, BufRead, stdin, ErrorKind, Result, Write};
 use std::path::Path;
 use uucore::fs::{canonicalize, CanonicalizeMode};
+use walkdir::WalkDir;
 
 #[derive(Clone, Eq, PartialEq)]
 pub enum Mode {
@@ -27,6 +32,32 @@ pub enum Mode {
     Version,
 }
 
+#[derive (Clone, Eq, PartialEq)]
+pub enum OverwriteMode {
+    NoClobber,
+    Interactive,
+    Force,
+}
+
+#[derive(Clone, Eq, PartialEq)]
+pub enum BackupMode {
+    NoBackup,
+    SimpleBackup,
+    NumberedBackup,
+    ExistingBackup,
+}
+
+pub struct Behaviour {
+    overwrite: OverwriteMode,
+    backup: BackupMode,
+    suffix: String,
+    update: bool,
+    target_dir: Option<String>,
+    no_target_dir: bool,
+    verbose: bool,
+    link : bool,
+    recursive : bool,
+}
 static NAME: &'static str = "cp";
 static VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -34,16 +65,19 @@ pub fn uumain(args: Vec<String>) -> i32 {
     let mut opts = Options::new();
 
     opts.optflag("h", "help", "display this help and exit");
-    opts.optflag("", "version", "output version information and exit");
-    opts.optopt("t", "target-directory", "copy all SOURCE arguments into DIRECTORY", "DEST");
+    opts.optflag("V", "version", "output version information and exit");
+    opts.optopt("t", "target-directory", "copy all SOURCE arguments into DIRECTORY", "DIRECTORY");
     opts.optflag("T", "no-target-directory", "Treat DEST as a regular file and not a directory");
     opts.optflag("v", "verbose", "explicitly state what is being done");
+    opts.optflag("n", "no-clobber", "don't overwrite a file that already exists");
+    opts.optflag("r", "recursive", "copy directories recursively");
+    opts.optflag("l", "link", "hard-link files instead of copying");
+    opts.optflag("i", "interactive", "ask before overwriting files");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(e) => {
-            show_error!("{}", e);
-            panic!()
+            crash!(1, "{}", e)
         },
     };
     let usage = opts.usage("Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.");
@@ -79,96 +113,178 @@ fn help(usage: &str) {
 }
 
 fn copy(matches: getopts::Matches) {
-    let verbose = matches.opt_present("verbose");
+    let behavior = Behaviour {
+        overwrite : if matches.opt_present("no-clobber") {
+            OverwriteMode::NoClobber
+        } else if matches.opt_present("interactive") {
+            OverwriteMode::Interactive
+        } else {
+            OverwriteMode::Force
+        },
+        recursive : matches.opt_present("recursive"),
+        backup : BackupMode::NoBackup, //TODO: actually do backup
+        suffix : String::from("~"), //TODO: implement backup with suffix
+        update : false, //TODO: implement updating
+        //target_dir = if matches.opt_present("no-target-dir"){
+        //    None todo: implement this shit
+        //}
+        verbose : matches.opt_present("verbose"),
+        no_target_dir : matches.opt_present("no-target-directory"),
+        link : matches.opt_present("link"),
+        target_dir : matches.opt_str("target-directory"),
+
+    };
     let sources: Vec<String> = if matches.free.is_empty() {
-        show_error!("Missing SOURCE or DEST argument. Try --help.");
-        panic!()
-    } else if !matches.opt_present("target-directory") {
+        crash!(1, "Missing SOURCE or DEST argument. Try --help for usage")
+    } else if !behavior.target_dir.is_some() {
         matches.free[..matches.free.len() - 1].iter().cloned().collect()
     } else {
         matches.free.iter().cloned().collect()
     };
-    let dest_str = if matches.opt_present("target-directory") {
+    let dest_str = if behavior.target_dir.is_some() {
         matches.opt_str("target-directory").expect("Option -t/--target-directory requires an argument")
     } else {
         matches.free[matches.free.len() - 1].clone()
     };
-    let dest = if matches.free.len() < 2 && !matches.opt_present("target-directory") {
-        show_error!("Missing DEST argument. Try --help.");
-        panic!()
+    if behavior.no_target_dir && behavior.target_dir.is_some() {
+        crash!(1, "Options --no-target-dir and --target-dir are mutually exclusive");
+    }
+    let dest = if matches.free.len() < 2 && !behavior.target_dir.is_some() {
+        crash!(1, "Missing DEST argument. Try --help for usage")
     } else {
         //the argument to the -t/--target-directory= options
         let path = Path::new(&dest_str);
-        if !path.is_dir() && matches.opt_present("target-directory") {
-            show_error!("Target {} is not a directory", matches.opt_str("target-directory").unwrap());
-            panic!()
+        if !path.is_dir() && behavior.target_dir.is_some() {
+            crash!(1, "Target {} is not a directory", behavior.target_dir.unwrap())
         } else {
             path
         }
 
     };
 
-    assert!(sources.len() >= 1);
-    if matches.opt_present("no-target-directory") && dest.is_dir() {
-        show_error!("Can't overwrite directory {} with non-directory", dest.display());
-        panic!()
+    if sources.len() < 1 {
+        crash!(1, "No source files specified. Try --help for usage")
+    }
+    //assert!(sources.len() >= 1);
+    if behavior.no_target_dir && dest.is_dir() {
+        crash!(1, "Can't overwrite directory {} with non-directory", dest.display())
     }
 
-    if sources.len() == 1 {
-        let source = Path::new(&sources[0]);
-        let same_file = paths_refer_to_same_file(source, dest).unwrap_or_else(|err| {
-            match err.kind() {
-                ErrorKind::NotFound => false,
-                _ => {
-                    show_error!("{}", err);
-                    panic!()
+
+    if !dest.is_dir() && sources.len() != 1  {
+        crash!(1, "Multiple SOURCE files can only be copied to a directory")
+    }
+    if !dest.exists() && (sources.len() != 1 && Path::new(&sources[0]).is_dir()) {
+        let io_result = fs::create_dir(dest.clone()).err();
+        match io_result {
+            None => {},
+            Some(t) => {
+                crash!(1, "{}", t);
+            }
+        }
+    }
+    let folder_copy = !dest.exists();
+    'outer: for src in &sources {
+        for item in WalkDir::new(src){
+            let item1 = item.unwrap();
+            let item=item1.path();
+            if !(item.is_dir() || item.is_file()) {
+                crash!(1, "{} is invalid or inaccessible", item.display())
+            }
+            let full_dest = if Path::new(src).is_dir() && !folder_copy {
+                dest.join(item.strip_prefix(Path::new(src).parent().unwrap()).unwrap()) //Christmas day!
+            /*
+                If the source of the files (as given by the user args) is a directory
+                the ending destination of a particular file is:
+                the
+            */
+            } else if Path::new(src).is_dir() && folder_copy {
+                dest.join(item.canonicalize().unwrap().strip_prefix(&Path::new(src).canonicalize().unwrap()).unwrap())
+            } else if !dest.is_dir() { //source is not a directory, for the rest
+                dest.to_path_buf() //figure out how to copy a directory; not copy a directory into another
+            } else  {
+                dest.join(item)
+            };
+            //println!("{:?}", None);
+            if item.is_dir() {
+                if !behavior.recursive {
+                    println!("{}: skipping directory '{}' (use -r/--recursive to include directories)", NAME, item.display());
+                    continue 'outer;
                 }
-            }
-        });
+                if behavior.verbose {
+                    println!("{} -> {}", item.display(), full_dest.display());
+                }
+                if full_dest.is_dir() {
+                    continue; //merge the directories that already exist
+                }
+                match fs::create_dir_all(full_dest.clone()) {
+                    Err(e) => {
+                        crash!(1, "{}", e)
+                    },
+                    Ok(t) => {
+                        let permissions = fs::metadata(item).unwrap().permissions();
+                         match fs::set_permissions(full_dest, permissions) {
+                             Ok(t) => t,
+                             Err(t) => show_error!("{}", t)
+                         }
+                        t
+                    },
+                }
+            } else {
+                let same_file = paths_refer_to_same_file(item, full_dest.as_path()).unwrap_or_else(|err| {
+                    match err.kind() {
+                        ErrorKind::NotFound => false,
+                        _ => {
+                            crash!(1, "{}", err)
+                        }
+                    }
+                });
+                if !item.is_file() {
+                    show_error!("\"{}\" is not a file", item.display());
+                    continue;
+                }
+                if same_file {
+                    crash!(1, "\"{}\" and \"{}\" are the same file",
+                        item.display(),
+                        full_dest.display())
+                }
+                if full_dest.exists() {
+                    match behavior.overwrite {
+                        OverwriteMode::NoClobber => {
+                            show_error!("Not overwriting {} because of option 'no-clobber'", full_dest.display());
+                            continue; //if the destination file exists, we promised not to overwrite
+                        },
+                        OverwriteMode::Interactive => {
+                            if !read_yes() {
+                                continue;
+                            }
+                        },
+                        OverwriteMode::Force => {
+                            let io_result = fs::remove_file(full_dest.clone()).err();
+                            match io_result {
+                                None => {},
+                                Some(t) => {
+                                    crash!(1, "{}", t)
+                                }
+                            }
+                        },
 
-        if same_file {
-            show_error!("\"{}\" and \"{}\" are the same file",
-                source.display(),
-                dest.display());
-            panic!();
-        }
-        let mut full_dest = dest.to_path_buf();
-        if dest.is_dir() {
-            full_dest.push(source.file_name().unwrap()); //the destination path is the destination
-        } // directory + the file name we're copying
-        if verbose {
-            println!("{} -> {}", source.display(), full_dest.display());
-        }
-        if let Err(err) = fs::copy(source, full_dest) {
-            show_error!("{}", err);
-            panic!();
-        }
-    } else {
-        if !dest.is_dir() {
-            show_error!("TARGET must be a directory");
-            panic!();
-        }
-        for src in &sources {
-            let source = Path::new(&src);
-
-            if !source.is_file() {
-                show_error!("\"{}\" is not a file", source.display());
-                continue;
-            }
-
-            let mut full_dest = dest.to_path_buf();
-
-            full_dest.push(source.file_name().unwrap());
-
-            if verbose {
-                println!("{} -> {}", source.display(), full_dest.display());
-            }
-
-            let io_result = fs::copy(source, full_dest);
-
-            if let Err(err) = io_result {
-                show_error!("{}", err);
-                panic!()
+                    }
+                }
+                if behavior.verbose {
+                    println!("{} -> {}", item.display(), full_dest.display());
+                }
+                let io_result = if behavior.link {
+                    fs::hard_link(item, full_dest).err()
+                } else {
+                    fs::copy(item, full_dest).err() //carry out the copy
+                };
+                match io_result {
+                    None => continue,
+                    Some(t) => {
+                        crash!(1, "{}", t)
+                    }
+                }
             }
         }
     }
@@ -180,4 +296,15 @@ pub fn paths_refer_to_same_file(p1: &Path, p2: &Path) -> Result<bool> {
     let pathbuf2 = try!(canonicalize(p2, CanonicalizeMode::Normal));
 
     Ok(pathbuf1 == pathbuf2)
+}
+
+fn read_yes() -> bool {
+    let mut s = String::new();
+    match BufReader::new(stdin()).read_line(&mut s) {
+        Ok(_) => match s.char_indices().nth(0) {
+            Some((_, x)) => x == 'y' || x == 'Y',
+            _ => false
+        },
+        _ => false
+    }
 }
