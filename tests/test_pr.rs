000a2f5521206a8422413f0c896ab6035f4e55f7
Tilak Patidar <tilakpatidar@gmail.com>
2018-12-16 11:53:30 +0530

    pr: add suport for -n [char][width] and -N
    pr: Fix long name for -n
    
    pr: Add -N first line number option
    
    pr: Add -n[char][width] support

--- tests/test_pr.rs
+++ tests/test_pr.rs
@@ -124,3 +124,59 @@ fn test_with_long_double_space_option() {
             ("{last_modified_time}".to_string(), value),
         ]);
 }
+
+#[test]
+fn test_with_first_line_number_option() {
+    let test_file_path = "test_one_page.log";
+    let expected_test_file_path = "test_one_page_first_line.log.expected";
+    let mut scenario = new_ucmd!();
+    let value = file_last_modified_time(&scenario, test_file_path);
+    scenario
+        .args(&["-N", "5", "-n", test_file_path])
+        .succeeds()
+        .stdout_is_templated_fixture(expected_test_file_path, vec![
+            ("{last_modified_time}".to_string(), value),
+        ]);
+}
+
+#[test]
+fn test_with_first_line_number_long_option() {
+    let test_file_path = "test_one_page.log";
+    let expected_test_file_path = "test_one_page_first_line.log.expected";
+    let mut scenario = new_ucmd!();
+    let value = file_last_modified_time(&scenario, test_file_path);
+    scenario
+        .args(&["--first-line-number=5", "-n", test_file_path])
+        .succeeds()
+        .stdout_is_templated_fixture(expected_test_file_path, vec![
+            ("{last_modified_time}".to_string(), value),
+        ]);
+}
+
+#[test]
+fn test_with_number_option_with_custom_separator_char() {
+    let test_file_path = "test_num_page.log";
+    let expected_test_file_path = "test_num_page_char.log.expected";
+    let mut scenario = new_ucmd!();
+    let value = file_last_modified_time(&scenario, test_file_path);
+    scenario
+        .args(&["-nc", test_file_path])
+        .succeeds()
+        .stdout_is_templated_fixture(expected_test_file_path, vec![
+            ("{last_modified_time}".to_string(), value),
+        ]);
+}
+
+#[test]
+fn test_with_number_option_with_custom_separator_char_and_width() {
+    let test_file_path = "test_num_page.log";
+    let expected_test_file_path = "test_num_page_char_one.log.expected";
+    let mut scenario = new_ucmd!();
+    let value = file_last_modified_time(&scenario, test_file_path);
+    scenario
+        .args(&["-nc1", test_file_path])
+        .succeeds()
+        .stdout_is_templated_fixture(expected_test_file_path, vec![
+            ("{last_modified_time}".to_string(), value),
+        ]);
+}
