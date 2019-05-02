8a86c844616223a293634e057eec3e9630ea2d15
Alex Lyon <arcterus@mail.com>
2019-05-01 17:51:59 -0700

    uucore: allow the Linux zero-copy setup on Android

--- src/uucore/zero_copy/platform/mod.rs
+++ src/uucore/zero_copy/platform/mod.rs
@@ -1,21 +1,21 @@
 #[cfg(unix)]
 pub use self::unix::*;
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "android"))]
 pub use self::linux::*;
 #[cfg(windows)]
 pub use self::windows::*;
 
 // Add any operating systems we support here
-#[cfg(not(any(target_os = "linux")))]
+#[cfg(not(any(target_os = "linux", target_os = "android")))]
 pub use self::default::*;
 
 #[cfg(unix)]
 mod unix;
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "android"))]
 mod linux;
 #[cfg(windows)]
 mod windows;
 
 // Add any operating systems we support here
-#[cfg(not(any(target_os = "linux")))]
+#[cfg(not(any(target_os = "linux", target_os = "android")))]
 mod default;
