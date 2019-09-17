uutils/coreutils

commit 2df34512b70464892d38a3092a1b2e4f429f4472

more: set console mode on Windows

src/more/more.rs
@@ -29,6 +29,13 @@ extern crate redox_termios;
 #[cfg(target_os = "redox")]
 extern crate syscall;
 
+#[cfg(windows)]
+extern crate kernel32;
+extern crate winapi;
+use kernel32::{GetStdHandle, GetConsoleMode, SetConsoleMode};
+use winapi::um::wincon::{ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT};
+use winapi::um::winbase::{STD_INPUT_HANDLE};
+
 #[derive(Clone, Eq, PartialEq)]
 pub enum Mode {
     More,
@@ -96,7 +103,22 @@ fn setup_term() -> termios::Termios {
     term
 }
 
-#[cfg(any(windows, target_os = "fuchsia"))]
+#[cfg(windows)]
+#[inline(always)]
+fn setup_term() { unsafe {
+    let term = GetStdHandle(STD_INPUT_HANDLE); // TODO PIPE case
+    let mut mode = 0;
+    let ok = GetConsoleMode(term, &mut mode);
+    if ok == 0 {
+        panic!("Could not get Console Mode");
+    }
+    let ok = SetConsoleMode(term, mode & !ENABLE_ECHO_INPUT & !ENABLE_LINE_INPUT);
+    if ok == 0 {
+        panic!("Could not set Console Mode");
+    }
+}}
+
+#[cfg(target_os = "fuchsia")]
 #[inline(always)]
 fn setup_term() -> usize {
     0
@@ -121,7 +143,22 @@ fn reset_term(term: &mut termios::Termios) {
     termios::tcsetattr(0, termios::TCSADRAIN, &term).unwrap();
 }
 
-#[cfg(any(windows, target_os = "fuchsia"))]
+#[cfg(windows)]
+#[inline(always)]
+fn reset_term(_: &mut ()) { unsafe {
+    let term = GetStdHandle(STD_INPUT_HANDLE); // TODO PIPE case
+    let mut mode = 0;
+    let ok = GetConsoleMode(term, &mut mode);
+    if ok == 0 {
+        panic!("Could not get Console Mode");
+    }
+    let ok = SetConsoleMode(term, mode | ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
+    if ok == 0 {
+        panic!("Could not set Console Mode");
+    }
+}}
+
+#[cfg(target_os = "fuchsia")]
 #[inline(always)]
 fn reset_term(_: &mut usize) {}
 

src/more/Cargo.toml
@@ -20,6 +20,10 @@ redox_syscall = "0.1"
 [target.'cfg(all(unix, not(target_os = "fuchsia")))'.dependencies]
 nix = "0.8.1"
 
+[target.'cfg(windows)'.dependencies]
+kernel32-sys = "0.2.2"
+winapi = { version = "0.3.7", features = [ "winbase", "wincon" ] }
+
 [[bin]]
 name = "more"
 path = "../../uumain.rs"

commit 381ac42101cb7a8727673771dea894c559b2b07a

more: conditional compilation for Windows [fixup]
2df34512 broke compilation everywhere except windows, because I
expected the cfg attribute to apply to multiple lines.  This fixes it.

src/more/more.rs
@@ -31,9 +31,13 @@ extern crate syscall;
 
 #[cfg(windows)]
 extern crate kernel32;
+#[cfg(windows)]
 extern crate winapi;
+#[cfg(windows)]
 use kernel32::{GetStdHandle, GetConsoleMode, SetConsoleMode};
+#[cfg(windows)]
 use winapi::um::wincon::{ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT};
+#[cfg(windows)]
 use winapi::um::winbase::{STD_INPUT_HANDLE};
 
 #[derive(Clone, Eq, PartialEq)]