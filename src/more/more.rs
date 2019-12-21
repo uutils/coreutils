a9b1a03b372e241a97890a827697c785b0e1ec6c
Roy Ivy III <rivy.dev@gmail.com>
2019-12-21 14:43:09 -0600

    refactor/polish ~ fix `cargo clippy` complaints (fatal/write_all)

--- src/more/more.rs
+++ src/more/more.rs
@@ -146,7 +146,7 @@ fn more(matches: getopts::Matches) {
         if sz == 0 {
             break;
         }
-        stdout().write(&buffer[0..sz]).unwrap();
+        stdout().write_all(&buffer[0..sz]).unwrap();
         for byte in std::io::stdin().bytes() {
             match byte.unwrap() {
                 b' ' => break,
