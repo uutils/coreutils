a93d007cde36a3370d25e7a7bf9646f266875601
Sylvestre Ledru <sledru@mozilla.com>
2020-05-05 15:52:17 +0200

    test(uname): Add a test for the -o/--operating-system flag

--- tests/test_uname.rs
+++ tests/test_uname.rs
@@ -15,3 +15,13 @@ fn test_uname_name() {
     let result = ucmd.arg("-n").run();
     assert!(result.success);
 }
+
+#[test]
+fn test_uname_kernel() {
+    let (_, mut ucmd) = at_and_ucmd!();
+
+    let result = ucmd.arg("-o").run();
+    assert!(result.success);
+    #[cfg(target_os = "linux")]
+    assert!(result.stdout.to_lowercase().contains("linux"));
+}
