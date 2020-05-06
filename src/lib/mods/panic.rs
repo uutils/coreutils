e41b4148006f3ff1d301a35ee9dc5a8066c45773
Roy Ivy III <rivy.dev@gmail.com>
2020-05-06 12:57:30 -0500

    change ~ improve naming and documentation of default SIGPIPE handling

--- src/lib/mods/panic.rs
+++ src/lib/mods/panic.rs
@@ -1,6 +1,12 @@
+// spell-checker:ignore () SIGPIPE maint uutils
+
 use std::panic;
 
-pub fn install_sigpipe_hook() {
+//## SIGPIPE handling background/discussions ...
+//* `uutils` ~ <https://github.com/uutils/coreutils/issues/374> , <https://github.com/uutils/coreutils/pull/1106>
+//* rust and `rg` ~ <https://github.com/rust-lang/rust/issues/62569> , <https://github.com/BurntSushi/ripgrep/issues/200> , <https://github.com/crev-dev/cargo-crev/issues/287>
+
+pub fn mute_sigpipe_panic() {
     let hook = panic::take_hook();
     panic::set_hook(Box::new(move |info| {
         if let Some(res) = info.payload().downcast_ref::<String>() {
