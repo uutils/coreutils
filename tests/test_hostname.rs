186c173559d6b42684279f78685b3938b308a992
Sylvestre Ledru <sylvestre@debian.org>
2020-05-18 23:07:21 +0200

    test(hostname): Test more options (#1515)

--- tests/test_hostname.rs
+++ tests/test_hostname.rs
@@ -9,3 +9,18 @@ fn test_hostname() {
     assert!(ls_default_res.stdout.len() >= ls_short_res.stdout.len());
     assert!(ls_default_res.stdout.len() >= ls_domain_res.stdout.len());
 }
+
+#[test]
+fn test_hostname_ip() {
+    let result = new_ucmd!().arg("-i").succeeds();
+    assert!(!result.stdout.trim().is_empty());
+}
+
+#[test]
+fn test_hostname_full() {
+    let result = new_ucmd!().arg("-f").succeeds();
+    assert!(!result.stdout.trim().is_empty());
+
+    let ls_short_res = new_ucmd!().arg("-s").succeeds();
+    assert!(result.stdout.trim().contains(ls_short_res.stdout.trim()));
+}
