e1a6dbe6193f97727e0a1493151dd6cb1119a65d
nicoo <nicoo@debian.org>
2020-05-24 15:41:23 +0200

    factor::table: Remove obsolete, commented code

--- src/uu/factor/src/table.rs
+++ src/uu/factor/src/table.rs
@@ -28,14 +28,5 @@ pub(crate) fn factor(mut num: u64) -> (Factors, u64) {
         }
     }
 
-    // do we still have more factoring to do?
-    // Decide whether to use Pollard Rho or slow divisibility based on
-    // number's size:
-    //if num >= 1 << 63 {
-    // number is too big to use rho pollard without overflowing
-    //trial_division_slow(num, factors);
-    //} else if num > 1 {
-    // number is still greater than 1, but not so big that we have to worry
     (factors, num)
-    //}
 }
