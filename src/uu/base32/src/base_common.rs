e3fd15e73c53878e6b970b4d5386b7168548e4c9
Roy Ivy III <rivy.dev@gmail.com>
2020-05-27 00:31:30 -0500

    fix `cargo clippy` complaint (single_component_path_imports)

--- src/uu/base32/src/base_common.rs
+++ src/uu/base32/src/base_common.rs
@@ -7,7 +7,6 @@
 // For the full copyright and license information, please view the LICENSE file
 // that was distributed with this source code.
 
-use uucore;
 use uucore::encoding::{wrap_print, Data, Format};
 
 use std::fs::File;
