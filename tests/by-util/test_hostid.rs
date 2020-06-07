3d240e55633a45e133668684abfe8f8d90255d53
Sylvestre Ledru <sylvestre@debian.org>
2020-06-07 17:25:35 +0200

    test(hostid) add a test for hostid

--- tests/by-util/test_hostid.rs
+++ tests/by-util/test_hostid.rs
@@ -1,1 +1,13 @@
-// ToDO: add tests
+use crate::common::util::*;
+extern crate regex;
+use self::regex::Regex;
+
+#[test]
+fn test_normal() {
+    let (_, mut ucmd) = at_and_ucmd!();
+    let result = ucmd.run();
+
+    assert!(result.success);
+    let re = Regex::new(r"^[0-9a-f]{8}").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+}
