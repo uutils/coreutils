c11cebc4d3ddc1ac2bf8ed3645f90faa8047e234
nicoo <nicoo@debian.org>
2020-08-19 10:21:32 +0200

    factor::numeric::gcd: Exclude the 0 case from test `divisor`

--- src/uu/factor/src/numeric/gcd.rs
+++ src/uu/factor/src/numeric/gcd.rs
@@ -75,8 +75,7 @@ mod tests {
         fn divisor(a: u64, b: u64) -> bool {
             // Test that gcd(a, b) divides a and b
             let g = gcd(a, b);
-            if g == 0 { return a == 0 && b == 0; }
-            a % g == 0 && b % g == 0
+            (a == 0 && b == 0) || (a % g == 0 && b % g == 0)
         }
 
         fn commutative(a: u64, b: u64) -> bool {
