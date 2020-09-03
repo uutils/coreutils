e6d99d973f4bf964559ae8162e9611da88de5217
Roy Ivy III <rivy.dev@gmail.com>
2020-09-03 14:47:36 -0500

    factor::numeric::gcd: modify `divisor()` test to return correct true/false results for all possible inputs

--- src/uu/factor/src/numeric/gcd.rs
+++ src/uu/factor/src/numeric/gcd.rs
@@ -79,7 +79,7 @@ mod tests {
         fn divisor(a: u64, b: u64) -> bool {
             // Test that gcd(a, b) divides a and b
             let g = gcd(a, b);
-            (a == 0 && b == 0) || (a % g == 0 && b % g == 0)
+            (g != 0 && a % g == 0 && b % g == 0) || (g == 0 && a == 0 && b == 0)
         }
 
         fn commutative(a: u64, b: u64) -> bool {
