8964b3702b574e53106d365265d7826d7442920e
Sylvestre Ledru <sylvestre@debian.org>
2020-11-15 22:44:36 +0100

    feature(sync): add --data & --file-system

--- tests/by-util/test_sync.rs
+++ tests/by-util/test_sync.rs
@@ -1,11 +1,39 @@
 use crate::common::util::*;
 
+use std::fs;
+extern crate tempfile;
+use self::tempfile::tempdir;
+
 #[test]
 fn test_sync_default() {
-    new_ucmd!().run();
+    let result = new_ucmd!().run();
+    assert!(result.success);
 }
 
 #[test]
 fn test_sync_incorrect_arg() {
     new_ucmd!().arg("--foo").fails();
+}
+
+#[test]
+fn test_sync_fs() {
+    let temporary_directory = tempdir().unwrap();
+    let temporary_path = fs::canonicalize(temporary_directory.path()).unwrap();
+    let result = new_ucmd!().arg("--file-system").arg(&temporary_path).run();
+    assert!(result.success);
+}
+
+#[test]
+fn test_sync_data() {
+    // Todo add a second arg
+    let temporary_directory = tempdir().unwrap();
+    let temporary_path = fs::canonicalize(temporary_directory.path()).unwrap();
+    let result = new_ucmd!().arg("--data").arg(&temporary_path).run();
+    assert!(result.success);
+}
+
+#[test]
+fn test_sync_no_existing_files() {
+    let result = new_ucmd!().arg("--data").arg("do-no-exist").fails();
+    assert!(result.stderr.contains("error: cannot stat"));
 }
