9094b876c5bb9df231465ee1f564f620358931a9
Sylvestre Ledru <sledru@mozilla.com>
2021-02-16 13:35:15 +0100

    Update of the comment

--- tests/by-util/test_wc.rs
+++ tests/by-util/test_wc.rs
@@ -15,7 +15,8 @@ fn test_utf8() {
         .pipe_in_fixture("UTF_8_test.txt")
         .run()
         .stdout_is(" 0 0 0 0 0\n");
-    //GNU returns "  300  2086 22219 22781    79"
+    // GNU returns "  300  2086 22219 22781    79"
+    // TODO: we should fix that to match GNU's behavior
 }
 
 #[test]
