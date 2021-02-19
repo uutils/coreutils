61a0bf53677158caaca2bc8407983d0c15671643
Alex Lyon <dev@arct.rs>
2021-02-18 18:46:30 -0800

    uucore: fix potential use-after-free in utmpx iterator

--- src/uucore/src/lib/features/utmpx.rs
+++ src/uucore/src/lib/features/utmpx.rs
@@ -242,9 +242,10 @@ impl UtmpxIter {
     ///
     /// If not set, default record file will be used(file path depends on the target OS)
     pub fn read_from(self, f: &str) -> Self {
-        // FixME: discuss and revise a rewrite which is correct and satisfies clippy/rustc
-        #[allow(clippy::temporary_cstring_as_ptr)]
-        let res = unsafe { utmpxname(CString::new(f).unwrap().as_ptr()) };
+        let res = unsafe {
+            let cstr = CString::new(f).unwrap();
+            utmpxname(cstr.as_ptr())
+        };
         if res != 0 {
             println!("Warning: {}", IOError::last_os_error());
         }
