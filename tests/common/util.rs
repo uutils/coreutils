5ced3a670b02f296d51ef60dd790ad7e0686fa24
Ali <320322+ali5h@users.noreply.github.com>
2021-03-12 04:26:09 -0800

    paste: cleanup multi-stdin support (#1803)
    cleaner impl for multi-stdin support

--- tests/common/util.rs
+++ tests/common/util.rs
@@ -261,8 +261,7 @@ impl AtPath {
     }
 
     pub fn write(&self, name: &str, contents: &str) {
-        let mut f = self.open(name);
-        let _ = f.write(contents.as_bytes());
+        let _ = std::fs::write(self.plus(name), contents);
     }
 
     pub fn append(&self, name: &str, contents: &str) {
