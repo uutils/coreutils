2158b2c5b43c501b66e1db0fc8715878c78c0f73
Andre Julius <noromoron@gmail.com>
2021-03-13 23:11:11 +0100

    sleep: move from getopts to clap #1735 (#1777)
    and Add some sleep test cases #1735

--- tests/by-util/test_sleep.rs
+++ tests/by-util/test_sleep.rs
@@ -1,1 +1,47 @@
-// ToDO: add tests
+use crate::common::util::*;
+
+use std::time::{Duration, Instant};
+
+#[test]
+fn test_sleep_no_suffix() {
+    let millis_100 = Duration::from_millis(100);
+    let before_test = Instant::now();
+
+    new_ucmd!().args(&["0.1"]).succeeds().stdout_only("");
+
+    let duration = before_test.elapsed();
+    assert!(duration >= millis_100);
+}
+
+#[test]
+fn test_sleep_s_suffix() {
+    let millis_100 = Duration::from_millis(100);
+    let before_test = Instant::now();
+
+    new_ucmd!().args(&["0.1s"]).succeeds().stdout_only("");
+
+    let duration = before_test.elapsed();
+    assert!(duration >= millis_100);
+}
+
+#[test]
+fn test_sleep_m_suffix() {
+    let millis_600 = Duration::from_millis(600);
+    let before_test = Instant::now();
+
+    new_ucmd!().args(&["0.01m"]).succeeds().stdout_only("");
+
+    let duration = before_test.elapsed();
+    assert!(duration >= millis_600);
+}
+
+#[test]
+fn test_sleep_h_suffix() {
+    let millis_360 = Duration::from_millis(360);
+    let before_test = Instant::now();
+
+    new_ucmd!().args(&["0.0001h"]).succeeds().stdout_only("");
+
+    let duration = before_test.elapsed();
+    assert!(duration >= millis_360);
+}
