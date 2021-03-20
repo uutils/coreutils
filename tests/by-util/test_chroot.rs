607aa272bbac3c4ca9e9d511a23921b47d213113
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-20 15:14:00 +0300

    test: add test for no such directory

--- tests/by-util/test_chroot.rs
+++ tests/by-util/test_chroot.rs
@@ -9,3 +9,14 @@ fn test_missing_operand() {
         )
         .status_code(1);
 }
+
+#[test]
+fn test_no_such_directory() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.touch(&at.plus_as_string("a"));
+
+    ucmd.arg(&["a"])
+        .fails()
+        .stderr_is("chroot: error: cannot change root directory to `a`: no such directory")
+}
