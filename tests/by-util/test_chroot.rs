cfce5f119ddd638a715c595962b780c258d1abaf
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-20 14:10:47 +0300

    test: add test for missing operand

--- tests/by-util/test_chroot.rs
+++ tests/by-util/test_chroot.rs
@@ -1,1 +1,8 @@
-// ToDO: add tests
+use crate::common::util::*;
+
+#[test]
+fn test_missing_operand() {
+    new_ucmd!()
+        .fails()
+        .stderr_is("Missing operand: NEWROOT\nTry `chroot --help` for more information.");
+}
