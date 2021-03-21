68fc1f3c1e478b4f3f3aa894b38741e7048ee98c
Árni Dagur <arni@dagur.eu>
2021-03-21 07:08:34 +0000

    wc: Don't read() if we only need to count number of bytes

--- src/uu/wc/src/wc.rs
+++ src/uu/wc/src/wc.rs
@@ -10,13 +10,30 @@
 #[macro_use]
 extern crate uucore;
 
-use clap::{App, Arg, ArgMatches};
+mod count_bytes;
+use count_bytes::count_bytes_fast;
 
+use clap::{App, Arg, ArgMatches};
+use thiserror::Error;
+
+use std::cmp::max;
 use std::fs::File;
-use std::io::{stdin, BufRead, BufReader, Read};
+use std::io::{self, stdin, BufRead, BufReader, Read, Stdin};
+use std::ops::{Add, AddAssign};
+#[cfg(unix)]
+use std::os::unix::io::AsRawFd;
 use std::path::Path;
-use std::result::Result as StdResult;
 use std::str::from_utf8;
+
+#[derive(Error, Debug)]
+pub enum WcError {
+    #[error("IO error")]
+    Io(#[from] io::Error),
+    #[error("Expected a file, found directory {0}")]
+    IsDirectory(String),
+}
+
+type WcResult<T> = Result<T, WcError>;
 
 struct Settings {
     show_bytes: bool,
@@ -55,13 +72,59 @@ impl Settings {
     }
 }
 
-struct Result {
-    title: String,
+#[cfg(unix)]
+trait WordCountable: Read + AsRawFd {}
+#[cfg(not(unix))]
+trait WordCountable: Read {}
+impl WordCountable for Stdin {}
+impl WordCountable for File {}
+
+#[derive(Debug, Default, Copy, Clone)]
+struct WordCount {
     bytes: usize,
     chars: usize,
     lines: usize,
     words: usize,
     max_line_length: usize,
+}
+
+impl Add for WordCount {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            bytes: self.bytes + other.bytes,
+            chars: self.chars + other.chars,
+            lines: self.lines + other.lines,
+            words: self.words + other.words,
+            max_line_length: max(self.max_line_length, other.max_line_length),
+        }
+    }
+}
+
+impl AddAssign for WordCount {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other
+    }
+}
+
+impl WordCount {
+    fn with_title<'a>(self, title: &'a str) -> TitledWordCount<'a> {
+        return TitledWordCount {
+            title: title,
+            count: self,
+        };
+    }
+}
+
+/// This struct supplements the actual word count with a title that is displayed
+/// to the user at the end of the program.
+/// The reason we don't simply include title in the `WordCount` struct is that
+/// it would result in unneccesary copying of `String`.
+#[derive(Debug, Default, Clone)]
+struct TitledWordCount<'a> {
+    title: &'a str,
+    count: WordCount,
 }
 
 static ABOUT: &str = "Display newline, word, and byte counts for each FILE, and a total line if
@@ -137,12 +200,11 @@ pub fn uumain(args: impl uucore::Args) -> i32 {
 
     let settings = Settings::new(&matches);
 
-    match wc(files, &settings) {
-        Ok(()) => ( /* pass */ ),
-        Err(e) => return e,
+    if wc(files, &settings).is_ok() {
+        0
+    } else {
+        1
     }
-
-    0
 }
 
 const CR: u8 = b'\r';
@@ -157,151 +219,152 @@ fn is_word_separator(byte: u8) -> bool {
     byte == SPACE || byte == TAB || byte == CR || byte == SYN || byte == FF
 }
 
-fn wc(files: Vec<String>, settings: &Settings) -> StdResult<(), i32> {
-    let mut total_line_count: usize = 0;
-    let mut total_word_count: usize = 0;
-    let mut total_char_count: usize = 0;
-    let mut total_byte_count: usize = 0;
-    let mut total_longest_line_length: usize = 0;
-
-    let mut results = vec![];
-    let mut max_width: usize = 0;
+fn word_count_from_reader<T: WordCountable>(
+    reader: &mut T,
+    settings: &Settings,
+    path: &String,
+) -> WcResult<WordCount> {
+    let only_count_bytes = settings.show_bytes
+        && (!(settings.show_chars
+            || settings.show_lines
+            || settings.show_max_line_length
+            || settings.show_words));
+    if only_count_bytes {
+        return Ok(WordCount {
+            bytes: count_bytes_fast(reader)?,
+            ..WordCount::default()
+        });
+    }
 
     // we do not need to decode the byte stream if we're only counting bytes/newlines
     let decode_chars = settings.show_chars || settings.show_words || settings.show_max_line_length;
 
+    let mut line_count: usize = 0;
+    let mut word_count: usize = 0;
+    let mut byte_count: usize = 0;
+    let mut char_count: usize = 0;
+    let mut longest_line_length: usize = 0;
+    let mut raw_line = Vec::new();
+    let mut ends_lf: bool;
+
+    let mut buffered_reader = BufReader::new(reader);
+    // reading from a TTY seems to raise a condition on, rather than return Some(0) like a file.
+    // hence the option wrapped in a result here
+    while match buffered_reader.read_until(LF, &mut raw_line) {
+        Ok(n) if n > 0 => true,
+        Err(ref e) if !raw_line.is_empty() => {
+            show_warning!("Error while reading {}: {}", path, e);
+            !raw_line.is_empty()
+        }
+        _ => false,
+    } {
+        // GNU 'wc' only counts lines that end in LF as lines
+        ends_lf = *raw_line.last().unwrap() == LF;
+        line_count += ends_lf as usize;
+
+        byte_count += raw_line.len();
+
+        if decode_chars {
+            // try and convert the bytes to UTF-8 first
+            let current_char_count;
+            match from_utf8(&raw_line[..]) {
+                Ok(line) => {
+                    word_count += line.split_whitespace().count();
+                    current_char_count = line.chars().count();
+                }
+                Err(..) => {
+                    word_count += raw_line.split(|&x| is_word_separator(x)).count();
+                    current_char_count = raw_line.iter().filter(|c| c.is_ascii()).count()
+                }
+            }
+            char_count += current_char_count;
+            if current_char_count > longest_line_length {
+                // -L is a GNU 'wc' extension so same behavior on LF
+                longest_line_length = current_char_count - (ends_lf as usize);
+            }
+        }
+
+        raw_line.truncate(0);
+    }
+
+    Ok(WordCount {
+        bytes: byte_count,
+        chars: char_count,
+        lines: line_count,
+        words: word_count,
+        max_line_length: longest_line_length,
+    })
+}
+
+fn word_count_from_path(path: &String, settings: &Settings) -> WcResult<WordCount> {
+    if path == "-" {
+        let mut reader = stdin();
+        return Ok(word_count_from_reader(&mut reader, settings, path)?);
+    } else {
+        let path_obj = Path::new(path);
+        if path_obj.is_dir() {
+            return Err(WcError::IsDirectory(path.clone()));
+        } else {
+            let mut reader = File::open(path)?;
+            return Ok(word_count_from_reader(&mut reader, settings, path)?);
+        }
+    }
+}
+
+fn wc(files: Vec<String>, settings: &Settings) -> Result<(), u32> {
+    let mut total_word_count = WordCount::default();
+    let mut results = vec![];
+    let mut max_width: usize = 0;
+    let mut error_count = 0;
+
+    let num_files = files.len();
+
     for path in &files {
-        let mut reader = open(&path[..])?;
-
-        let mut line_count: usize = 0;
-        let mut word_count: usize = 0;
-        let mut byte_count: usize = 0;
-        let mut char_count: usize = 0;
-        let mut longest_line_length: usize = 0;
-        let mut raw_line = Vec::new();
-        let mut ends_lf: bool;
-        // reading from a TTY seems to raise a condition on, rather than return Some(0) like a file.
-        // hence the option wrapped in a result here
-        while match reader.read_until(LF, &mut raw_line) {
-            Ok(n) if n > 0 => true,
-            Err(ref e) if !raw_line.is_empty() => {
-                show_warning!("Error while reading {}: {}", path, e);
-                !raw_line.is_empty()
-            }
-            _ => false,
-        } {
-            // GNU 'wc' only counts lines that end in LF as lines
-            ends_lf = *raw_line.last().unwrap() == LF;
-            line_count += ends_lf as usize;
-
-            byte_count += raw_line.len();
-
-            if decode_chars {
-                // try and convert the bytes to UTF-8 first
-                let current_char_count;
-                match from_utf8(&raw_line[..]) {
-                    Ok(line) => {
-                        word_count += line.split_whitespace().count();
-                        current_char_count = line.chars().count();
-                    }
-                    Err(..) => {
-                        word_count += raw_line.split(|&x| is_word_separator(x)).count();
-                        current_char_count = raw_line.iter().filter(|c| c.is_ascii()).count()
-                    }
-                }
-                char_count += current_char_count;
-                if current_char_count > longest_line_length {
-                    // -L is a GNU 'wc' extension so same behavior on LF
-                    longest_line_length = current_char_count - (ends_lf as usize);
-                }
-            }
-
-            raw_line.truncate(0);
-        }
-
-        results.push(Result {
-            title: path.clone(),
-            bytes: byte_count,
-            chars: char_count,
-            lines: line_count,
-            words: word_count,
-            max_line_length: longest_line_length,
+        let word_count = word_count_from_path(&path, settings).unwrap_or_else(|err| {
+            eprintln!("{}", err);
+            error_count += 1;
+            WordCount::default()
         });
-
-        total_line_count += line_count;
+        max_width = max(max_width, word_count.bytes.to_string().len() + 1);
         total_word_count += word_count;
-        total_char_count += char_count;
-        total_byte_count += byte_count;
-
-        if longest_line_length > total_longest_line_length {
-            total_longest_line_length = longest_line_length;
-        }
-
-        // used for formatting
-        max_width = total_byte_count.to_string().len() + 1;
+        results.push(word_count.with_title(path));
     }
 
     for result in &results {
         print_stats(settings, &result, max_width);
     }
 
-    if files.len() > 1 {
-        let result = Result {
-            title: "total".to_owned(),
-            bytes: total_byte_count,
-            chars: total_char_count,
-            lines: total_line_count,
-            words: total_word_count,
-            max_line_length: total_longest_line_length,
-        };
-        print_stats(settings, &result, max_width);
+    if num_files > 1 {
+        let total_result = total_word_count.with_title("total");
+        print_stats(settings, &total_result, max_width);
     }
 
-    Ok(())
+    match error_count {
+        0 => Ok(()),
+        _ => Err(error_count),
+    }
 }
 
-fn print_stats(settings: &Settings, result: &Result, max_width: usize) {
+fn print_stats(settings: &Settings, result: &TitledWordCount, max_width: usize) {
     if settings.show_lines {
-        print!("{:1$}", result.lines, max_width);
+        print!("{:1$}", result.count.lines, max_width);
     }
     if settings.show_words {
-        print!("{:1$}", result.words, max_width);
+        print!("{:1$}", result.count.words, max_width);
     }
     if settings.show_bytes {
-        print!("{:1$}", result.bytes, max_width);
+        print!("{:1$}", result.count.bytes, max_width);
     }
     if settings.show_chars {
-        print!("{:1$}", result.chars, max_width);
+        print!("{:1$}", result.count.chars, max_width);
     }
     if settings.show_max_line_length {
-        print!("{:1$}", result.max_line_length, max_width);
+        print!("{:1$}", result.count.max_line_length, max_width);
     }
 
-    if result.title != "-" {
-        println!(" {}", result.title);
+    if result.title == "-" {
+        println!("");
     } else {
-        println!();
-    }
-}
-
-fn open(path: &str) -> StdResult<BufReader<Box<dyn Read + 'static>>, i32> {
-    if "-" == path {
-        let reader = Box::new(stdin()) as Box<dyn Read>;
-        return Ok(BufReader::new(reader));
-    }
-
-    let fpath = Path::new(path);
-    if fpath.is_dir() {
-        show_info!("{}: is a directory", path);
-    }
-    match File::open(&fpath) {
-        Ok(fd) => {
-            let reader = Box::new(fd) as Box<dyn Read>;
-            Ok(BufReader::new(reader))
-        }
-        Err(e) => {
-            show_error!("wc: {}: {}", path, e);
-            Err(1)
-        }
+        println!(" {}", result.title);
     }
 }
