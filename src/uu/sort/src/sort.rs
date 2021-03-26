1a198c79b4ee5dad91cdda9df6eb2425118811ca
electricboogie <32370782+electricboogie@users.noreply.github.com>
2021-03-26 17:09:12 -0500

    replace fasthash with twox-hash crate, which builds one all targets

--- src/uu/sort/src/sort.rs
+++ src/uu/sort/src/sort.rs
@@ -12,8 +12,8 @@ extern crate uucore;
 
 use clap::{App, Arg};
 use itertools::Itertools;
+use twox_hash::XxHash64;
 use std::hash::{Hash, Hasher};
-use fasthash::{xx, XXHasher};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use semver::Version;
@@ -538,8 +538,8 @@ fn random_shuffle(a: &str, b: &str, salt: String) -> Ordering {
     #![allow(clippy::comparison_chain)]
     let salt_slice = salt.as_str();
 
-    let da = xx::hash64([a, salt_slice].concat());
-    let db = xx::hash64([b, salt_slice].concat());
+    let da = xxhash(&[a, salt_slice].concat());
+    let db = xxhash(&[b, salt_slice].concat());
 
     da.cmp(&db)
 }
@@ -554,7 +554,7 @@ fn get_rand_string() -> String {
 }
 
 fn xxhash<T: Hash>(t: &T) -> u64 {
-    let mut s: XXHasher = Default::default();
+    let mut s: XxHash64 = Default::default();
     t.hash(&mut s);
     s.finish()
 }
