1b941c426f1627127598a0a320f3419e6735ed1d
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-27 01:13:38 +0300

    test: add tests and fixtures

--- tests/by-util/test_cksum.rs
+++ tests/by-util/test_cksum.rs
@@ -24,3 +24,72 @@ fn test_stdin() {
         .succeeds()
         .stdout_is_fixture("stdin.expected");
 }
+
+#[test]
+fn test_empty() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.touch("a");
+
+    ucmd.arg("a").succeeds().stdout.ends_with("0 a");
+}
+
+#[test]
+fn test_arg_overrides_stdin() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.touch("a");
+
+    ucmd.pipe_in("foobarfoobar")
+        .arg("a")
+        .succeeds()
+        .stdout
+        .ends_with("0 a");
+}
+
+#[test]
+fn test_invalid_file() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    let folder_name = "a".to_string();
+    at.mkdir(&folder_name);
+
+    ucmd.arg(&folder_name).fails().stderr_is(format!(
+        "cksum: error: '{}' Is a directory (os error 21)",
+        &folder_name
+    ));
+}
+
+// Make sure crc is correct for files larger than 32 bytes
+// but <128 bytes (1 fold pclmul)
+#[test]
+fn test_crc_for_bigger_than_32_bytes() {
+    let (_, mut ucmd) = at_and_ucmd!();
+
+    let result = ucmd.arg("chars.txt").run();
+
+    let stdout_splitted: Vec<&str> = result.stdout.split(" ").collect();
+
+    let cksum: i64 = stdout_splitted[0].parse().unwrap();
+    let bytes_cnt: i64 = stdout_splitted[1].parse().unwrap();
+
+    assert!(result.success);
+    assert_eq!(cksum, 586047089);
+    assert_eq!(bytes_cnt, 16);
+}
+
+#[test]
+fn test_stdin_larger_than_128_bytes() {
+    let (_, mut ucmd) = at_and_ucmd!();
+
+    let result = ucmd.arg("larger_than_2056_bytes.txt").run();
+
+    let stdout_splitted: Vec<&str> = result.stdout.split(" ").collect();
+
+    let cksum: i64 = stdout_splitted[0].parse().unwrap();
+    let bytes_cnt: i64 = stdout_splitted[1].parse().unwrap();
+
+    assert!(result.success);
+    assert_eq!(cksum, 945881979);
+    assert_eq!(bytes_cnt, 2058);
+}
