d37f23349de58599518a3d38e69664449cfd35f1
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-28 13:03:10 +0300

    fix: check metadata of the path

--- src/uu/cksum/src/cksum.rs
+++ src/uu/cksum/src/cksum.rs
@@ -140,6 +140,19 @@ fn cksum(fname: &str) -> io::Result<(u32, usize)> {
     let mut rd: Box<dyn Read> = match fname {
         "-" => Box::new(stdin()),
         _ => {
+            let path = &Path::new(fname);
+            if path.is_dir() {
+                return Err(std::io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Is a directory",
+                ));
+            };
+            if !path.metadata().is_ok() {
+                return Err(std::io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "No such file or directory",
+                ));
+            };
             file = File::open(&Path::new(fname))?;
             Box::new(BufReader::new(file))
         }
