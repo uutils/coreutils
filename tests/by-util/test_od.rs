52b93c7bfc0fa63310fa8bcc928b1a158a763cdd
Mikadore <admin@mikadore.eu>
2021-03-30 18:55:00 +0200

    refactored life-time issues in od

--- tests/by-util/test_od.rs
+++ tests/by-util/test_od.rs
@@ -83,19 +83,19 @@ fn test_no_file() {
     let tmpdir = Path::new(&temp);
     let file = tmpdir.join("}surely'none'would'thus'a'file'name");
 
-    let result = new_ucmd!().arg(file.as_os_str()).succeeds();
+    new_ucmd!().arg(file.as_os_str()).succeeds();
 }
 
 // Test that od reads from stdin instead of a file
 #[test]
 fn test_from_stdin() {
     let input = "abcdefghijklmnopqrstuvwxyz\n";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
-        .run_piped_stdin(input.as_bytes()).success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), unindent(ALPHA_OUT));
+        .run_piped_stdin(input.as_bytes())
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(ALPHA_OUT));
 }
 
 // Test that od reads from stdin and also from files
@@ -114,31 +114,27 @@ fn test_from_mixed() {
         }
     }
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg(file1.as_os_str())
         .arg("-")
         .arg(file3.as_os_str())
         .run_piped_stdin(data2.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), unindent(ALPHA_OUT));
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(ALPHA_OUT));
 }
 
 #[test]
 fn test_multiple_formats() {
     let input = "abcdefghijklmnopqrstuvwxyz\n";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-c")
         .arg("-b")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             "
             0000000   a   b   c   d   e   f   g   h   i   j   k   l   m   n   o   p
                     141 142 143 144 145 146 147 150 151 152 153 154 155 156 157 160
@@ -146,8 +142,7 @@ fn test_multiple_formats() {
                     161 162 163 164 165 166 167 170 171 172 012
             0000033
             "
-        )
-    );
+        ));
 }
 
 #[test]
@@ -161,14 +156,13 @@ fn test_dec() {
             0000016
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-s")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -180,14 +174,13 @@ fn test_hex16() {
             0000011
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-x")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -199,14 +192,13 @@ fn test_hex32() {
             0000011
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-X")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -227,15 +219,14 @@ fn test_f16() {
             0000016
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-tf2")
         .arg("-w8")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -256,14 +247,13 @@ fn test_f32() {
             0000034
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-f")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -286,36 +276,31 @@ fn test_f64() {
             0000050
             ",
     );
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-F")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
 fn test_multibyte() {
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-c")
         .arg("-w12")
         .run_piped_stdin("Universität Tübingen \u{1B000}".as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             "
             0000000   U   n   i   v   e   r   s   i   t   ä  **   t
             0000014       T   ü  **   b   i   n   g   e   n       \u{1B000}
             0000030  **  **  **
             0000033
             "
-        )
-    );
+        ));
 }
 
 #[test]
@@ -329,10 +314,13 @@ fn test_width() {
             ",
     );
 
-    let result = new_ucmd!().arg("-w4").arg("-v").run_piped_stdin(&input[..]).success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+    new_ucmd!()
+        .arg("-w4")
+        .arg("-v")
+        .run_piped_stdin(&input[..])
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -346,13 +334,13 @@ fn test_invalid_width() {
             ",
     );
 
-    let result = new_ucmd!().arg("-w5").arg("-v").run_piped_stdin(&input[..]).success();
-
-    assert_eq!(
-        result.stderr_str(),
-        "od: warning: invalid width 5; using 2 instead\n"
-    );
-    assert_eq!(result.stdout_str(), expected_output);
+    new_ucmd!()
+        .arg("-w5")
+        .arg("-v")
+        .run_piped_stdin(&input[..])
+        .success()
+        .stderr_is("od: warning: invalid width 5; using 2 instead\n")
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -366,13 +354,13 @@ fn test_zero_width() {
             ",
     );
 
-    let result = new_ucmd!().arg("-w0").arg("-v").run_piped_stdin(&input[..]).success();
-
-    assert_eq!(
-        result.stderr_str(),
-        "od: warning: invalid width 0; using 2 instead\n"
-    );
-    assert_eq!(result.stdout_str(), expected_output);
+    new_ucmd!()
+        .arg("-w0")
+        .arg("-v")
+        .run_piped_stdin(&input[..])
+        .success()
+        .stderr_is("od: warning: invalid width 0; using 2 instead\n")
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -384,10 +372,12 @@ fn test_width_without_value() {
             0000050
             ");
 
-    let result = new_ucmd!().arg("-w").run_piped_stdin(&input[..]).success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+    new_ucmd!()
+        .arg("-w")
+        .run_piped_stdin(&input[..])
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -412,15 +402,14 @@ fn test_suppress_duplicates() {
             ",
     );
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-w4")
         .arg("-O")
         .arg("-x")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -437,17 +426,16 @@ fn test_big_endian() {
         ",
     );
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=big")
         .arg("-F")
         .arg("-f")
         .arg("-X")
         .arg("-x")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -465,16 +453,15 @@ fn test_alignment_Xxa() {
     );
 
     // in this case the width of the -a (8-bit) determines the alignment for the other fields
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-X")
         .arg("-x")
         .arg("-a")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -491,15 +478,14 @@ fn test_alignment_Fx() {
     );
 
     // in this case the width of the -F (64-bit) determines the alignment for the other field
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("-F")
         .arg("-x")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -519,16 +505,15 @@ fn test_maxuint() {
             ",
     );
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--format=o8")
         .arg("-Oobtu8")
         .arg("-Dd")
         .arg("--format=u1")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -544,15 +529,14 @@ fn test_hex_offset() {
             ",
     );
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-Ax")
         .arg("-X")
         .arg("-X")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -568,15 +552,14 @@ fn test_dec_offset() {
             ",
     );
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-Ad")
         .arg("-X")
         .arg("-X")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
@@ -585,47 +568,42 @@ fn test_no_offset() {
     const LINE: &'static str = " 00000000 00000000 00000000 00000000\n";
     let expected_output = [LINE, LINE, LINE, LINE].join("");
 
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-An")
         .arg("-X")
         .arg("-X")
         .run_piped_stdin(&input[..])
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), expected_output);
+        .success()
+        .no_stderr()
+        .stdout_is(expected_output);
 }
 
 #[test]
 fn test_invalid_offset() {
-    let result = new_ucmd!().arg("-Ab").fails();
+    new_ucmd!().arg("-Ab").fails();
 }
 
 #[test]
 fn test_skip_bytes() {
     let input = "abcdefghijklmnopq";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-c")
         .arg("--skip-bytes=5")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             "
             0000005   f   g   h   i   j   k   l   m   n   o   p   q
             0000021
-            "
-        )
-    );
+            ",
+        ));
 }
 
 #[test]
 fn test_skip_bytes_error() {
     let input = "12345";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--skip-bytes=10")
         .run_piped_stdin(input.as_bytes())
         .failure();
@@ -634,14 +612,13 @@ fn test_skip_bytes_error() {
 #[test]
 fn test_read_bytes() {
     let input = "abcdefghijklmnopqrstuvwxyz\n12345678";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--endian=little")
         .arg("--read-bytes=27")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(result.stdout_str(), unindent(ALPHA_OUT));
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(ALPHA_OUT));
 }
 
 #[test]
@@ -650,23 +627,22 @@ fn test_ascii_dump() {
         0x00, 0x01, 0x0a, 0x0d, 0x10, 0x1f, 0x20, 0x61, 0x62, 0x63, 0x7d, 0x7e, 0x7f, 0x80, 0x90,
         0xa0, 0xb0, 0xc0, 0xd0, 0xe0, 0xf0, 0xff,
     ];
-    let result = new_ucmd!().arg("-tx1zacz").run_piped_stdin(&input[..]).success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+    new_ucmd!()
+        .arg("-tx1zacz")
+        .run_piped_stdin(&input[..])
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             r"
             0000000  00  01  0a  0d  10  1f  20  61  62  63  7d  7e  7f  80  90  a0  >...... abc}~....<
                     nul soh  nl  cr dle  us  sp   a   b   c   }   ~ del nul dle  sp
-                     \0 001  \n  \r 020 037       a   b   c   }   ~ 177  **  **  **  >...... abc}~....<
+                    \0 001  \n  \r 020 037       a   b   c   }   ~ 177  **  **  **  >...... abc}~....<
             0000020  b0  c0  d0  e0  f0  ff                                          >......<
-                      0   @   P   `   p del
-                     ** 300 320 340 360 377                                          >......<
+                    0   @   P   `   p del
+                    ** 300 320 340 360 377                                          >......<
             0000026
-            "
-        )
-    );
+            ",
+        ));
 }
 
 #[test]
@@ -702,8 +678,8 @@ fn test_stdin_offset() {
     let result = new_ucmd!()
         .arg("-c")
         .arg("+5")
-        .run_piped_stdin(input.as_bytes())
-        .success();
+        .run_piped_stdin(input.as_bytes());
+    result.success();
 
     assert_empty_stderr!(result);
     assert_eq!(
@@ -719,25 +695,26 @@ fn test_stdin_offset() {
 
 #[test]
 fn test_file_offset() {
-    let result = new_ucmd!().arg("-c").arg("--").arg("-f").arg("10").succeeds();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+    new_ucmd!()
+        .arg("-c")
+        .arg("--")
+        .arg("-f")
+        .arg("10")
+        .succeeds()
+        .no_stderr()
+        .stdout_is(unindent(
             r"
-            0000010   w   e   r   c   a   s   e       f  \n
-            0000022
-            "
-        )
-    );
+        0000010   w   e   r   c   a   s   e       f  \n
+        0000022
+        ",
+        ));
 }
 
 #[test]
 fn test_traditional() {
     // note gnu od does not align both lines
     let input = "abcdefghijklmnopq";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--traditional")
         .arg("-a")
         .arg("-c")
@@ -745,72 +722,59 @@ fn test_traditional() {
         .arg("10")
         .arg("0")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .no_stderr()
+        .stdout_is(unindent(
             r"
             0000010 (0000000)   i   j   k   l   m   n   o   p   q
                                 i   j   k   l   m   n   o   p   q
             0000021 (0000011)
-            "
-        )
-    );
+            ",
+        ));
 }
 
 #[test]
 fn test_traditional_with_skip_bytes_override() {
     // --skip-bytes is ignored in this case
     let input = "abcdefghijklmnop";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--traditional")
         .arg("--skip-bytes=10")
         .arg("-c")
         .arg("0")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             r"
             0000000   a   b   c   d   e   f   g   h   i   j   k   l   m   n   o   p
             0000020
-            "
-        )
-    );
+            ",
+        ));
 }
 
 #[test]
 fn test_traditional_with_skip_bytes_non_override() {
     // no offset specified in the traditional way, so --skip-bytes is used
     let input = "abcdefghijklmnop";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--traditional")
         .arg("--skip-bytes=10")
         .arg("-c")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .success()
+        .no_stderr()
+        .stdout_is(unindent(
             r"
             0000012   k   l   m   n   o   p
             0000020
-            "
-        )
-    );
+            ",
+        ));
 }
 
 #[test]
 fn test_traditional_error() {
     // file "0" exists - don't fail on that, but --traditional only accepts a single input
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("--traditional")
         .arg("0")
         .arg("0")
@@ -822,7 +786,7 @@ fn test_traditional_error() {
 #[test]
 fn test_traditional_only_label() {
     let input = "abcdefghijklmnopqrstuvwxyz";
-    let result = new_ucmd!()
+    new_ucmd!()
         .arg("-An")
         .arg("--traditional")
         .arg("-a")
@@ -831,19 +795,14 @@ fn test_traditional_only_label() {
         .arg("10")
         .arg("0x10")
         .run_piped_stdin(input.as_bytes())
-        .success();
-
-    assert_empty_stderr!(result);
-    assert_eq!(
-        result.stdout_str(),
-        unindent(
+        .no_stderr()
+        .stdout_is(unindent(
             r"
             (0000020)   i   j   k   l   m   n   o   p   q   r   s   t   u   v   w   x
                         i   j   k   l   m   n   o   p   q   r   s   t   u   v   w   x
             (0000040)   y   z
                         y   z
             (0000042)
-            "
-        )
-    );
+            ",
+        ));
 }
