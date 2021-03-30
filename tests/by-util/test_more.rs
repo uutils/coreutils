76ad2027819395e3cec2f382885b3178e80a018a
Mikadore <admin@mikadore.eu>
2021-03-30 16:31:11 +0200

    fixed more

--- tests/by-util/test_more.rs
+++ tests/by-util/test_more.rs
@@ -3,6 +3,5 @@ use crate::common::util::*;
 #[test]
 fn test_more_no_arg() {
     let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.run();
-    assert!(!result.success);
-}
+    let result = ucmd.fails();
+ }
