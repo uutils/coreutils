c2ac7d8517b09f6ce97b73c51c52539086c336a7
Mikadore <admin@mikadore.eu>
2021-03-30 17:31:06 +0200

    fixed printenv

--- tests/by-util/test_printenv.rs
+++ tests/by-util/test_printenv.rs
@@ -7,10 +7,9 @@ fn test_get_all() {
     env::set_var(key, "VALUE");
     assert_eq!(env::var(key), Ok("VALUE".to_string()));
 
-    let result = TestScenario::new(util_name!()).ucmd_keepenv().run();
-    assert!(result.success);
-    assert!(result.stdout.contains("HOME="));
-    assert!(result.stdout.contains("KEY=VALUE"));
+    let result = TestScenario::new(util_name!()).ucmd_keepenv().succeeds();
+    assert!(result.stdout_str().contains("HOME="));
+    assert!(result.stdout_str().contains("KEY=VALUE"));
 }
 
 #[test]
@@ -22,9 +21,8 @@ fn test_get_var() {
     let result = TestScenario::new(util_name!())
         .ucmd_keepenv()
         .arg("KEY")
-        .run();
+        .succeeds();
 
-    assert!(result.success);
-    assert!(!result.stdout.is_empty());
-    assert!(result.stdout.trim() == "VALUE");
+    assert!(!result.stdout_str().is_empty());
+    assert!(result.stdout_str().trim() == "VALUE");
 }
