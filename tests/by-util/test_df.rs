1aa354f3d893d19cd296bca5e47d1afd7eabfe5a
Mikadore <admin@mikadore.eu>
2021-03-30 16:01:23 +0200

    fixed df

--- tests/by-util/test_df.rs
+++ tests/by-util/test_df.rs
@@ -3,29 +3,25 @@ use crate::common::util::*;
 #[test]
 fn test_df_compatible_no_size_arg() {
     let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("-a").run();
-    assert!(result.success);
-}
+    let result = ucmd.arg("-a").succeeds();
+ }
 
 #[test]
 fn test_df_compatible() {
     let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("-ah").run();
-    assert!(result.success);
+    let result = ucmd.arg("-ah").succeeds();
 }
 
 #[test]
 fn test_df_compatible_type() {
     let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("-aT").run();
-    assert!(result.success);
+    let result = ucmd.arg("-aT").succeeds();
 }
 
 #[test]
 fn test_df_compatible_si() {
     let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("-aH").run();
-    assert!(result.success);
+    let result = ucmd.arg("-aH").succeeds();
 }
 
 // ToDO: more tests...
