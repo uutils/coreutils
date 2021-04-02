973229c721fe0124fe081a63c09b73fd108d6891
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2021-04-02 13:56:50 +0100

    rm: more char magic

--- src/uucore/src/lib/features/fs.rs
+++ src/uucore/src/lib/features/fs.rs
@@ -337,7 +337,7 @@ mod tests {
             let path = Path::new(test.path);
             let normalized = normalize_path(path);
             assert_eq!(
-                test.test.replace("/", std::path::MAIN_SEPARATOR.as_str),
+                test.test.replace("/", std::path::MAIN_SEPARATOR.to_string().as_str()),
                 normalized.to_str().expect("Path is not valid utf-8!")
             );
         }
