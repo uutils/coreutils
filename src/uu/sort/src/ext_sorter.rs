0275a43c5bc414c52e40edbb1e2cd2d531255009
electricboogie <32370782+electricboogie@users.noreply.github.com>
2021-04-18 14:05:27 -0500

    Make modifications clearer per Apache license

--- src/uu/sort/src/ext_sorter.rs
+++ src/uu/sort/src/ext_sorter.rs
@@ -1,5 +1,5 @@
 // Copyright 2018 Andre-Philippe Paquet
-// Copyright 2021 Robert Swinford
+// Modifications copyright 2021 Robert Swinford
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// This file has been modified for use in the uutils project.
 
 use rayon::prelude::*;
 use std::{
