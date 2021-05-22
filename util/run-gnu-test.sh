0d1b14ee333bdaae9942cf45f1d5cb7e395f8ab6
Sylvestre Ledru <sylvestre@debian.org>
2021-05-22 11:08:03 +0200

    Bring back the run expensive tests

--- util/run-gnu-test.sh
+++ util/run-gnu-test.sh
@@ -4,4 +4,4 @@ BUILDDIR="${PWD}/uutils/target/release"
 GNULIB_DIR="${PWD}/gnulib"
 pushd gnu
 
-timeout -sKILL 2h make -j "$(nproc)" check SUBDIRS=. RUN_EXPENSIVE_TESTS=no RUN_VERY_EXPENSIVE_TESTS=no VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
+timeout -sKILL 2h make -j "$(nproc)" check SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
