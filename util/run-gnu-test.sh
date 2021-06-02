e7f59168646b1f2ccd533ce3991694a53970906c
Sylvestre Ledru <sylvestre@debian.org>
2021-06-02 21:21:12 +0200

    gnu/ci: allow to run a single gnu test (and document it)

--- util/run-gnu-test.sh
+++ util/run-gnu-test.sh
@@ -6,4 +6,10 @@ GNULIB_DIR="${PWD}/gnulib"
 pushd gnu
 
 export RUST_BACKTRACE=1
-timeout -sKILL 2h make -j "$(nproc)" check SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
+
+if test -n "$1"; then
+    # if set, run only the test passed
+    export RUN_TEST="TESTS=$1"
+fi
+
+timeout -sKILL 2h make -j "$(nproc)" check $RUN_TEST SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
