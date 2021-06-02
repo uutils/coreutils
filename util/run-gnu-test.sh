0105531b7f368ffa25464b17aaaba33ca3fa1608
Sylvestre Ledru <sylvestre@debian.org>
2021-06-02 20:59:53 +0200

    show the backtrace in case of a rust crash

--- util/run-gnu-test.sh
+++ util/run-gnu-test.sh
@@ -5,4 +5,5 @@ BUILDDIR="${PWD}/uutils/target/release"
 GNULIB_DIR="${PWD}/gnulib"
 pushd gnu
 
+export RUST_BACKTRACE=1
 timeout -sKILL 2h make -j "$(nproc)" check SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
