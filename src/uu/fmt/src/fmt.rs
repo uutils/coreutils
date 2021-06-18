a8d62b9b2351e4dc216942c998cedec19f6b6fe9
Michael Debertol <michael.debertol@gmail.com>
2021-06-18 11:57:19 +0200

    fmt: fix indentation for help

--- src/uu/fmt/src/fmt.rs
+++ src/uu/fmt/src/fmt.rs
@@ -219,10 +219,10 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("c")
                 .long(OPT_CROWN_MARGIN)
                 .help(
-                    "First and second line of paragraph
-        may have different indentations, in which
-        case the first line's indentation is preserved,
-        and each subsequent line's indentation matches the second line.",
+                    "First and second line of paragraph \
+                    may have different indentations, in which \
+                    case the first line's indentation is preserved, \
+                    and each subsequent line's indentation matches the second line.",
                 ),
         )
         .arg(
@@ -230,7 +230,7 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("t")
                 .long("tagged-paragraph")
                 .help(
-                    "Like -c, except that the first and second line of a paragraph *must*
+                    "Like -c, except that the first and second line of a paragraph *must* \
                     have different indentation or they are treated as separate paragraphs.",
                 ),
         )
@@ -239,7 +239,7 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("m")
                 .long("preserve-headers")
                 .help(
-                    "Attempt to detect and preserve mail headers in the input.
+                    "Attempt to detect and preserve mail headers in the input. \
                     Be careful when combining this flag with -p.",
                 ),
         )
@@ -254,10 +254,10 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("u")
                 .long("uniform-spacing")
                 .help(
-                    "Insert exactly one
-                    space between words, and two between sentences.
-                    Sentence breaks in the input are detected as [?!.]
-                    followed by two spaces or a newline; other punctuation
+                    "Insert exactly one \
+                    space between words, and two between sentences. \
+                    Sentence breaks in the input are detected as [?!.] \
+                    followed by two spaces or a newline; other punctuation \
                     is not interpreted as a sentence break.",
                 ),
         )
@@ -266,9 +266,9 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("p")
                 .long("prefix")
                 .help(
-                    "Reformat only lines
-                    beginning with PREFIX, reattaching PREFIX to reformatted lines.
-                    Unless -x is specified, leading whitespace will be ignored
+                    "Reformat only lines \
+                    beginning with PREFIX, reattaching PREFIX to reformatted lines. \
+                    Unless -x is specified, leading whitespace will be ignored \
                     when matching PREFIX.",
                 )
                 .value_name("PREFIX"),
@@ -278,8 +278,8 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("P")
                 .long("skip-prefix")
                 .help(
-                    "Do not reformat lines
-                    beginning with PSKIP. Unless -X is specified, leading whitespace
+                    "Do not reformat lines \
+                    beginning with PSKIP. Unless -X is specified, leading whitespace \
                     will be ignored when matching PSKIP",
                 )
                 .value_name("PSKIP"),
@@ -289,7 +289,7 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("x")
                 .long("exact-prefix")
                 .help(
-                    "PREFIX must match at the
+                    "PREFIX must match at the \
                     beginning of the line with no preceding whitespace.",
                 ),
         )
@@ -298,7 +298,7 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("X")
                 .long("exact-skip-prefix")
                 .help(
-                    "PSKIP must match at the
+                    "PSKIP must match at the \
                     beginning of the line with no preceding whitespace.",
                 ),
         )
@@ -317,7 +317,7 @@ pub fn uu_app() -> App<'static, 'static> {
                 .value_name("GOAL"),
         )
         .arg(Arg::with_name(OPT_QUICK).short("q").long("quick").help(
-            "Break lines more quickly at the
+            "Break lines more quickly at the \
             expense of a potentially more ragged appearance.",
         ))
         .arg(
@@ -325,8 +325,8 @@ pub fn uu_app() -> App<'static, 'static> {
                 .short("T")
                 .long("tab-width")
                 .help(
-                    "Treat tabs as TABWIDTH spaces for
-                    determining line length, default 8. Note that this is used only for
+                    "Treat tabs as TABWIDTH spaces for \
+                    determining line length, default 8. Note that this is used only for \
                     calculating line lengths; tabs are preserved in the output.",
                 )
                 .value_name("TABWIDTH"),
