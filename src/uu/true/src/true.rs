2e027bf45d717a47430008d963bcae55d2e5c567
Michael Debertol <michael.debertol@gmail.com>
2021-06-18 12:00:08 +0200

    true, false: enable --help and --version

--- src/uu/true/src/true.rs
+++ src/uu/true/src/true.rs
@@ -5,15 +5,14 @@
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
-use clap::{App, AppSettings};
+use clap::App;
 use uucore::executable;
 
-pub fn uumain(_: impl uucore::Args) -> i32 {
+pub fn uumain(args: impl uucore::Args) -> i32 {
+    uu_app().get_matches_from(args);
     0
 }
 
 pub fn uu_app() -> App<'static, 'static> {
     App::new(executable!())
-        .setting(AppSettings::DisableHelpFlags)
-        .setting(AppSettings::DisableVersion)
 }
