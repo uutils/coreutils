c2bd7ae9b1be95ec4ae4197a37790eb58093eeb1
Jan Scheer <jhscheer@users.noreply.github.com>
2021-07-07 15:53:26 +0200

    test_id: refactor use expected_result from common/util.rs

--- tests/by-util/test_id.rs
+++ tests/by-util/test_id.rs
@@ -2,54 +2,13 @@ use crate::common::util::*;
 
 // spell-checker:ignore (ToDO) coreutil
 
-// These tests run the GNU coreutils `(g)id` binary in `$PATH` in order to gather reference values.
-// If the `(g)id` in `$PATH` doesn't include a coreutils version string,
-// or the version is too low, the test is skipped.
-
-// The reference version is 8.32. Here 8.30 was chosen because right now there's no
-// ubuntu image for github action available with a higher version than 8.30.
-const VERSION_MIN: &str = "8.30"; // minimum Version for the reference `id` in $PATH
 const VERSION_MIN_MULTIPLE_USERS: &str = "8.31"; // this feature was introduced in GNU's coreutils 8.31
-const UUTILS_WARNING: &str = "uutils-tests-warning";
-const UUTILS_INFO: &str = "uutils-tests-info";
-
-macro_rules! unwrap_or_return {
-    ( $e:expr ) => {
-        match $e {
-            Ok(x) => x,
-            Err(e) => {
-                println!("{}: test skipped: {}", UUTILS_INFO, e);
-                return;
-            }
-        }
-    };
-}
-
-fn whoami() -> String {
-    // Apparently some CI environments have configuration issues, e.g. with 'whoami' and 'id'.
-    //
-    // From the Logs: "Build (ubuntu-18.04, x86_64-unknown-linux-gnu, feat_os_unix, use-cross)"
-    //    whoami: cannot find name for user ID 1001
-    // id --name: cannot find name for user ID 1001
-    // id --name: cannot find name for group ID 116
-    //
-    // However, when running "id" from within "/bin/bash" it looks fine:
-    // id: "uid=1001(runner) gid=118(docker) groups=118(docker),4(adm),101(systemd-journal)"
-    // whoami: "runner"
-
-    // Use environment variable to get current user instead of
-    // invoking `whoami` and fall back to user "nobody" on error.
-    std::env::var("USER").unwrap_or_else(|e| {
-        println!("{}: {}, using \"nobody\" instead", UUTILS_WARNING, e);
-        "nobody".to_string()
-    })
-}
 
 #[test]
 #[cfg(unix)]
 fn test_id_no_specified_user() {
     let result = new_ucmd!().run();
-    let exp_result = unwrap_or_return!(expected_result(&[]));
+    let exp_result = unwrap_or_return!(expected_result(util_name!(), &[]));
     let mut _exp_stdout = exp_result.stdout_str().to_string();
 
     #[cfg(target_os = "linux")]
@@ -72,7 +31,7 @@ fn test_id_single_user() {
     let test_users = [&whoami()[..]];
 
     let scene = TestScenario::new(util_name!());
-    let mut exp_result = unwrap_or_return!(expected_result(&test_users));
+    let mut exp_result = unwrap_or_return!(expected_result(util_name!(), &test_users));
     scene
         .ucmd()
         .args(&test_users)
@@ -85,7 +44,7 @@ fn test_id_single_user() {
     for &opt in &["--user", "--group", "--groups"] {
         let mut args = vec![opt];
         args.extend_from_slice(&test_users);
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -94,7 +53,7 @@ fn test_id_single_user() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--zero");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -103,7 +62,7 @@ fn test_id_single_user() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--name");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -112,7 +71,7 @@ fn test_id_single_user() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.pop();
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -128,7 +87,7 @@ fn test_id_single_user() {
 fn test_id_single_user_non_existing() {
     let args = &["hopefully_non_existing_username"];
     let result = new_ucmd!().args(args).run();
-    let exp_result = unwrap_or_return!(expected_result(args));
+    let exp_result = unwrap_or_return!(expected_result(util_name!(), args));
 
     // It is unknown why on macOS (and possibly others?) `id` adds "Invalid argument".
     // coreutils 8.32: $ LC_ALL=C id foobar
@@ -147,7 +106,7 @@ fn test_id_name() {
     for &opt in &["--user", "--group", "--groups"] {
         let args = [opt, "--name"];
         let result = scene.ucmd().args(&args).run();
-        let exp_result = unwrap_or_return!(expected_result(&args));
+        let exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         result
             .stdout_is(exp_result.stdout_str())
             .stderr_is(exp_result.stderr_str())
@@ -166,7 +125,7 @@ fn test_id_real() {
     for &opt in &["--user", "--group", "--groups"] {
         let args = [opt, "--real"];
         let result = scene.ucmd().args(&args).run();
-        let exp_result = unwrap_or_return!(expected_result(&args));
+        let exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         result
             .stdout_is(exp_result.stdout_str())
             .stderr_is(exp_result.stderr_str())
@@ -206,21 +165,16 @@ fn test_id_password_style() {
 #[test]
 #[cfg(unix)]
 fn test_id_multiple_users() {
-    #[cfg(target_os = "linux")]
-    let util_name = util_name!();
-    #[cfg(all(unix, not(target_os = "linux")))]
-    let util_name = &format!("g{}", util_name!());
-    let version_check_string = check_coreutil_version(util_name, VERSION_MIN_MULTIPLE_USERS);
-    if version_check_string.starts_with(UUTILS_WARNING) {
-        println!("{}\ntest skipped", version_check_string);
-        return;
-    }
+    unwrap_or_return!(check_coreutil_version(
+        util_name!(),
+        VERSION_MIN_MULTIPLE_USERS
+    ));
 
     // Same typical users that GNU test suite is using.
     let test_users = ["root", "man", "postfix", "sshd", &whoami()];
 
     let scene = TestScenario::new(util_name!());
-    let mut exp_result = unwrap_or_return!(expected_result(&test_users));
+    let mut exp_result = unwrap_or_return!(expected_result(util_name!(), &test_users));
     scene
         .ucmd()
         .args(&test_users)
@@ -233,7 +187,7 @@ fn test_id_multiple_users() {
     for &opt in &["--user", "--group", "--groups"] {
         let mut args = vec![opt];
         args.extend_from_slice(&test_users);
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -242,7 +196,7 @@ fn test_id_multiple_users() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--zero");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -251,7 +205,7 @@ fn test_id_multiple_users() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--name");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -260,7 +214,7 @@ fn test_id_multiple_users() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.pop();
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -274,15 +228,10 @@ fn test_id_multiple_users() {
 #[test]
 #[cfg(unix)]
 fn test_id_multiple_users_non_existing() {
-    #[cfg(target_os = "linux")]
-    let util_name = util_name!();
-    #[cfg(all(unix, not(target_os = "linux")))]
-    let util_name = &format!("g{}", util_name!());
-    let version_check_string = check_coreutil_version(util_name, VERSION_MIN_MULTIPLE_USERS);
-    if version_check_string.starts_with(UUTILS_WARNING) {
-        println!("{}\ntest skipped", version_check_string);
-        return;
-    }
+    unwrap_or_return!(check_coreutil_version(
+        util_name!(),
+        VERSION_MIN_MULTIPLE_USERS
+    ));
 
     let test_users = [
         "root",
@@ -298,7 +247,7 @@ fn test_id_multiple_users_non_existing() {
     ];
 
     let scene = TestScenario::new(util_name!());
-    let mut exp_result = unwrap_or_return!(expected_result(&test_users));
+    let mut exp_result = unwrap_or_return!(expected_result(util_name!(), &test_users));
     scene
         .ucmd()
         .args(&test_users)
@@ -311,7 +260,7 @@ fn test_id_multiple_users_non_existing() {
     for &opt in &["--user", "--group", "--groups"] {
         let mut args = vec![opt];
         args.extend_from_slice(&test_users);
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -320,7 +269,7 @@ fn test_id_multiple_users_non_existing() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--zero");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -329,7 +278,7 @@ fn test_id_multiple_users_non_existing() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.push("--name");
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -338,7 +287,7 @@ fn test_id_multiple_users_non_existing() {
             .stderr_is(exp_result.stderr_str().replace(": Invalid argument", ""))
             .code_is(exp_result.code());
         args.pop();
-        exp_result = unwrap_or_return!(expected_result(&args));
+        exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
         scene
             .ucmd()
             .args(&args)
@@ -360,12 +309,12 @@ fn test_id_default_format() {
             .ucmd()
             .args(&args)
             .fails()
-            .stderr_only(unwrap_or_return!(expected_result(&args)).stderr_str());
+            .stderr_only(unwrap_or_return!(expected_result(util_name!(), &args)).stderr_str());
         for &opt2 in &["--user", "--group", "--groups"] {
             // u/g/G n/r
             let args = [opt2, opt1];
             let result = scene.ucmd().args(&args).run();
-            let exp_result = unwrap_or_return!(expected_result(&args));
+            let exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
             result
                 .stdout_is(exp_result.stdout_str())
                 .stderr_is(exp_result.stderr_str())
@@ -379,7 +328,7 @@ fn test_id_default_format() {
             .ucmd()
             .args(&args)
             .succeeds()
-            .stdout_only(unwrap_or_return!(expected_result(&args)).stdout_str());
+            .stdout_only(unwrap_or_return!(expected_result(util_name!(), &args)).stdout_str());
     }
 }
 
@@ -393,7 +342,7 @@ fn test_id_zero() {
             .ucmd()
             .args(&[z_flag])
             .fails()
-            .stderr_only(unwrap_or_return!(expected_result(&[z_flag])).stderr_str());
+            .stderr_only(unwrap_or_return!(expected_result(util_name!(), &[z_flag])).stderr_str());
         for &opt1 in &["--name", "--real"] {
             // id: cannot print only names or real IDs in default format
             let args = [opt1, z_flag];
@@ -401,12 +350,12 @@ fn test_id_zero() {
                 .ucmd()
                 .args(&args)
                 .fails()
-                .stderr_only(unwrap_or_return!(expected_result(&args)).stderr_str());
+                .stderr_only(unwrap_or_return!(expected_result(util_name!(), &args)).stderr_str());
             for &opt2 in &["--user", "--group", "--groups"] {
                 // u/g/G n/r z
                 let args = [opt2, z_flag, opt1];
                 let result = scene.ucmd().args(&args).run();
-                let exp_result = unwrap_or_return!(expected_result(&args));
+                let exp_result = unwrap_or_return!(expected_result(util_name!(), &args));
                 result
                     .stdout_is(exp_result.stdout_str())
                     .stderr_is(exp_result.stderr_str())
@@ -420,86 +369,7 @@ fn test_id_zero() {
                 .ucmd()
                 .args(&args)
                 .succeeds()
-                .stdout_only(unwrap_or_return!(expected_result(&args)).stdout_str());
+                .stdout_only(unwrap_or_return!(expected_result(util_name!(), &args)).stdout_str());
         }
     }
-}
-
-fn check_coreutil_version(util_name: &str, version_expected: &str) -> String {
-    // example:
-    // $ id --version | head -n 1
-    // id (GNU coreutils) 8.32.162-4eda
-    let scene = TestScenario::new(util_name);
-    let version_check = scene
-        .cmd_keepenv(&util_name)
-        .env("LC_ALL", "C")
-        .arg("--version")
-        .run();
-    version_check
-        .stdout_str()
-        .split('\n')
-        .collect::<Vec<_>>()
-        .get(0)
-        .map_or_else(
-            || format!("{}: unexpected output format for reference coreutil: '{} --version'", UUTILS_WARNING, util_name),
-            |s| {
-                if s.contains(&format!("(GNU coreutils) {}", version_expected)) {
-                    s.to_string()
-                } else if s.contains("(GNU coreutils)") {
-                    let version_found = s.split_whitespace().last().unwrap()[..4].parse::<f32>().unwrap_or_default();
-                    let version_expected = version_expected.parse::<f32>().unwrap_or_default();
-                    if version_found > version_expected {
-                    format!("{}: version for the reference coreutil '{}' is higher than expected; expected: {}, found: {}", UUTILS_INFO, util_name, version_expected, version_found)
-                    } else {
-                    format!("{}: version for the reference coreutil '{}' does not match; expected: {}, found: {}", UUTILS_WARNING, util_name, version_expected, version_found) }
-                } else {
-                    format!("{}: no coreutils version string found for reference coreutils '{} --version'", UUTILS_WARNING, util_name)
-                }
-            },
-        )
-}
-
-#[allow(clippy::needless_borrow)]
-#[cfg(unix)]
-fn expected_result(args: &[&str]) -> Result<CmdResult, String> {
-    #[cfg(target_os = "linux")]
-    let util_name = util_name!();
-    #[cfg(all(unix, not(target_os = "linux")))]
-    let util_name = &format!("g{}", util_name!());
-
-    let version_check_string = check_coreutil_version(util_name, VERSION_MIN);
-    if version_check_string.starts_with(UUTILS_WARNING) {
-        return Err(version_check_string);
-    }
-    println!("{}", version_check_string);
-
-    let scene = TestScenario::new(util_name);
-    let result = scene
-        .cmd_keepenv(util_name)
-        .env("LC_ALL", "C")
-        .args(args)
-        .run();
-
-    let (stdout, stderr): (String, String) = if cfg!(target_os = "linux") {
-        (
-            result.stdout_str().to_string(),
-            result.stderr_str().to_string(),
-        )
-    } else {
-        // strip 'g' prefix from results:
-        let from = util_name.to_string() + ":";
-        let to = &from[1..];
-        (
-            result.stdout_str().replace(&from, to),
-            result.stderr_str().replace(&from, to),
-        )
-    };
-
-    Ok(CmdResult::new(
-        Some(result.tmpd()),
-        Some(result.code()),
-        result.succeeded(),
-        stdout.as_bytes(),
-        stderr.as_bytes(),
-    ))
 }
