d4f27f1454d90de7e1484ab571c72a968fdbeb68
Sylvestre Ledru <sylvestre@debian.org>
2021-07-11 11:23:51 +0200

    rustfmt the recent change

--- src/uu/du/src/du.rs
+++ src/uu/du/src/du.rs
@@ -419,26 +419,22 @@ impl Display for DuError {
             DuError::SummarizeDepthConflict(s) => {
                 write!(f, "summarizing conflicts with --max-depth={}", s)
             }
-            DuError::InvalidTimeStyleArg(s) => {
-                write!(
-                    f,
-                    "invalid argument '{}' for 'time style'
+            DuError::InvalidTimeStyleArg(s) => write!(
+                f,
+                "invalid argument '{}' for 'time style'
 Valid arguments are:
 - 'full-iso'
 - 'long-iso'
 - 'iso'
 Try '{} --help' for more information.",
-                    s, NAME
-                )
-            }
-            DuError::InvalidTimeArg(s) => {
-                write!(
-                    f,
-                    "Invalid argument '{}' for --time.
+                s, NAME
+            ),
+            DuError::InvalidTimeArg(s) => write!(
+                f,
+                "Invalid argument '{}' for --time.
 'birth' and 'creation' arguments are not supported on this platform.",
-                    s
-                )
-            }
+                s
+            ),
         }
     }
 }
