4f0555a1164b64a114367d6d5a7d0038aabc8ce2
Sylvestre Ledru <sylvestre@debian.org>
2021-07-11 20:59:29 +0200

    update uucore_proces to 0.0.6

--- util/update-version.sh
+++ util/update-version.sh
@@ -6,6 +6,9 @@
 FROM="0.0.6"
 TO="0.0.7"
 
+UUCORE_PROCS_FROM="0.0.5"
+UUCORE_PROCS_TO="0.0.6"
+
 UUCORE_FROM="0.0.8"
 UUCORE_TO="0.0.9"
 
@@ -13,6 +16,9 @@ PROGS=$(ls -1d src/uu/*/Cargo.toml src/uu/stdbuf/src/libstdbuf/Cargo.toml Cargo.
 
 # update the version of all programs
 sed -i -e "s|version = \"$FROM\"|version = \"$TO\"|" $PROGS
+
+# Update uucore_procs
+sed -i -e "s|version = \"$UUCORE_PROCS_FROM\"|version = \"$UUCORE_PROCS_TO\"|" src/uucore_procs/Cargo.toml
 
 # Update the stdbuf stuff
 sed -i -e "s|libstdbuf = { version=\"$FROM\"|libstdbuf = { version=\"$TO\"|" src/uu/stdbuf/Cargo.toml
@@ -22,3 +28,5 @@ sed -i -e "s|= { optional=true, version=\"$FROM\", package=\"uu_|= { optional=tr
 sed -i -e "s|version = \"$UUCORE_FROM\"|version = \"$UUCORE_TO\"|" src/uucore/Cargo.toml
 # Update crates using uucore
 sed -i -e "s|uucore = { version=\">=$UUCORE_FROM\",|uucore = { version=\">=$UUCORE_TO\",|" $PROGS
+# Update crates using uucore_procs
+sed -i -e "s|uucore_procs = { version=\">=$UUCORE_PROCS_FROM\",|uucore_procs = { version=\">=$UUCORE_PROCS_TO\",|" $PROGS
