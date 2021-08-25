97cb118ae984979d44a9b237e5813efd381f73a6
Andreas Hartmann <andreas.h@rtmann.de>
2021-08-25 13:57:37 +0200

    cut: Rephrase help text
    Adapt the help text to make it comply with GNUs cut.

--- src/uu/cut/src/cut.rs
+++ src/uu/cut/src/cut.rs
@@ -27,88 +27,23 @@ use uucore::InvalidEncodingHandling;
 mod searcher;
 
 static NAME: &str = "cut";
-static SYNTAX: &str =
-    "[-d] [-s] [-z] [--output-delimiter] ((-f|-b|-c) {{sequence}}) {{sourcefile}}+";
-static SUMMARY: &str =
-    "Prints specified byte or field columns from each line of stdin or the input files";
-static LONG_HELP: &str = "
- Each call must specify a mode (what to use for columns),
- a sequence (which columns to print), and provide a data source
+static ABOUT: &str =
+    "Print selected parts of lines from each FILE to standard output.
 
- Specifying a mode
+With no FILE, or when FILE is -, read standard input.
 
-    Use --bytes (-b) or --characters (-c) to specify byte mode
+Mandatory arguments to long options are mandatory for short options too.";
+static AFTER_HELP: &str =
+"Use one, and only one of -b, -c or -f.  Each LIST is made up of one
+range, or many ranges separated by commas.  Selected input is written
+in the same order that it is read, and is written exactly once.
+Each range is one of:
 
-    Use --fields (-f) to specify field mode, where each line is broken into
-    fields identified by a delimiter character. For example for a typical CSV
-    you could use this in combination with setting comma as the delimiter
+  N     N'th byte, character or field, counted from 1
+  N-    from N'th byte, character or field, to end of line
+  N-M   from N'th to M'th (included) byte, character or field
+  -M    from first to M'th (included) byte, character or field
 
- Specifying a sequence
-
-    A sequence is a group of 1 or more numbers or inclusive ranges separated
-    by a commas.
-
-    cut -f 2,5-7 some_file.txt
-    will display the 2nd, 5th, 6th, and 7th field for each source line
-
-    Ranges can extend to the end of the row by excluding the the second number
-
-    cut -f 3- some_file.txt
-    will display the 3rd field and all fields after for each source line
-
-    The first number of a range can be excluded, and this is effectively the
-    same as using 1 as the first number: it causes the range to begin at the
-    first column. Ranges can also display a single column
-
-    cut -f 1,3-5 some_file.txt
-    will display the 1st, 3rd, 4th, and 5th field for each source line
-
-    The --complement option, when used, inverts the effect of the sequence
-
-    cut --complement -f 4-6 some_file.txt
-    will display the every field but the 4th, 5th, and 6th
-
- Specifying a data source
-
-    If no sourcefile arguments are specified, stdin is used as the source of
-    lines to print
-
-    If sourcefile arguments are specified, stdin is ignored and all files are
-    read in consecutively if a sourcefile is not successfully read, a warning
-    will print to stderr, and the eventual status code will be 1, but cut
-    will continue to read through proceeding sourcefiles
-
-    To print columns from both STDIN and a file argument, use - (dash) as a
-    sourcefile argument to represent stdin.
-
- Field Mode options
-
-    The fields in each line are identified by a delimiter (separator)
-
-    Set the delimiter
-        Set the delimiter which separates fields in the file using the
-        --delimiter (-d) option. Setting the delimiter is optional.
-        If not set, a default delimiter of Tab will be used.
-
-    Optionally Filter based on delimiter
-        If the --only-delimited (-s) flag is provided, only lines which
-        contain the delimiter will be printed
-
-    Replace the delimiter
-        If the --output-delimiter option is provided, the argument used for
-        it will replace the delimiter character in each line printed. This is
-        useful for transforming tabular data - e.g. to convert a CSV to a
-        TSV (tab-separated file)
-
- Line endings
-
-    When the --zero-terminated (-z) option is used, cut sees \\0 (null) as the
-    'line ending' character (both for the purposes of reading lines and
-    separating printed lines) instead of \\n (newline). This is useful for
-    tabular data where some of the cells may contain newlines
-
-    echo 'ab\\0cd' | cut -z -c 1
-    will result in 'a\\0c\\0'
 ";
 
 struct Options {
@@ -435,7 +370,8 @@ mod options {
     pub const ONLY_DELIMITED: &str = "only-delimited";
     pub const OUTPUT_DELIMITER: &str = "output-delimiter";
     pub const COMPLEMENT: &str = "complement";
-    pub const FILE: &str = "file";
+    pub const FILE: &str = "FILE";
+    pub const N: &str = "n";
 }
 
 #[uucore_procs::gen_uumain]
@@ -589,82 +525,83 @@ pub fn uu_app() -> App<'static, 'static> {
     App::new(uucore::util_name())
         .name(NAME)
         .version(crate_version!())
-        .usage(SYNTAX)
-        .about(SUMMARY)
-        .after_help(LONG_HELP)
+        .about(ABOUT)
+        .after_help(AFTER_HELP)
         .arg(
             Arg::with_name(options::BYTES)
                 .short("b")
                 .long(options::BYTES)
                 .takes_value(true)
-                .help("filter byte columns from the input source")
+                .help("select only these bytes")
                 .allow_hyphen_values(true)
-                .value_name("LIST")
-                .display_order(1),
+                .value_name("LIST"),
         )
         .arg(
             Arg::with_name(options::CHARACTERS)
                 .short("c")
                 .long(options::CHARACTERS)
-                .help("alias for character mode")
+                .help("select only these characters")
                 .takes_value(true)
                 .allow_hyphen_values(true)
-                .value_name("LIST")
-                .display_order(2),
+                .value_name("LIST"),
         )
         .arg(
             Arg::with_name(options::DELIMITER)
                 .short("d")
                 .long(options::DELIMITER)
-                .help("specify the delimiter character that separates fields in the input source. Defaults to Tab.")
+                .help("use DELIM instead of TAB for field delimiter")
                 .takes_value(true)
-                .value_name("DELIM")
-                .display_order(3),
+                .value_name("DELIM"),
         )
         .arg(
             Arg::with_name(options::FIELDS)
                 .short("f")
                 .long(options::FIELDS)
-                .help("filter field columns from the input source")
+                .help("select only these fields;  also print any line
+  that contains no delimiter character, unless
+  the -s option is specified")
                 .takes_value(true)
                 .allow_hyphen_values(true)
-                .value_name("LIST")
-                .display_order(4),
+                .value_name("LIST"),
+        )
+        .arg(
+            Arg::with_name(options::N)
+                .short(options::N)
+                .help("with -b: don't split multibyte characters")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name(options::COMPLEMENT)
                 .long(options::COMPLEMENT)
-                .help("invert the filter - instead of displaying only the filtered columns, display all but those columns")
-                .takes_value(false)
-                .display_order(5),
+                .help("complement the set of selected bytes, characters
+  or fields")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name(options::ONLY_DELIMITED)
-            .short("s")
+                .short("s")
                 .long(options::ONLY_DELIMITED)
-                .help("in field mode, only print lines which contain the delimiter")
-                .takes_value(false)
-                .display_order(6),
-        )
-        .arg(
-            Arg::with_name(options::ZERO_TERMINATED)
-            .short("z")
-                .long(options::ZERO_TERMINATED)
-                .help("instead of filtering columns based on line, filter columns based on \\0 (NULL character)")
-                .takes_value(false)
-                .display_order(8),
+                .help("do not print lines not containing delimiters")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name(options::OUTPUT_DELIMITER)
             .long(options::OUTPUT_DELIMITER)
-                .help("in field mode, replace the delimiter in output lines with this option's argument")
+                .help("use STRING as the output delimiter
+  the default is to use the input delimiter")
                 .takes_value(true)
-                .value_name("NEW_DELIM")
-                .display_order(7),
+                .value_name("STRING"),
+        )
+        .arg(
+            Arg::with_name(options::ZERO_TERMINATED)
+                .short("z")
+                .long(options::ZERO_TERMINATED)
+                .help("line delimiter is NUL, not newline")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name(options::FILE)
-            .hidden(true)
+                .hidden(false)
                 .multiple(true)
         )
 }
