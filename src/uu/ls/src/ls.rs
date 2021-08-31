c3809e199975792908e92f60fa35a05aaa119dc5
mtsoltan <mahmoud.t.soltan@gmail.com>
2021-08-31 10:10:24 +0200

    Fixed some documentation of display_item_long that was missed in #2623

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -1575,22 +1575,12 @@ fn display_grid(
 ///
 /// That's why we have the parameters:
 /// ```txt
-///    max_links: usize,
+///    longest_link_count_len: usize,
 ///    longest_uname_len: usize,
 ///    longest_group_len: usize,
-///    max_size: usize,
+///    longest_size_len: usize,
 /// ```
 /// that decide the maximum possible character count of each field.
-///
-/// [`get_inode`]: ls::get_inode
-/// [`display_permissions`]: ls::display_permissions
-/// [`display_symlink_count`]: ls::display_symlink_count
-/// [`display_uname`]: ls::display_uname
-/// [`display_group`]: ls::display_group
-/// [`display_size_or_rdev`]: ls::display_size_or_rdev
-/// [`get_system_time`]: ls::get_system_time
-/// [`display_file_name`]: ls::display_file_name
-/// [`pad_left`]: ls::pad_left
 fn display_item_long(
     item: &PathData,
     longest_link_count_len: usize,
