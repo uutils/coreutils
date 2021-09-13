450a3faf9fa5edf061e327acf25647cf85692616
Sylvestre Ledru <sledru@mozilla.com>
2021-09-13 12:29:28 +0200

    Add to the spell ignore list

--- tests/common/util.rs
+++ tests/common/util.rs
@@ -3,7 +3,7 @@
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
-//spell-checker: ignore (linux) rlimit prlimit Rlim coreutil
+//spell-checker: ignore (linux) rlimit prlimit Rlim coreutil ggroups
 
 #![allow(dead_code)]
 
