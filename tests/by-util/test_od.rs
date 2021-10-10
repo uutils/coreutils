51613c02eca5db5e935232b61b1a353e6f13966b
Sylvestre Ledru <sylvestre@debian.org>
2021-10-10 10:10:46 +0200

    add a word to ignore...

--- tests/by-util/test_od.rs
+++ tests/by-util/test_od.rs
@@ -1,3 +1,4 @@
+// spell-checker:ignore abcdefghijklmnopqrstuvwxyz
 //  * This file is part of the uutils coreutils package.
 //  *
 //  * For the full copyright and license information, please view the LICENSE
