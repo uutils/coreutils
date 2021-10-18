cb34b660cbba77c94c35e23b38909246f3558591
Smicry <smicry@vip.qq.com>
2021-10-19 00:15:17 +0800

    add tail usage

--- src/uu/head/src/head.rs
+++ src/uu/head/src/head.rs
@@ -19,7 +19,6 @@ const BUF_SIZE: usize = 65536;
 const ABOUT: &str = "\
                      Print the first 10 lines of each FILE to standard output.\n\
                      With more than one FILE, precede each with a header giving the file name.\n\
-                     \n\
                      With no FILE, or when FILE is -, read standard input.\n\
                      \n\
                      Mandatory arguments to long flags are mandatory for short flags too.\
