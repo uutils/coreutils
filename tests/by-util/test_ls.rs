696323c44be21b33655ba79b108e3d30a4de0f33
electricboogie <32370782+electricboogie@users.noreply.github.com>
2021-12-22 17:19:25 -0600

    Fix lint error

--- tests/by-util/test_ls.rs
+++ tests/by-util/test_ls.rs
@@ -11,8 +11,6 @@ use std::collections::HashMap;
 use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
-use std::os::unix::fs::PermissionsExt;
-
 #[cfg(not(windows))]
 extern crate libc;
 #[cfg(not(windows))]
@@ -67,7 +65,7 @@ fn test_ls_io_errors() {
         .arg("-1")
         .arg("./some-dir/no_file_here")
         .fails()
-        .stderr_contains("cannot access");;
+        .stderr_contains("cannot access");
 }
 
 #[test]
