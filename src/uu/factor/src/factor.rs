a342df03f0a2e1e3aba7ea3687efa2da9591ab0e
Greg Guthe <226605+g-k@users.noreply.github.com>
2022-01-18 08:47:39 -0500

    tests: update factor Distribution<Factors> sample to take range

--- src/uu/factor/src/factor.rs
+++ src/uu/factor/src/factor.rs
@@ -258,7 +258,7 @@ impl Distribution<Factors> for Standard {
         // See Generating Random Factored Numbers, Easily, J. Cryptology (2003)
         'attempt: loop {
             while n > 1 {
-                n = rng.gen_range(1, n);
+                n = rng.gen_range(1..n);
                 if miller_rabin::is_prime(n) {
                     if let Some(h) = g.checked_mul(n) {
                         f.push(n);
