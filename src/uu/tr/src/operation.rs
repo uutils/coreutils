6aa433c70a6080c426c021c9a739ca64fd138d41
Terts Diepraam <terts.diepraam@gmail.com>
2022-01-19 19:07:11 +0100

    tr: adapt copyright to new guidelines

--- src/uu/tr/src/operation.rs
+++ src/uu/tr/src/operation.rs
@@ -1,9 +1,5 @@
 //  * This file is part of the uutils coreutils package.
 //  *
-//  * (c) Michael Gehring <mg@ebfe.org>
-//  * (c) kwantam <kwantam@gmail.com>
-//  * (c) Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
-//  *
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
