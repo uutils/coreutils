bfc0d81481c76c1c8933f909979cc6b8352cf293
Jeffrey Finkelstein <jeffrey.finkelstein@protonmail.com>
2022-01-21 21:43:02 -0500

    ci: update default branch to "main" in workflows

--- util/compare_gnu_result.py
+++ util/compare_gnu_result.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 """
-Compare the current results to the last results gathered from the master branch to highlight
+Compare the current results to the last results gathered from the main branch to highlight
 if a PR is making the results better/worse
 """
 
@@ -10,7 +10,7 @@ import sys
 from os import environ
 
 NEW = json.load(open("gnu-result.json"))
-OLD = json.load(open("master-gnu-result.json"))
+OLD = json.load(open("main-gnu-result.json"))
 
 # Extract the specific results from the dicts
 last = OLD[list(OLD.keys())[0]]
@@ -24,7 +24,7 @@ skip_d = int(current["skip"]) - int(last["skip"])
 
 # Get an annotation to highlight changes
 print(
-    f"::warning ::Changes from master: PASS {pass_d:+d} / FAIL {fail_d:+d} / ERROR {error_d:+d} / SKIP {skip_d:+d} "
+    f"::warning ::Changes from main: PASS {pass_d:+d} / FAIL {fail_d:+d} / ERROR {error_d:+d} / SKIP {skip_d:+d} "
 )
 
 # If results are worse fail the job to draw attention
