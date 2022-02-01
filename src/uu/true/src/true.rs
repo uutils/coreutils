98f16d4d46cd5cb6519481c523665aaf1abcee58
xxyzz <gitpull@protonmail.com>
2022-02-01 10:41:44 +0800

    true: add description and version

--- src/uu/true/src/true.rs
+++ src/uu/true/src/true.rs
@@ -5,8 +5,10 @@
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
-use clap::{App, AppSettings};
+use clap::{crate_version, App, AppSettings};
 use uucore::error::UResult;
+
+static ABOUT: &str = "Return an exit status of 0.";
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
@@ -15,5 +17,8 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
 }
 
 pub fn uu_app<'a>() -> App<'a> {
-    App::new(uucore::util_name()).setting(AppSettings::InferLongArgs)
+    App::new(uucore::util_name())
+        .about(ABOUT)
+        .version(crate_version!())
+        .setting(AppSettings::InferLongArgs)
 }
