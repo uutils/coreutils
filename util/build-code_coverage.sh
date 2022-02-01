d762bebc1c1a16c3fbb0d8fef024d4d995f9db51
Linux User <user@DESKTOP-LVO3GEU.localdomain>
2022-02-01 06:55:11 +0000

    update shell scripts according to shellcheck recommendations and minor cleanup

--- util/build-code_coverage.sh
+++ util/build-code_coverage.sh
@@ -8,12 +8,13 @@
 
 FEATURES_OPTION="--features feat_os_unix"
 
-ME_dir="$(dirname -- $(readlink -fm -- "$0"))"
+ME_dir="$(dirname -- "$(readlink -fm -- "$0")")"
 REPO_main_dir="$(dirname -- "${ME_dir}")"
 
-cd "${REPO_main_dir}"
+cd "${REPO_main_dir}" &&
 echo "[ \"$PWD\" ]"
 
+#shellcheck disable=SC2086
 UTIL_LIST=$("${ME_dir}"/show-utils.sh ${FEATURES_OPTION})
 CARGO_INDIVIDUAL_PACKAGE_OPTIONS=""
 for UTIL in ${UTIL_LIST}; do
@@ -30,10 +31,12 @@ export RUSTC_WRAPPER=""     ## NOTE: RUSTC_WRAPPER=='sccache' breaks code covera
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
 export RUSTUP_TOOLCHAIN="nightly-gnu"
-cargo build ${FEATURES_OPTION}
-cargo test --no-run ${FEATURES_OPTION}
-cargo test --quiet ${FEATURES_OPTION}
-cargo test --quiet ${FEATURES_OPTION} ${CARGO_INDIVIDUAL_PACKAGE_OPTIONS}
+#shellcheck disable=SC2086
+{   cargo build ${FEATURES_OPTION}
+    cargo test --no-run ${FEATURES_OPTION}
+    cargo test --quiet ${FEATURES_OPTION}
+    cargo test --quiet ${FEATURES_OPTION} ${CARGO_INDIVIDUAL_PACKAGE_OPTIONS}
+}
 
 export COVERAGE_REPORT_DIR
 if [ -z "${COVERAGE_REPORT_DIR}" ]; then COVERAGE_REPORT_DIR="${REPO_main_dir}/target/debug/coverage-nix"; fi
@@ -47,8 +50,7 @@ mkdir -p "${COVERAGE_REPORT_DIR}"
 grcov . --output-type lcov --output-path "${COVERAGE_REPORT_DIR}/../lcov.info" --branch --ignore build.rs --ignore '/*' --ignore '[A-Za-z]:/*' --ignore 'C:/Users/*' --excl-br-line '^\s*((debug_)?assert(_eq|_ne)?!|#\[derive\()'
 # * build HTML
 # -- use `genhtml` if available for display of additional branch coverage information
-genhtml --version 2>/dev/null 1>&2
-if [ $? -eq 0 ]; then
+if genhtml --version 2>/dev/null 1>&2; then
     genhtml "${COVERAGE_REPORT_DIR}/../lcov.info" --output-directory "${COVERAGE_REPORT_DIR}" --branch-coverage --function-coverage | grep ": [0-9]"
 else
     grcov . --output-type html --output-path "${COVERAGE_REPORT_DIR}" --branch --ignore build.rs --ignore '/*' --ignore '[A-Za-z]:/*' --ignore 'C:/Users/*' --excl-br-line '^\s*((debug_)?assert(_eq|_ne)?!|#\[derive\()'
--- util/run-gnu-test.sh
+++ util/run-gnu-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # spell-checker:ignore (env/vars) BUILDDIR GNULIB SUBDIRS
-cd "$(dirname "${BASH_SOURCE[0]}")/../.."
+cd "$(dirname -- "$(readlink -fm -- "$0")")/../.."
 set -e
 BUILDDIR="${PWD}/uutils/target/release"
 GNULIB_DIR="${PWD}/gnulib"
@@ -13,4 +13,5 @@ if test -n "$1"; then
     export RUN_TEST="TESTS=$1"
 fi
 
+#shellcheck disable=SC2086
 timeout -sKILL 2h make -j "$(nproc)" check $RUN_TEST SUBDIRS=. RUN_EXPENSIVE_TESTS=yes RUN_VERY_EXPENSIVE_TESTS=yes VERBOSE=no || : # Kill after 4 hours in case something gets stuck in make
