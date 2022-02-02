49b9d95d11f78a38f020e28d90fbbd8b986473a2
353fc443 aka Seagull <85840256+353fc443@users.noreply.github.com>
2022-02-02 21:49:34 +0530

    remove test

--- tests/by-util/test_tac.rs
+++ tests/by-util/test_tac.rs
@@ -269,8 +269,3 @@ fn test_regex_before() {
         //          |--------||----||---|
         .stdout_is("+---+c+d-e+--++b+-+a+");
 }
-
-#[test]
-fn closed_stdin() {
-    new_ucmd!().close_stdin().fails();
-}
