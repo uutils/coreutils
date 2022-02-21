36255257eac2823d12b056e6595d7ddaf35d1de2
Devin Gunay <devingunay@gmail.com>
2022-02-20 16:24:26 -0800

    touch: appease spellchecker

--- src/uu/touch/src/touch.rs
+++ src/uu/touch/src/touch.rs
@@ -6,7 +6,7 @@
 // For the full copyright and license information, please view the LICENSE file
 // that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) filetime strptime utcoff strs datetime MMDDhhmm clapv PWSTR lpszfilepath
+// spell-checker:ignore (ToDO) filetime strptime utcoff strs datetime MMDDhhmm clapv PWSTR lpszfilepath hresult
 
 pub extern crate filetime;
 
