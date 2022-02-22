163472ff7bd16567ebf2d891f36b75c96f3f7b9b
Alex Tibbles <alex@bleg.org>
2022-02-21 20:43:42 -0500

    hashsum: pass spell checker

--- src/uu/hashsum/src/digest.rs
+++ src/uu/hashsum/src/digest.rs
@@ -83,7 +83,7 @@ impl Digest for blake3::Hasher {
 }
 
 // Implements the Digest trait for sha2 / sha3 algorithms with fixed output
-macro_rules! impl_digest_rustcryptocommon {
+macro_rules! impl_digest_common {
     ($type: ty, $size: expr) => {
         impl Digest for $type {
             fn new() -> Self {
@@ -136,17 +136,17 @@ macro_rules! impl_digest_shake {
     };
 }
 
-impl_digest_rustcryptocommon!(md5::Md5, 128);
-impl_digest_rustcryptocommon!(sha1::Sha1, 160);
-impl_digest_rustcryptocommon!(sha2::Sha224, 224);
-impl_digest_rustcryptocommon!(sha2::Sha256, 256);
-impl_digest_rustcryptocommon!(sha2::Sha384, 384);
-impl_digest_rustcryptocommon!(sha2::Sha512, 512);
+impl_digest_common!(md5::Md5, 128);
+impl_digest_common!(sha1::Sha1, 160);
+impl_digest_common!(sha2::Sha224, 224);
+impl_digest_common!(sha2::Sha256, 256);
+impl_digest_common!(sha2::Sha384, 384);
+impl_digest_common!(sha2::Sha512, 512);
 
-impl_digest_rustcryptocommon!(sha3::Sha3_224, 224);
-impl_digest_rustcryptocommon!(sha3::Sha3_256, 256);
-impl_digest_rustcryptocommon!(sha3::Sha3_384, 384);
-impl_digest_rustcryptocommon!(sha3::Sha3_512, 512);
+impl_digest_common!(sha3::Sha3_224, 224);
+impl_digest_common!(sha3::Sha3_256, 256);
+impl_digest_common!(sha3::Sha3_384, 384);
+impl_digest_common!(sha3::Sha3_512, 512);
 impl_digest_shake!(sha3::Shake128);
 impl_digest_shake!(sha3::Shake256);
 
