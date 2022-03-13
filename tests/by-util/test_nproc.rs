24b4af768c98616908c4757a2a30d7bc2b9a4ebc
Sylvestre Ledru <sylvestre@debian.org>
2022-03-13 18:13:48 +0100

    nproc: Improve the support of OMP_NUM_THREADS

--- tests/by-util/test_nproc.rs
+++ tests/by-util/test_nproc.rs
@@ -1,3 +1,4 @@
+// spell-checker:ignore incorrectnumber
 use crate::common::util::*;
 
 #[test]
@@ -15,16 +16,24 @@ fn test_nproc_all_omp() {
 
     let result = TestScenario::new(util_name!())
         .ucmd_keepenv()
-        .env("OMP_NUM_THREADS", "1")
+        .env("OMP_NUM_THREADS", "60")
         .succeeds();
 
     let nproc_omp: u8 = result.stdout_str().trim().parse().unwrap();
-    assert!(nproc - 1 == nproc_omp);
+    assert!(nproc_omp == 60);
 
     let result = TestScenario::new(util_name!())
         .ucmd_keepenv()
         .env("OMP_NUM_THREADS", "1") // Has no effect
         .arg("--all")
+        .succeeds();
+    let nproc_omp: u8 = result.stdout_str().trim().parse().unwrap();
+    assert!(nproc == nproc_omp);
+
+    // If the parsing fails, returns the number of CPU
+    let result = TestScenario::new(util_name!())
+        .ucmd_keepenv()
+        .env("OMP_NUM_THREADS", "incorrectnumber") // returns the number CPU
         .succeeds();
     let nproc_omp: u8 = result.stdout_str().trim().parse().unwrap();
     assert!(nproc == nproc_omp);
@@ -51,4 +60,15 @@ fn test_nproc_ignore() {
         let nproc: u8 = result.stdout_str().trim().parse().unwrap();
         assert!(nproc_total - 1 == nproc);
     }
+}
+
+#[test]
+fn test_nproc_ignore_all_omp() {
+    let result = TestScenario::new(util_name!())
+        .ucmd_keepenv()
+        .env("OMP_NUM_THREADS", "42")
+        .arg("--ignore=40")
+        .succeeds();
+    let nproc: u8 = result.stdout_str().trim().parse().unwrap();
+    assert!(nproc == 2);
 }
