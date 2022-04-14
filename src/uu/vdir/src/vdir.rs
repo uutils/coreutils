d0cbf76be2ef89e3975766d2dc2c454235905204
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-04-14 06:50:21 -0700

    add vdir

---
+++ src/uu/vdir/src/vdir.rs
@@ -0,0 +1,25 @@
+// * This file is part of the uutils coreutils package.
+// *
+// * (c) gmnsii <gmnsii@protonmail.com>
+// *
+// * For the full copyright and license information, please view the LICENSE file
+// * that was distributed with this source code.
+
+use clap::Command;
+use uucore::error::UResult;
+
+#[uucore::main]
+pub fn uumain(args: impl uucore::Args) -> UResult<()> {
+    uu_ls::vdir_main(args)
+}
+
+// Coreutils won't compile if not every util have an uu_app function.
+// However, we can't  put it here since it needs to be in the same place as
+// the entry point for the vdir util, which is in ls/ls.rs. We could put the
+// entry point here, but we would need a lot of refactoring and to make a lot of
+// ls functions and structs public.
+//
+// To make our life easier, we use this dummy function.
+pub fn uu_app<'a>() -> Command<'a> {
+    Command::new(uucore::util_name())
+}
