62d96db16b278b6bd088cff943116fec46cae029
Terts Diepraam <terts.diepraam@gmail.com>
2022-04-17 21:33:51 +0200

    docs: don't download the tldr archive (#3415)
    The ureq dependency is causing compilation errors on various platforms (see #3184, #3216, #3375). Hence we remove that dependency and do not automatically download the archive anymore. Instead, we ask the user to download it separately when the archive is not found.

--- src/bin/uudoc.rs
+++ src/bin/uudoc.rs
@@ -8,21 +8,23 @@ use clap::Command;
 use std::collections::HashMap;
 use std::ffi::OsString;
 use std::fs::File;
-use std::io::Cursor;
 use std::io::{self, Read, Seek, Write};
 use zip::ZipArchive;
 
 include!(concat!(env!("OUT_DIR"), "/uutils_map.rs"));
 
 fn main() -> io::Result<()> {
-    println!("Downloading tldr archive");
-    let mut zip_reader = ureq::get("https://tldr.sh/assets/tldr.zip")
-        .call()
-        .unwrap()
-        .into_reader();
-    let mut buffer = Vec::new();
-    zip_reader.read_to_end(&mut buffer).unwrap();
-    let mut tldr_zip = ZipArchive::new(Cursor::new(buffer)).unwrap();
+    let mut tldr_zip = File::open("docs/tldr.zip")
+        .ok()
+        .and_then(|f| ZipArchive::new(f).ok());
+
+    if tldr_zip.is_none() {
+        println!("Warning: No tldr archive found, so the documentation will not include examples.");
+        println!("To include examples in the documentation, download the tldr archive and put it in the docs/ folder.");
+        println!();
+        println!("  curl https://tldr.sh/assets/tldr.zip -o docs/tldr.zip");
+        println!();
+    }
 
     let utils = util_map::<Box<dyn Iterator<Item = OsString>>>();
     match std::fs::create_dir("docs/src/utils/") {
@@ -109,7 +111,7 @@ struct MDWriter<'a, 'b> {
     w: Box<dyn Write>,
     command: Command<'a>,
     name: &'a str,
-    tldr_zip: &'b mut ZipArchive<Cursor<Vec<u8>>>,
+    tldr_zip: &'b mut Option<ZipArchive<File>>,
     utils_per_platform: &'b HashMap<&'b str, Vec<String>>,
 }
 
@@ -189,42 +191,43 @@ impl<'a, 'b> MDWriter<'a, 'b> {
     }
 
     fn examples(&mut self) -> io::Result<()> {
-        let content = if let Some(f) =
-            get_zip_content(self.tldr_zip, &format!("pages/common/{}.md", self.name))
-        {
-            f
-        } else if let Some(f) =
-            get_zip_content(self.tldr_zip, &format!("pages/linux/{}.md", self.name))
-        {
-            f
-        } else {
-            return Ok(());
-        };
-
-        writeln!(self.w, "## Examples")?;
-        writeln!(self.w)?;
-        for line in content.lines().skip_while(|l| !l.starts_with('-')) {
-            if let Some(l) = line.strip_prefix("- ") {
-                writeln!(self.w, "{}", l)?;
-            } else if line.starts_with('`') {
-                writeln!(self.w, "```shell\n{}\n```", line.trim_matches('`'))?;
-            } else if line.is_empty() {
-                writeln!(self.w)?;
+        if let Some(zip) = self.tldr_zip {
+            let content = if let Some(f) =
+                get_zip_content(zip, &format!("pages/common/{}.md", self.name))
+            {
+                f
+            } else if let Some(f) = get_zip_content(zip, &format!("pages/linux/{}.md", self.name)) {
+                f
             } else {
-                println!("Not sure what to do with this line:");
-                println!("{}", line);
+                return Ok(());
+            };
+
+            writeln!(self.w, "## Examples")?;
+            writeln!(self.w)?;
+            for line in content.lines().skip_while(|l| !l.starts_with('-')) {
+                if let Some(l) = line.strip_prefix("- ") {
+                    writeln!(self.w, "{}", l)?;
+                } else if line.starts_with('`') {
+                    writeln!(self.w, "```shell\n{}\n```", line.trim_matches('`'))?;
+                } else if line.is_empty() {
+                    writeln!(self.w)?;
+                } else {
+                    println!("Not sure what to do with this line:");
+                    println!("{}", line);
+                }
             }
+            writeln!(self.w)?;
+            writeln!(
+                self.w,
+                "> The examples are provided by the [tldr-pages project](https://tldr.sh) under the [CC BY 4.0 License](https://github.com/tldr-pages/tldr/blob/main/LICENSE.md)."
+            )?;
+            writeln!(self.w, ">")?;
+            writeln!(
+                self.w,
+                "> Please note that, as uutils is a work in progress, some examples might fail."
+            )?;
         }
-        writeln!(self.w)?;
-        writeln!(
-            self.w,
-            "> The examples are provided by the [tldr-pages project](https://tldr.sh) under the [CC BY 4.0 License](https://github.com/tldr-pages/tldr/blob/main/LICENSE.md)."
-        )?;
-        writeln!(self.w, ">")?;
-        writeln!(
-            self.w,
-            "> Please note that, as uutils is a work in progress, some examples might fail."
-        )
+        Ok(())
     }
 
     fn options(&mut self) -> io::Result<()> {
