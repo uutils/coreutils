3078ca8346fd15b89aab74916c048da877c33dfd
Sylvestre Ledru <sledru@mozilla.com>
2022-05-01 20:18:33 +0200

    Add CHARCLASS to the spell ignore

--- src/uu/ptx/src/ptx.rs
+++ src/uu/ptx/src/ptx.rs
@@ -5,7 +5,7 @@
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
-// spell-checker:ignore (ToDOs) corasick memchr Roff trunc oset iset
+// spell-checker:ignore (ToDOs) corasick memchr Roff trunc oset iset CHARCLASS
 
 use clap::{crate_version, Arg, Command};
 use regex::Regex;
