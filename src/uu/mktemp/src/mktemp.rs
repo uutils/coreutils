c8748022854904833a65939e4860a7eeed10fc42
Sylvestre Ledru <sledru@mozilla.com>
2022-06-05 09:32:24 +0200

    Add a word to the spell ignore list

--- src/uu/mktemp/src/mktemp.rs
+++ src/uu/mktemp/src/mktemp.rs
@@ -6,7 +6,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (paths) GPGHome
+// spell-checker:ignore (paths) GPGHome findxs
 
 use clap::{crate_version, Arg, ArgMatches, Command};
 use uucore::display::{println_verbatim, Quotable};
