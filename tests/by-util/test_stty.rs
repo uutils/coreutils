1288cb844ba64f06486f84444bdd19e30a91c951
Terts Diepraam <terts.diepraam@gmail.com>
2022-06-25 22:37:28 +0200

    add some simple tests to stty (still failing)

---
+++ tests/by-util/test_stty.rs
@@ -0,0 +1,18 @@
+use crate::common::util::*;
+
+#[test]
+fn runs() {
+    new_ucmd!().succeeds();
+}
+
+#[test]
+fn print_all() {
+    let res = new_ucmd!().succeeds();
+
+    // Random selection of flags to check for
+    for flag in [
+        "parenb", "parmrk", "ixany", "iuclc", "onlcr", "ofdel", "icanon", "noflsh",
+    ] {
+        res.stdout_contains(flag);
+    }
+}
