48440b2506c6d9ece2432cfa52e83bd809c5e620
Sam Nystrom <82178396+Stonks3141@users.noreply.github.com>
2022-07-23 07:10:30 -0400

    tests/ls: update spell ignore

--- tests/by-util/test_ls.rs
+++ tests/by-util/test_ls.rs
@@ -1,4 +1,4 @@
-// spell-checker:ignore (words) READMECAREFULLY birthtime doesntexist oneline somebackup lrwx somefile somegroup somehiddenbackup somehiddenfile tabsize
+// spell-checker:ignore (words) READMECAREFULLY birthtime doesntexist oneline somebackup lrwx somefile somegroup somehiddenbackup somehiddenfile tabsize aaaaaaaa bbbb cccc dddddddd
 
 #[cfg(not(windows))]
 extern crate libc;
