33e8c2a61c49b5f01157e35eb7c0f71212cc8efa
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-06 22:06:10 +0300

    tail: fix clippy advice

--- src/uu/tail/src/tail.rs
+++ src/uu/tail/src/tail.rs
@@ -787,28 +787,26 @@ fn follow(files: &mut FileHandling, settings: &mut Settings) -> UResult<()> {
                     } else {
                         // nothing changed
                     }
-                } else {
-                    if let Some(old_md) = &data.metadata {
-                        if !old_md.is_tailable() {
-                            show_error!("{} has become accessible", display_name.quote());
-                            files.update_reader(path)?;
-                        } else if data.reader.is_none() {
-                            show_error!(
-                                "{} has appeared;  following new file",
-                                display_name.quote()
-                            );
-                            files.update_reader(path)?;
-                        } else if !old_md.file_id_eq(&new_md) {
-                            show_error!(
-                                "{} has been replaced;  following new file",
-                                display_name.quote()
-                            );
-                            files.update_reader(path)?;
-                        }
-                    } else {
-                        show_error!("{} has appeared;  following new file", display_name.quote());
+                } else if let Some(old_md) = &data.metadata {
+                    if !old_md.is_tailable() {
+                        show_error!("{} has become accessible", display_name.quote());
+                        files.update_reader(path)?;
+                    } else if data.reader.is_none() {
+                        show_error!(
+                            "{} has appeared;  following new file",
+                            display_name.quote()
+                        );
+                        files.update_reader(path)?;
+                    } else if !old_md.file_id_eq(&new_md) {
+                        show_error!(
+                            "{} has been replaced;  following new file",
+                            display_name.quote()
+                        );
                         files.update_reader(path)?;
                     }
+                } else {
+                    show_error!("{} has appeared;  following new file", display_name.quote());
+                    files.update_reader(path)?;
                 }
                 files.update_metadata(path, Some(new_md));
                 files.tail_file(path, settings.verbose)?;
