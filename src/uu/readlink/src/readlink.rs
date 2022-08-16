9c97b700ecd7b51dcd2b658a6f291ef1498cb4a6
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-16 08:25:02 +0300

    readlink: reformat using rustfmt

--- src/uu/readlink/src/readlink.rs
+++ src/uu/readlink/src/readlink.rs
@@ -79,7 +79,9 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
             canonicalize(&p, can_mode, res_mode)
         };
         match path_result {
-            Ok(path) => show(&path, no_trailing_delimiter, use_zero).map_err_context(String::new)?,
+            Ok(path) => {
+                show(&path, no_trailing_delimiter, use_zero).map_err_context(String::new)?
+            }
             Err(err) => {
                 if verbose {
                     return Err(USimpleError::new(
