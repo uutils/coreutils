9796e70a286cea0df080d440c10cedc649b76ec4
Joining7943 <111500881+Joining7943@users.noreply.github.com>
2023-01-23 21:13:59 +0100

    uucore: Fix #4298: Build on s390x and riscv64 architectures fails

--- src/uucore/src/lib/features/fs.rs
+++ src/uucore/src/lib/features/fs.rs
@@ -115,6 +115,7 @@ impl FileInformation {
             not(target_os = "android"),
             not(target_os = "freebsd"),
             not(target_arch = "aarch64"),
+            not(target_arch = "riscv64"),
             target_pointer_width = "64"
         ))]
         return self.0.st_nlink;
@@ -125,6 +126,7 @@ impl FileInformation {
                 target_os = "android",
                 target_os = "freebsd",
                 target_arch = "aarch64",
+                target_arch = "riscv64",
                 not(target_pointer_width = "64")
             )
         ))]
