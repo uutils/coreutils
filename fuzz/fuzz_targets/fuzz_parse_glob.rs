ef0b177e1872d36852d70ab0b5e528e27c9be871
Sylvestre Ledru <sylvestre@debian.org>
2023-03-07 23:24:13 +0100

    add more fuzzers Many thanks to Jemma Nelson @fwip

---
+++ fuzz/fuzz_targets/fuzz_parse_glob.rs
@@ -0,0 +1,12 @@
+// spell-checker:ignore libfuzzer
+
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use uucore::parse_glob;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        _ = parse_glob::from_str(s)
+    }
+});
