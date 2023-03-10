9e0d3ea698bb97eec6ff2576897c4330459c1942
Terts Diepraam <terts.diepraam@gmail.com>
2023-03-11 00:01:22 +0100

    shred: add "prefill" to spelling list

--- src/uu/shred/src/shred.rs
+++ src/uu/shred/src/shred.rs
@@ -6,7 +6,7 @@
 // * For the full copyright and license information, please view the LICENSE
 // * file that was distributed with this source code.
 
-// spell-checker:ignore (words) wipesync
+// spell-checker:ignore (words) wipesync prefill
 
 use clap::{crate_version, Arg, ArgAction, Command};
 use rand::{rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
