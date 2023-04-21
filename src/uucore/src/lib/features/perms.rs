2b8b82f420ea83f24c4e1372525f31ea8968155d
Moïse Valvassori <moise.valvassori@gmail.com>
2023-04-21 12:30:45 +0200

    print messages to stdout

--- src/uucore/src/lib/features/perms.rs
+++ src/uucore/src/lib/features/perms.rs
@@ -265,7 +265,7 @@ impl ChownExecutor {
                 // the `--from` args.
                 if self.dest_gid.is_none() {
                     let uid = meta.uid();
-                    show_error!(
+                    println!(
                         "ownership of {} retained as {}",
                         path.quote(),
                         entries::uid2usr(uid).unwrap_or_else(|_| uid.to_string()),
@@ -273,7 +273,7 @@ impl ChownExecutor {
                 } else {
                     let uid = meta.uid();
                     let gid = meta.gid();
-                    show_error!(
+                    println!(
                         "ownership of {} retained as {}:{}",
                         path.quote(),
                         entries::uid2usr(uid).unwrap_or_else(|_| uid.to_string()),
