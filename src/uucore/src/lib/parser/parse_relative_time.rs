d771ac4cb762b8bb79a3bb98675bfa2a8e2adfb9
Sylvestre Ledru <sylvestre@debian.org>
2023-04-22 21:11:33 +0200

    parse_relative_time::from_str: document the function

--- src/uucore/src/lib/parser/parse_relative_time.rs
+++ src/uucore/src/lib/parser/parse_relative_time.rs
@@ -6,6 +6,51 @@
 #[cfg(feature = "time")]
 use time::Duration;
 
+/// Parses a relative time string and returns a `Duration` representing the
+/// relative time.
+///
+/// # Arguments
+///
+/// * `s` - A string slice representing the relative time.
+///
+/// # Examples
+///
+/// ```
+/// use time::Duration;
+/// let duration = parse_relative_time("+3 days");
+/// assert_eq!(duration, Some(Duration::days(3)));
+/// ```
+///
+/// # Supported formats
+///
+/// The function supports the following formats for relative time:
+///
+/// * [num] [unit] (e.g., "-1 hour", "+3 days")
+/// * [unit] (e.g., "hour", "day")
+/// * "now" or "today"
+/// * "yesterday"
+/// * "tomorrow"
+///
+/// [num] can be a positive or negative integer.
+/// [unit] can be one of the following: "fortnight", "week", "day", "hour",
+/// "minute", "min", "second", "sec" and their plural forms.
+///
+/// # Unsupported formats
+///
+/// The function currently does not support the following formats:
+///
+/// * "year" and "month"
+/// * Times without spaces like "-1hour"
+///
+/// # Returns
+///
+/// * `Some(Duration)` - If the input string can be parsed as a relative time
+/// * `None` - If the input string cannot be parsed as a relative time
+///
+/// # Errors
+///
+/// This function will return `None` if the input string cannot be parsed as a
+/// relative time.
 #[cfg(feature = "time")]
 pub fn from_str(s: &str) -> Option<Duration> {
     // Relative time, like "-1 hour" or "+3 days".
