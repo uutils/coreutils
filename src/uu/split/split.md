0b84aa52f327b4290296ef63724bfc868e30417e
m11o <m11o@hey.jp>
2023-04-25 22:26:47 +0900

    fix check-spell

--- src/uu/split/split.md
+++ src/uu/split/split.md
@@ -1,3 +1,5 @@
+<!-- spell-checker:ignore PREFI -->
+
 # split
 
 ```
--- src/uu/split/src/split.rs
+++ src/uu/split/src/split.rs
@@ -5,7 +5,7 @@
 //  * For the full copyright and license information, please view the LICENSE
 //  * file that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) PREFIXaa PREFIXab nbbbb ncccc
+// spell-checker:ignore nbbbb ncccc
 
 mod filenames;
 mod number;
