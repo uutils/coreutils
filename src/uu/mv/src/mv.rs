ea2b725abbac4a3108612c87b424a6ed15cb7284
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2023-05-05 15:16:41 +0200

    mv: fix help output

--- src/uu/mv/src/mv.rs
+++ src/uu/mv/src/mv.rs
@@ -70,7 +70,7 @@ static ARG_FILES: &str = "files";
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
-    let mut app = uu_app().after_help(backup_control::BACKUP_CONTROL_LONG_HELP);
+    let mut app = uu_app();
     let matches = app.try_get_matches_from_mut(args)?;
 
     if !matches.contains_id(OPT_TARGET_DIRECTORY)
@@ -138,7 +138,10 @@ pub fn uu_app() -> Command {
         .version(crate_version!())
         .about(ABOUT)
         .override_usage(format_usage(USAGE))
-        .after_help(AFTER_HELP)
+        .after_help(format!(
+            "{AFTER_HELP}\n\n{}",
+            backup_control::BACKUP_CONTROL_LONG_HELP
+        ))
         .infer_long_args(true)
         .arg(
             Arg::new(OPT_FORCE)
