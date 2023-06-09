516311f830d7c13a83045166595ad2b660a355c4
Alexander Kunde <a.kunde@outlook.com>
2023-06-09 15:12:25 +0000

    spellcheck

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -5,7 +5,7 @@
 // For the full copyright and license information, please view the LICENSE file
 // that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) cpio svgz webm somegroup nlink rmvb xspf tabsize dired colorterm
+// spell-checker:ignore (ToDO) cpio svgz webm somegroup nlink rmvb xspf tabsize dired colorterm dircolor
 
 use clap::{
     builder::{NonEmptyStringValueParser, ValueParser},
