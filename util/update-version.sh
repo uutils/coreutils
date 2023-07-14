74530c0f51f9d6e2685610ca7d237f34accd45f2
Sylvestre Ledru <sylvestre@debian.org>
2023-07-14 13:04:17 +0200

    Update the version to 0.0.20

--- util/update-version.sh
+++ util/update-version.sh
@@ -14,8 +14,8 @@
 # 7) Run util/publish.sh --do-it
 # 8) In some cases, you might have to fix dependencies and run import
 
-FROM="0.0.18"
-TO="0.0.19"
+FROM="0.0.19"
+TO="0.0.20"
 
 PROGS=$(ls -1d src/uu/*/Cargo.toml src/uu/stdbuf/src/libstdbuf/Cargo.toml src/uucore/Cargo.toml Cargo.toml)
 
