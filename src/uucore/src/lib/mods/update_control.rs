892ad4b42afa906687b992bd5d5203866515b977
Sylvestre Ledru <sylvestre@debian.org>
2023-08-21 12:23:18 +0200

    Remove the author copyright notices (#5184)
    * Remove the author copyright notices
    
    Rational:
    * not maintained
    * does not reflect reality
    * don't provide any value (the info can be found in the git log)
    * we don't have rules to update them
      (ex: should you update it after one line, two lines, etc)
    
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- src/uucore/src/lib/mods/update_control.rs
+++ src/uucore/src/lib/mods/update_control.rs
@@ -1,7 +1,5 @@
 // This file is part of the uutils coreutils package.
 //
-// (c) John Shin <shinhs0506@gmail.com>
-//
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
