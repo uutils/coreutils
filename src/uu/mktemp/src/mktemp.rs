7b1c724558916b74f99cf57a52ea3b13c4cc2c86
Sylvestre Ledru <sylvestre@debian.org>
2023-08-21 11:20:09 +0200

    Remove another copyright notice
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- src/uu/mktemp/src/mktemp.rs
+++ src/uu/mktemp/src/mktemp.rs
@@ -1,6 +1,5 @@
 // This file is part of the uutils coreutils package.
 //
-// Collaborator: Jian Zeng
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
