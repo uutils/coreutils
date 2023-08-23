7677199dc4652b545def9dd684743dc0e105ff13
tommady <tommady@users.noreply.github.com>
2023-08-23 11:40:15 +0000

    correct the original test_fmt_width test case

--- tests/by-util/test_fmt.rs
+++ tests/by-util/test_fmt.rs
@@ -29,7 +29,7 @@ fn test_fmt_width() {
         new_ucmd!()
             .args(&["one-word-per-line.txt", param, "10"])
             .succeeds()
-            .stdout_is("this is\na file\nwith one\nword per\nline\n");
+            .stdout_is("this is a\nfile with\none word\nper line\n");
     }
 }
 
