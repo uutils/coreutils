2816df78093315a7de577bd4564719d2b15d2784
Sylvestre Ledru <sylvestre@debian.org>
2023-09-18 15:15:53 +0200

    remove duplication of the spell ignore
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- src/uu/ls/src/dired.rs
+++ src/uu/ls/src/dired.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore dired subdired dired
+// spell-checker:ignore dired subdired
 
 use crate::Config;
 use std::fmt;
