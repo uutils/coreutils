48769c1f28b57d95a1bec5b058fd65c17aa767bc
Sylvestre Ledru <sylvestre@debian.org>
2023-09-18 14:51:43 +0200

    Remove a word from the spell ignore
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) cpio svgz webm somegroup nlink rmvb xspf tabsize dired subdired dired
+// spell-checker:ignore (ToDO) cpio svgz webm somegroup nlink rmvb xspf tabsize dired subdired
 
 use clap::{
     builder::{NonEmptyStringValueParser, ValueParser},
