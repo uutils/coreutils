d01fe6a10f4e325de8b3c5bf4fdd5c221b47ca03
Rojin Raju <46747837+rojin254@users.noreply.github.com>
2023-09-19 01:17:02 +0530

    ls.rs: rename variable name dfn to displayed_file
    Closes: #5282

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -2482,13 +2482,13 @@ fn display_item_long(
             }
         };
 
-        let dfn = display_file_name(item, config, None, String::new(), out).contents;
+        let displayed_file = display_file_name(item, config, None, String::new(), out).contents;
 
         write!(
             out,
             " {} {}{}",
             display_date(md, config),
-            dfn,
+            displayed_file,
             config.line_ending
         )?;
     } else {
@@ -2561,7 +2561,7 @@ fn display_item_long(
             write!(out, " {}", pad_right("?", padding.uname))?;
         }
 
-        let dfn = display_file_name(item, config, None, String::new(), out).contents;
+        let displayed_file = display_file_name(item, config, None, String::new(), out).contents;
         let date_len = 12;
 
         writeln!(
@@ -2569,7 +2569,7 @@ fn display_item_long(
             " {} {} {}",
             pad_left("?", padding.size),
             pad_left("?", date_len),
-            dfn,
+            displayed_file,
         )?;
     }
 
