1e153a811d5e9092694501c1fde02283a792d0c4
PThorpe92 <preston@unlockedlabs.org>
2023-09-30 11:59:26 -0400

    docs(mv): add link to issue documenting unstable error message

--- src/uu/mv/src/mv.rs
+++ src/uu/mv/src/mv.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) sourcepath targetpath
+// spell-checker:ignore (ToDO) sourcepath targetpath ENOTEMPTY  EEXIST  EISDIR  ENOSPC  EMLINK  ETXTBSY
 
 mod error;
 
@@ -23,7 +23,7 @@ use uucore::backup_control::{self, source_is_target_backup, BackupMode};
 use uucore::display::Quotable;
 use uucore::error::{set_exit_code, FromIo, UError, UResult, USimpleError, UUsageError};
 use uucore::fs::{are_hardlinks_or_one_way_symlink_to_same_file, are_hardlinks_to_same_file};
-use uucore::libc::{EDQUOT, EEXIST, EISDIR, EMLINK, ENOSPC, ENOTEMPTY, ETXTBSY};
+use uucore::libc;
 use uucore::update_control::{self, UpdateMode};
 use uucore::{format_usage, help_about, help_section, help_usage, prompt_yes, show};
 
@@ -401,7 +401,14 @@ fn move_files_into_dir(files: &[PathBuf], target_dir: &Path, b: &Behavior) -> UR
             Err(e) if e.to_string().is_empty() => set_exit_code(1),
             Err(e) => {
                 match e.raw_os_error() {
-                    Some(ENOTEMPTY | EEXIST | EDQUOT | EISDIR | ENOSPC | EMLINK | ETXTBSY) => {
+                    Some(
+                        libc::ENOTEMPTY
+                        | libc::EEXIST
+                        | libc::EISDIR
+                        | libc::ENOSPC
+                        | libc::EMLINK
+                        | libc::ETXTBSY,
+                    ) => {
                         // The error message was changed to match GNU's decision
                         // when an issue was filed. These will match when merged upstream.
                         let e = e
@@ -495,7 +502,8 @@ fn rename(
                 fs::remove_dir(to)?;
             } else {
                 // TODO: change when 'io::Error::DirectoryNotEmpty' is stabilized.
-                return Err(io::Error::from_raw_os_error(ENOTEMPTY));
+                // https://github.com/rust-lang/rust/issues/86442
+                return Err(io::Error::from_raw_os_error(libc::ENOTEMPTY));
             }
         }
     }
