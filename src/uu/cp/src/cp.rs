cdde57608c028a8a678a4b55464589a299bcc4d0
tommady <tommady@users.noreply.github.com>
2023-10-04 08:06:30 +0000

    fix macos mode_t is u16 but other unix platforms are u32

--- src/uu/cp/src/cp.rs
+++ src/uu/cp/src/cp.rs
@@ -1755,8 +1755,16 @@ fn copy_file(
                 use libc::{
                     S_IRGRP, S_IROTH, S_IRUSR, S_IRWXG, S_IRWXO, S_IRWXU, S_IWGRP, S_IWOTH, S_IWUSR,
                 };
+
+                #[cfg(not(macos))]
+                const MODE_RW_UGO: u32 = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+                #[cfg(not(macos))]
+                const S_IRWXUGO: u32 = S_IRWXU | S_IRWXG | S_IRWXO;
+
+                #[cfg(macos)]
                 const MODE_RW_UGO: u32 =
                     (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH) as u32;
+                #[cfg(macos)]
                 const S_IRWXUGO: u32 = (S_IRWXU | S_IRWXG | S_IRWXO) as u32;
 
                 match is_explicit_no_preserve_mode {
