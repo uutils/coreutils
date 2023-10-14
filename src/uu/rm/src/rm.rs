226680aa001457140985412adbdd5778d3dddc6e
Sylvestre Ledru <sylvestre@debian.org>
2023-10-14 13:45:29 +0200

    Ignore inacc spell

--- src/uu/rm/src/rm.rs
+++ src/uu/rm/src/rm.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (path) eacces
+// spell-checker:ignore (path) eacces inacc
 
 use clap::{builder::ValueParser, crate_version, parser::ValueSource, Arg, ArgAction, Command};
 use std::collections::VecDeque;
