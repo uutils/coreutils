f63f9a06f62340ded772fac93e732af80e534208
Zhuoxun Yang <Zhuoxun.Yang777@outlook.com>
2023-10-18 22:50:54 +0800

    tests/pathchk: test empty path

--- tests/by-util/test_pathchk.rs
+++ tests/by-util/test_pathchk.rs
@@ -19,8 +19,10 @@ fn test_default_mode() {
     // accept non-portable chars
     new_ucmd!().args(&["dir#/$file"]).succeeds().no_stdout();
 
-    // accept empty path
-    new_ucmd!().args(&[""]).succeeds().no_stdout();
+    // fail on empty path
+    new_ucmd!().args(&[""]).fails().no_stdout();
+
+    new_ucmd!().args(&["", ""]).fails().no_stdout();
 
     // fail on long path
     new_ucmd!()
