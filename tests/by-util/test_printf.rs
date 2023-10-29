5e2bad413f4a80c7ed834fff479aec635a4f03b3
Zhuoxun Yang <Zhuoxun.Yang777@outlook.com>
2023-10-29 21:26:11 +0800

    tests/printf: support %q

--- tests/by-util/test_printf.rs
+++ tests/by-util/test_printf.rs
@@ -121,6 +121,22 @@ fn sub_b_string_ignore_subs() {
 }
 
 #[test]
+fn sub_q_string_non_printable() {
+    new_ucmd!()
+        .args(&["non-printable: %q", "\"$test\""])
+        .succeeds()
+        .stdout_only("non-printable: \\\"\\$test\\\"");
+}
+
+#[test]
+fn sub_q_string_more_non_printable() {
+    new_ucmd!()
+        .args(&["non-printable: %q", "[]{}<>"])
+        .succeeds()
+        .stdout_only("non-printable: \\[\\]\\{\\}\\<\\>");
+}
+
+#[test]
 fn sub_char() {
     new_ucmd!()
         .args(&["the letter %c", "A"])
