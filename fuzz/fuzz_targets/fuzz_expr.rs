a0a209f65fe5560283ef3f45e71ed7f37e073c57
Sylvestre Ledru <sylvestre@debian.org>
2023-11-21 11:20:20 +0100

    add index too
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- fuzz/fuzz_targets/fuzz_expr.rs
+++ fuzz/fuzz_targets/fuzz_expr.rs
@@ -22,7 +22,7 @@ static CMD_PATH: &str = "expr";
 fn generate_expr(max_depth: u32) -> String {
     let mut rng = rand::thread_rng();
     let ops = [
-        "+", "-", "*", "/", "%", "<", ">", "=", "&", "|", "!=", "<=", ">=", ":", "length", "substr",
+        "+", "-", "*", "/", "%", "<", ">", "=", "&", "|", "!=", "<=", ">=", ":", "index", "length", "substr",
     ];
 
     let mut expr = String::new();
