687dc5dd14addf895f68dfd3c5c4d26fb2e69207
Laurent Cheylus <foxy@free.fr>
2023-12-03 09:44:19 +0100

    nohup: fix for OpenBSD with _vprocmgr_detach_from_console function
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- src/uu/nohup/src/nohup.rs
+++ src/uu/nohup/src/nohup.rs
@@ -200,7 +200,12 @@ extern "C" {
     fn _vprocmgr_detach_from_console(flags: u32) -> *const libc::c_int;
 }
 
-#[cfg(any(target_os = "linux", target_os = "android", target_os = "freebsd"))]
+#[cfg(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "freebsd",
+    target_os = "openbsd"
+))]
 unsafe fn _vprocmgr_detach_from_console(_: u32) -> *const libc::c_int {
     std::ptr::null()
 }
