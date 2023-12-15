3a7a3bf63941492c7885ba0287c78854a6950c12
Arpit Bhadauria <singharpit511@gmail.com>
2023-12-15 16:44:31 +0530

    fmt: remove crash! macro (#5589)
    * fmt: remove crash! macro
    
    * Fix styling in fmt
    
    * Revert "Fix styling in fmt"
    
    This reverts commit 002e02f50c5bfebfc69fb1acb86d39a36d96d97d.
    
    * Revert "fmt: remove crash! macro"
    
    This reverts commit d65a3f85a1840d0c1d2e32f927e951a62258370f.
    
    * Replace crash! with unreachable! macro
    
    * Remove crash! import
    
    * Remove unreachable! from fmt
    
    * keep the helpful comment
    
    * Fix lint and format issues
    
    * review fixes

--- src/uu/fmt/src/linebreak.rs
+++ src/uu/fmt/src/linebreak.rs
@@ -8,8 +8,6 @@
 use std::io::{BufWriter, Stdout, Write};
 use std::{cmp, i64, mem};
 
-use uucore::crash;
-
 use crate::parasplit::{ParaWords, Paragraph, WordInfo};
 use crate::FmtOptions;
 
@@ -363,28 +361,26 @@ fn find_kp_breakpoints<'a, T: Iterator<Item = &'a WordInfo<'a>>>(
 }
 
 fn build_best_path<'a>(paths: &[LineBreak<'a>], active: &[usize]) -> Vec<(&'a WordInfo<'a>, bool)> {
-    let mut breakwords = vec![];
     // of the active paths, we select the one with the fewest demerits
-    let mut best_idx = match active.iter().min_by_key(|&&a| paths[a].demerits) {
-        None => crash!(
-            1,
-            "Failed to find a k-p linebreak solution. This should never happen."
-        ),
-        Some(&s) => s,
-    };
-
-    // now, chase the pointers back through the break list, recording
-    // the words at which we should break
-    loop {
-        let next_best = &paths[best_idx];
-        match next_best.linebreak {
-            None => return breakwords,
-            Some(prev) => {
-                breakwords.push((prev, next_best.break_before));
-                best_idx = next_best.prev;
+    active
+        .iter()
+        .min_by_key(|&&a| paths[a].demerits)
+        .map(|&(mut best_idx)| {
+            let mut breakwords = vec![];
+            // now, chase the pointers back through the break list, recording
+            // the words at which we should break
+            loop {
+                let next_best = &paths[best_idx];
+                match next_best.linebreak {
+                    None => return breakwords,
+                    Some(prev) => {
+                        breakwords.push((prev, next_best.break_before));
+                        best_idx = next_best.prev;
+                    }
+                }
             }
-        }
-    }
+        })
+        .unwrap_or_default()
 }
 
 // "infinite" badness is more like (1+BAD_INFTY)^2 because of how demerits are computed
