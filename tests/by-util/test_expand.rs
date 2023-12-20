734b7337e2e910cedf6366b8cf39eedfc5a503c6
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2023-12-20 23:21:13 +0200

    fix duplicate arguments

--- tests/by-util/test_expand.rs
+++ tests/by-util/test_expand.rs
@@ -392,3 +392,17 @@ fn test_comma_with_plus_4() {
         //          01234567890
         .stdout_is("a  b    c");
 }
+
+#[test]
+fn test_args_override() {
+    new_ucmd!()
+        .args(&["-i", "-i", "alice_in_wonderland.txt"])
+        .run()
+        .stdout_is(
+            "Alice was beginning to get very tired of sitting by\n\
+        her sister on the bank, and of having nothing to do: once or twice\n\
+        she had peeped into the book her sister was reading, but it had no\n\
+        pictures or conversations in it, \"and what is the use of a book,\"\n\
+        thought Alice \"without pictures or conversation?\"\n",
+        );
+}
