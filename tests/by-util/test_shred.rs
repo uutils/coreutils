971a4b04d46fc8699d8a0509684d60f59c3c0a0b
Kostiantyn Hryshchuk <statheres@gmail.com>
2024-01-05 16:38:52 +0100

    added 'wipesync' to spell-checker:ignore

--- tests/by-util/test_shred.rs
+++ tests/by-util/test_shred.rs
@@ -2,6 +2,9 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+
+// spell-checker:ignore wipesync
+
 use crate::common::util::TestScenario;
 
 #[test]
