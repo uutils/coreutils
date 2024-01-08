a5026afce6233fd4793a3f6ca5d7f0372c92dee4
Sylvestre Ledru <sylvestre@debian.org>
2024-01-09 00:28:30 +0100

    ls: if acl are used, show the + in the perms
    Tested by tests/mkdir/p-acl.sh

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) somegroup nlink tabsize dired subdired dtype colorterm
+// spell-checker:ignore (ToDO) somegroup nlink tabsize dired subdired dtype colorterm getxattr
 
 use clap::{
     builder::{NonEmptyStringValueParser, ValueParser},
@@ -36,6 +36,7 @@ use std::{
 };
 use term_grid::{Cell, Direction, Filling, Grid, GridOptions};
 use unicode_width::UnicodeWidthStr;
+
 #[cfg(any(
     target_os = "linux",
     target_os = "macos",
@@ -2620,6 +2621,18 @@ fn display_grid(
     Ok(())
 }
 
+#[cfg(all(unix, not(any(target_os = "android", target_os = "macos"))))]
+fn file_has_acl<P: AsRef<Path>>(file: P) -> bool {
+    // don't use exacl here, it is doing more getxattr call then needed
+    match xattr::list(file) {
+        Ok(acl) => {
+            // if we have extra attributes, we have an acl
+            acl.count() > 0
+        }
+        Err(_) => false,
+    }
+}
+
 /// This writes to the BufWriter out a single string of the output of `ls -l`.
 ///
 /// It writes the following keys, in order:
@@ -2663,14 +2676,25 @@ fn display_item_long(
         output_display += "  ";
     }
     if let Some(md) = item.get_metadata(out) {
+        #[cfg(any(not(unix), target_os = "android", target_os = "macos"))]
+        // TODO: See how Mac should work here
+        let is_acl_set = false;
+        #[cfg(all(unix, not(any(target_os = "android", target_os = "macos"))))]
+        let is_acl_set = file_has_acl(item.display_name.as_os_str());
         write!(
             output_display,
-            "{}{} {}",
+            "{}{}{} {}",
             display_permissions(md, true),
             if item.security_context.len() > 1 {
                 // GNU `ls` uses a "." character to indicate a file with a security context,
                 // but not other alternate access method.
                 "."
+            } else {
+                ""
+            },
+            if is_acl_set {
+                // if acl has been set, we display a "+" at the end of the file permissions
+                "+"
             } else {
                 ""
             },
