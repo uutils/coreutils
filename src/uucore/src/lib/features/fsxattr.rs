3872aca9c66a34ea99566e011c1828e6c0ff20be
Sylvestre Ledru <sylvestre@debian.org>
2024-01-14 21:39:59 +0100

    spell: ignore getxattr

--- src/uucore/src/lib/features/fsxattr.rs
+++ src/uucore/src/lib/features/fsxattr.rs
@@ -3,6 +3,8 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+// spell-checker:ignore getxattr
+
 //! Set of functions to manage xattr on files and dirs
 use std::collections::HashMap;
 use std::ffi::OsString;
