a1c1cbe148a8cd735df4b6ba9f8a6292ddc3c66e
Sylvestre Ledru <sylvestre@debian.org>
2024-01-14 17:10:18 +0100

    Update tests/by-util/test_cp.rs
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -3738,7 +3738,7 @@ fn test_acl_preserve() {
     // calling the command directly. xattr requires some dev packages to be installed
     // and it adds a complex dependency just for a test
     match Command::new("setfacl")
-        .args(["-m", "group::rwx", &file])
+        .args(["-m", "group::rwx", &path1])
         .status()
         .map(|status| status.code())
     {
