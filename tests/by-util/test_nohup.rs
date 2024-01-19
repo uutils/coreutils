f95ab692b974ce496ecae846a3a1386dfa368544
Ulrich Hornung <hornunguli@gmx.de>
2024-01-19 21:57:24 +0100

    fix: some OSes return Ok() when reading from closed pty

--- tests/by-util/test_nohup.rs
+++ tests/by-util/test_nohup.rs
@@ -5,7 +5,7 @@
 // spell-checker:ignore winsize Openpty openpty xpixel ypixel
 use crate::common::util::TestScenario;
 use nix;
-use std::thread::sleep;
+use std::{os::fd::OwnedFd, thread::sleep};
 
 // General observation: nohup.out will not be created in tests run by cargo test
 // because stdin/stdout is not attached to a TTY.
@@ -34,6 +34,26 @@ fn test_nohup_multiple_args_and_flags() {
     assert!(at.file_exists("file2"));
 }
 
+#[cfg(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "freebsd",
+    target_vendor = "apple"
+))]
+fn read_string_from_pty(pty_fd: OwnedFd, buffer_out: &mut String) {
+    use std::io::Read;
+    let result = std::fs::File::from(pty_fd).read_to_string(buffer_out);
+    match result {
+        Ok(_) => {}
+        // Input/output error (os error 5) is returned due to pipe closes. Buffer gets content anyway.
+        Err(e) if e.raw_os_error().unwrap_or_default() == 5 => {}
+        Err(e) => {
+            eprintln!("Unexpected error: {:?}", e);
+            assert!(false);
+        }
+    }
+}
+
 #[test]
 #[cfg(any(
     target_os = "linux",
@@ -44,10 +64,6 @@ fn test_nohup_multiple_args_and_flags() {
 fn test_nohup_with_pseudo_terminal_emulation_on_stdin_stdout_stderr_get_replaced() {
     use libc::winsize;
     use nix::pty::OpenptyResult;
-    use std::{
-        fs::{self, File},
-        io::Read,
-    };
 
     let ts = TestScenario::new(util_name!());
 
@@ -79,17 +95,11 @@ fn test_nohup_with_pseudo_terminal_emulation_on_stdin_stdout_stderr_get_replaced
         .succeeds();
 
     let mut buffer_stdout = String::new();
-    // Input/output error (os error 5) is returned due to pipe closes. Buffer gets content anyway.
-    File::from(po_master)
-        .read_to_string(&mut buffer_stdout)
-        .unwrap_err();
+    read_string_from_pty(po_master, &mut buffer_stdout);
     assert_eq!(buffer_stdout, "");
 
     let mut buffer_stderr = String::new();
-    // Input/output error (os error 5) is returned due to pipe closes. Buffer gets content anyway.
-    File::from(pe_master)
-        .read_to_string(&mut buffer_stderr)
-        .unwrap_err();
+    read_string_from_pty(pe_master, &mut buffer_stderr);
     assert_eq!(
         buffer_stderr.trim(),
         "nohup: ignoring input and appending output to 'nohup.out'"
@@ -99,7 +109,7 @@ fn test_nohup_with_pseudo_terminal_emulation_on_stdin_stdout_stderr_get_replaced
 
     // this proves that nohup was exchanging the stdio file descriptors
     assert_eq!(
-        fs::read_to_string(ts.fixtures.plus_as_string("nohup.out")).unwrap(),
+        std::fs::read_to_string(ts.fixtures.plus_as_string("nohup.out")).unwrap(),
         "stdin is not atty\nstdout is not atty\nstderr is not atty\n"
     );
 }
