5d359783a00818bd61a09daedd0342755f89ee83
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2024-02-29 15:27:11 +0100

    uucore/format: add padlen to spell-checker:ignore

--- src/uucore/src/lib/features/format/spec.rs
+++ src/uucore/src/lib/features/format/spec.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (vars) intmax ptrdiff
+// spell-checker:ignore (vars) intmax ptrdiff padlen
 
 use crate::quoting_style::{escape_name, QuotingStyle};
 
