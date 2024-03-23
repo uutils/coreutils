0fcc273b1dad2690834b43fcd98bb828e19ba376
Ulrich Hornung <hornunguli@gmx.de>
2024-03-23 21:05:36 +0100

    adapt performance benchmark. reduce to factor function

--- tests/benches/factor/benches/table.rs
+++ tests/benches/factor/benches/table.rs
@@ -4,7 +4,10 @@
 // file that was distributed with this source code.
 use array_init::array_init;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use uu_factor::{table::*, Factors};
+use std::{collections::BTreeMap, mem};
+use num_bigint::BigUint;
+use num_traits::FromPrimitive;
+
 
 fn table(c: &mut Criterion) {
     #[cfg(target_os = "linux")]
@@ -26,21 +29,13 @@ fn table(c: &mut Criterion) {
     group.throughput(Throughput::Elements(INPUT_SIZE as _));
     for a in inputs.take(10) {
         let a_str = format!("{:?}", a);
-        group.bench_with_input(BenchmarkId::new("factor_chunk", &a_str), &a, |b, &a| {
-            b.iter(|| {
-                let mut n_s = a;
-                let mut f_s: [_; INPUT_SIZE] = array_init(|_| Factors::one());
-                for (n_s, f_s) in n_s.chunks_mut(CHUNK_SIZE).zip(f_s.chunks_mut(CHUNK_SIZE)) {
-                    factor_chunk(n_s.try_into().unwrap(), f_s.try_into().unwrap());
-                }
-            });
-        });
         group.bench_with_input(BenchmarkId::new("factor", &a_str), &a, |b, &a| {
             b.iter(|| {
                 let mut n_s = a;
-                let mut f_s: [_; INPUT_SIZE] = array_init(|_| Factors::one());
-                for (n, f) in n_s.iter_mut().zip(f_s.iter_mut()) {
-                    factor(n, f);
+                let mut f_s: [BTreeMap<BigUint, usize>; INPUT_SIZE] = array_init(|_| BTreeMap::new());
+                for (&mut n, f) in n_s.iter_mut().zip(f_s.iter_mut()) {
+                    let r: BTreeMap<BigUint, usize> = num_prime::nt_funcs::factorize(BigUint::from_u64(n).unwrap());
+                    _ = mem::replace(f, r);
                 }
             });
         });
