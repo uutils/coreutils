343076c588e81755e6f8fbd8726e01706856a762
Krysztal Huang <krysztal.huang@outlook.com>
2024-06-19 23:30:26 +0800

    misc: Add spell-checker ignore for `process.rs`

--- src/uucore/src/lib/features/process.rs
+++ src/uucore/src/lib/features/process.rs
@@ -3,8 +3,9 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (vars) cvar exitstatus
+// spell-checker:ignore (vars) cvar exitstatus cmdline kworker
 // spell-checker:ignore (sys/unix) WIFSIGNALED
+// spell-checker:ignore pgrep pwait snice
 
 //! Set of functions to manage IDs
 //!
