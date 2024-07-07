fb48e7d280f277c04d305e9d116c1830b329e85a
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2024-07-07 12:20:41 +0200

    Fix spelling, update comments, and add documentation for improved code clarity
    fixes #5066

--- src/uucore/src/lib/lib.rs
+++ src/uucore/src/lib/lib.rs
@@ -2,7 +2,8 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// library ~ (core/bundler file)
+//! library ~ (core/bundler file)
+// #![deny(missing_docs)] //TODO: enable this
 
 // * feature-gated external crates (re-shared as public internal modules)
 #[cfg(feature = "libc")]
