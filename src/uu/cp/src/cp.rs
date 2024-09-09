5b1d4d9bdce4a075ff1cd064481831a6959a7c52
LoricAndre <loric.andre@pm.me>
2024-09-10 00:12:59 +0200

    fix: add arg count for cp

--- src/uu/cp/src/cp.rs
+++ src/uu/cp/src/cp.rs
@@ -677,6 +677,8 @@ pub fn uu_app() -> Command {
         .arg(
             Arg::new(options::PATHS)
                 .action(ArgAction::Append)
+                .num_args(1..)
+                .required(true)
                 .value_hint(clap::ValueHint::AnyPath)
                 .value_parser(ValueParser::os_string()),
         )
