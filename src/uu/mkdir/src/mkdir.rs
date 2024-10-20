80b63931a4bdc4334c0ed64eb85361355b710b19
Andrew Liebenow <andrewliebenow@gmail.com>
2024-10-20 10:45:46 -0500

    mkdir: emit error when path is empty

--- src/uu/mkdir/src/mkdir.rs
+++ src/uu/mkdir/src/mkdir.rs
@@ -160,6 +160,13 @@ fn exec(dirs: ValuesRef<OsString>, recursive: bool, mode: u32, verbose: bool) ->
 /// To match the GNU behavior, a path with the last directory being a single dot
 /// (like `some/path/to/.`) is created (with the dot stripped).
 pub fn mkdir(path: &Path, recursive: bool, mode: u32, verbose: bool) -> UResult<()> {
+    if path.as_os_str().is_empty() {
+        return Err(USimpleError::new(
+            1,
+            "cannot create directory '': No such file or directory".to_owned(),
+        ));
+    }
+
     // Special case to match GNU's behavior:
     // mkdir -p foo/. should work and just create foo/
     // std::fs::create_dir("foo/."); fails in pure Rust
