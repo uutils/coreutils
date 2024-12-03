67b92a8e6e2345464a89c7fe3e040e4c84d3bdb3
Sylvestre Ledru <sylvestre@debian.org>
2024-12-03 22:34:14 +0100

    clippy: fix clippy warnings
    See:
    https://rust-lang.github.io/rust-clippy/master/index.html#map_unwrap_or

--- src/uu/split/src/split.rs
+++ src/uu/split/src/split.rs
@@ -492,7 +492,7 @@ impl Settings {
                 }
                 match first.as_str() {
                     "\\0" => b'\0',
-                    s if s.as_bytes().len() == 1 => s.as_bytes()[0],
+                    s if s.len() == 1 => s.as_bytes()[0],
                     s => return Err(SettingsError::MultiCharacterSeparator(s.to_string())),
                 }
             }
