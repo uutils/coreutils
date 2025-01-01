c19fbb90e71c9d4fa667d10cfdde5765061deebe
Sylvestre Ledru <sylvestre@debian.org>
2025-01-01 17:24:53 +0100

    printf: Improve support for printing multi-byte values of characters

--- tests/by-util/test_printf.rs
+++ tests/by-util/test_printf.rs
@@ -963,3 +963,13 @@ fn float_switch_switch_decimal_scientific() {
         .succeeds()
         .stdout_only("1e-05");
 }
+
+#[test]
+fn mb_input() {
+    for format in ["\"á", "\'á"] {
+        new_ucmd!()
+            .args(&["%04x\n", format])
+            .succeeds()
+            .stdout_only("00e1\n");
+    }
+}
