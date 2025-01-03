77fadc6897e66f7f8722e3b0684c39ae94957b8c
Daringcuteseal <daringcuteseal@gmail.com>
2025-01-03 19:04:06 +0700

    cp: implement copying content from streams
    Previously, the only stream that cp supports copying are FIFOs. Any other files will be handled using
    std::io::copy which does not handle special files like block devices or character specials. This
    commit fixes the previously missing support by using std::io::copy or splice syscall for Linux to
    handle those cases.

--- src/uu/cp/src/platform/mod.rs
+++ src/uu/cp/src/platform/mod.rs
@@ -2,6 +2,18 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+
+#[cfg(all(
+    unix,
+    not(any(target_os = "macos", target_os = "linux", target_os = "android"))
+))]
+mod other_unix;
+#[cfg(all(
+    unix,
+    not(any(target_os = "macos", target_os = "linux", target_os = "android"))
+))]
+pub(crate) use self::other_unix::copy_on_write;
+
 #[cfg(target_os = "macos")]
 mod macos;
 #[cfg(target_os = "macos")]
@@ -12,7 +24,13 @@ mod linux;
 #[cfg(any(target_os = "linux", target_os = "android"))]
 pub(crate) use self::linux::copy_on_write;
 
-#[cfg(not(any(target_os = "linux", target_os = "android", target_os = "macos")))]
+#[cfg(not(any(
+    unix,
+    any(target_os = "macos", target_os = "linux", target_os = "android")
+)))]
 mod other;
-#[cfg(not(any(target_os = "linux", target_os = "android", target_os = "macos")))]
+#[cfg(not(any(
+    unix,
+    any(target_os = "macos", target_os = "linux", target_os = "android")
+)))]
 pub(crate) use self::other::copy_on_write;
