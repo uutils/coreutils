900a63a061e9d1263549da2a94ff5732faf8daef
Sylvestre Ledru <sylvestre@debian.org>
2025-03-15 13:41:08 +0100

    adjust the script to handle selinux

--- util/analyze-gnu-results.sh
+++ util/analyze-gnu-results.sh
@@ -6,20 +6,34 @@ set -e
 # to present the merge results
 # this script will export the values in the term
 
-if test $# -ne 2; then
+if test $# -ne 4; then
     echo "syntax:"
-    echo "$0 testsuite.log root-testsuite.log"
+    echo "$0 testsuite.log root-testsuite.log selinux-testsuite.log selinux-root-testsuite.log"
+    exit 1
 fi
 
 SUITE_LOG_FILE=$1
 ROOT_SUITE_LOG_FILE=$2
+SELINUX_SUITE_LOG_FILE=$3
+SELINUX_ROOT_SUITE_LOG_FILE=$4
 
 if test ! -f "${SUITE_LOG_FILE}"; then
     echo "${SUITE_LOG_FILE} has not been found"
     exit 1
 fi
+
 if test ! -f "${ROOT_SUITE_LOG_FILE}"; then
     echo "${ROOT_SUITE_LOG_FILE} has not been found"
+    exit 1
+fi
+
+if test ! -f "${SELINUX_SUITE_LOG_FILE}"; then
+    echo "${SELINUX_SUITE_LOG_FILE} has not been found"
+    exit 1
+fi
+
+if test ! -f "${SELINUX_ROOT_SUITE_LOG_FILE}"; then
+    echo "${SELINUX_ROOT_SUITE_LOG_FILE} has not been found"
     exit 1
 fi
 
@@ -35,24 +49,38 @@ function get_pass {
     # In the normal run, they are SKIP
     NON_ROOT=$(sed -n "s/.*# PASS: \(.*\)/\1/p" "${SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     AS_ROOT=$(sed -n "s/.*# PASS: \(.*\)/\1/p" "${ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_NON_ROOT=$(sed -n "s/.*# PASS: \(.*\)/\1/p" "${SELINUX_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_AS_ROOT=$(sed -n "s/.*# PASS: \(.*\)/\1/p" "${SELINUX_ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     echo $((NON_ROOT + AS_ROOT))
 }
 
 function get_skip {
-    # As some of the tests executed as root as still SKIP (ex: selinux), we
-    # need to some maths:
-    # Number of tests skip as user - total test as root + skipped as root
+    # Calculate skips accounting for all test runs
+    # Number of tests skip as user - total test as root + skipped as root - total selinux + skipped selinux - total selinux_root + skipped selinux_root
     TOTAL_AS_ROOT=$(sed -n "s/.*# TOTAL: \(.*\)/\1/p" "${ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     NON_ROOT=$(sed -n "s/.*# SKIP: \(.*\)/\1/p" "${SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     AS_ROOT=$(sed -n "s/.*# SKIP: \(.*\)/\1/p" "${ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
-    echo $((NON_ROOT - TOTAL_AS_ROOT + AS_ROOT))
+
+    TOTAL_SELINUX=$(sed -n "s/.*# TOTAL: \(.*\)/\1/p" "${SELINUX_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX=$(sed -n "s/.*# SKIP: \(.*\)/\1/p" "${SELINUX_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+
+    TOTAL_SELINUX_ROOT=$(sed -n "s/.*# TOTAL: \(.*\)/\1/p" "${SELINUX_ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_ROOT=$(sed -n "s/.*# SKIP: \(.*\)/\1/p" "${SELINUX_ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+
+    ADJUSTED_SKIP=$((NON_ROOT - TOTAL_AS_ROOT + AS_ROOT - TOTAL_SELINUX + SELINUX - TOTAL_SELINUX_ROOT + SELINUX_ROOT))
+    if [[ $ADJUSTED_SKIP -lt 0 ]]; then
+        ADJUSTED_SKIP=0
+    fi
+    echo $ADJUSTED_SKIP
 }
 
 function get_fail {
     # They used to be SKIP, now they fail (this is a good news)
     NON_ROOT=$(sed -n "s/.*# FAIL: \(.*\)/\1/p" "${SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     AS_ROOT=$(sed -n "s/.*# FAIL: \(.*\)/\1/p" "${ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
-    echo $((NON_ROOT + AS_ROOT))
+    SELINUX_NON_ROOT=$(sed -n "s/.*# FAIL: \(.*\)/\1/p" "${SELINUX_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_AS_ROOT=$(sed -n "s/.*# FAIL: \(.*\)/\1/p" "${SELINUX_ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    echo $((NON_ROOT + AS_ROOT + SELINUX_NON_ROOT + SELINUX_AS_ROOT))
 }
 
 function get_xpass {
@@ -64,6 +92,8 @@ function get_error {
     # They used to be SKIP, now they error (this is a good news)
     NON_ROOT=$(sed -n "s/.*# ERROR: \(.*\)/\1/p" "${SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     AS_ROOT=$(sed -n "s/.*# ERROR:: \(.*\)/\1/p" "${ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_NON_ROOT=$(sed -n "s/.*# ERROR: \(.*\)/\1/p" "${SELINUX_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
+    SELINUX_AS_ROOT=$(sed -n "s/.*# ERROR:: \(.*\)/\1/p" "${SELINUX_ROOT_SUITE_LOG_FILE}" | tr -d '\r' | head -n1)
     echo $((NON_ROOT + AS_ROOT))
 }
 
