9b16feac4e03a6f61b22dfb7bd395b2e89ed258e
Solomon Victorino <git@solomonvictorino.com>
2025-03-22 18:18:15 -0600

    tac: move to thiserror

--- src/uu/tac/src/error.rs
+++ src/uu/tac/src/error.rs
@@ -3,58 +3,42 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 //! Errors returned by tac during processing of a file.
-use std::error::Error;
-use std::fmt::Display;
+use thiserror::Error;
 
 use uucore::display::Quotable;
 use uucore::error::UError;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum TacError {
     /// A regular expression given by the user is invalid.
+    #[error("invalid regular expression: {0}")]
     InvalidRegex(regex::Error),
 
     /// An argument to tac is invalid.
+    #[error("{}: read error: Invalid argument", _0.maybe_quote())]
     InvalidArgument(String),
 
     /// The specified file is not found on the filesystem.
+    #[error("failed to open {} for reading: No such file or directory", _0.quote())]
     FileNotFound(String),
 
     /// An error reading the contents of a file or stdin.
     ///
     /// The parameters are the name of the file and the underlying
     /// [`std::io::Error`] that caused this error.
+    #[error("failed to read from {0}: {1}")]
     ReadError(String, std::io::Error),
 
     /// An error writing the (reversed) contents of a file or stdin.
     ///
     /// The parameter is the underlying [`std::io::Error`] that caused
     /// this error.
+    #[error("failed to write to stdout: {0}")]
     WriteError(std::io::Error),
 }
 
 impl UError for TacError {
     fn code(&self) -> i32 {
         1
-    }
-}
-
-impl Error for TacError {}
-
-impl Display for TacError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::InvalidRegex(e) => write!(f, "invalid regular expression: {e}"),
-            Self::InvalidArgument(s) => {
-                write!(f, "{}: read error: Invalid argument", s.maybe_quote())
-            }
-            Self::FileNotFound(s) => write!(
-                f,
-                "failed to open {} for reading: No such file or directory",
-                s.quote()
-            ),
-            Self::ReadError(s, e) => write!(f, "failed to read from {s}: {e}"),
-            Self::WriteError(e) => write!(f, "failed to write to stdout: {e}"),
-        }
     }
 }
