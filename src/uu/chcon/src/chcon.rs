59396e32bc3d20917a2cdebdb048024fde6e3d9e
Nicolas Boichat <nicolas@boichat.ch>
2025-03-25 19:51:27 +0100

    chcon/runcon: Only build on Linux
    chcon/runcon rely on the selinux crate, that is empty on
    non-Linux platforms.
    
    This doesn't matter for normal builds that use the default features
    for the platform (explicitly trying to build them will fail though).
    
    This is a problem when using `cargo test --workspace` though,
    as that tries to build all packages, including uu_chcon/uu_runcon.
    
    Just prevent compilation of these source files when target_os != linux.

--- src/uu/chcon/src/chcon.rs
+++ src/uu/chcon/src/chcon.rs
@@ -3,6 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 // spell-checker:ignore (vars) RFILE
+#![cfg(target_os = "linux")]
 #![allow(clippy::upper_case_acronyms)]
 
 use clap::builder::ValueParser;
