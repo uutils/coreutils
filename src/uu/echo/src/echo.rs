uutils/coreutils

commit fcf00a02a6df95221340dc1ea8b42f217d48af9f

echo: fix duplication of `--` arguments
Fixes #7558.

Previously, a workaround for handling double hyphens had the erroneous side
effect of echoing `--` twice in a row if the `--` is the first double hyphen to
be passed on the argument list and it is also not the first argument to `echo`.
This commit fixes the aforementioned issue.

src/uu/echo/src/echo.rs
@@ -29,8 +29,8 @@ fn handle_double_hyphens(args: impl uucore::Args) -> impl uucore::Args {
     let mut result = Vec::new();
     let mut is_first_double_hyphen = true;
 
-    for arg in args {
-        if arg == "--" && is_first_double_hyphen {
+    for (i, arg) in args.enumerate() {
+        if i < 2 && arg == "--" && is_first_double_hyphen {
             result.push(OsString::from("--"));
             is_first_double_hyphen = false;
         }

commit 98252b47ef9bf077ea574b8d8f794401f5806b0e

echo: fix omission of double-hyphen arguments
Fixes a bug introduced by fcf00a02a6df95221340dc1ea8b42f217d48af9f
as mentioned by @cakebaker.

`echo -n -e --` no longer eats the double hyphen.

However, this commit also reduces the double-hyphen duplication fix that
fcf00a02a6df95221340dc1ea8b42f217d48af9f was supposed to introduce to a partial
fix. The following example will cause double-hyphen duplication:

```sh
$ cargo run -q echo -nonsense --
-nonsense -- --
```

To fix that as well, a more involved workaround may be necessary.

src/uu/echo/src/echo.rs
@@ -8,6 +8,7 @@ use clap::{Arg, ArgAction, ArgMatches, Command};
 use std::env;
 use std::ffi::{OsStr, OsString};
 use std::io::{self, StdoutLock, Write};
+use std::os::unix::ffi::OsStrExt;
 use uucore::error::{UResult, USimpleError};
 use uucore::format::{parse_escape_only, EscapedChar, FormatChar, OctalParsing};
 use uucore::{format_usage, help_about, help_section, help_usage};
@@ -28,11 +29,16 @@ mod options {
 fn handle_double_hyphens(args: impl uucore::Args) -> impl uucore::Args {
     let mut result = Vec::new();
     let mut is_first_double_hyphen = true;
+    let mut option_index = 0;
 
     for (i, arg) in args.enumerate() {
-        if i < 2 && arg == "--" && is_first_double_hyphen {
-            result.push(OsString::from("--"));
-            is_first_double_hyphen = false;
+        if is_first_double_hyphen {
+            if i < option_index + 2 && arg == "--" {
+                result.push(OsString::from("--"));
+                is_first_double_hyphen = false;
+            } else if arg.as_bytes()[0] == b'-' {
+                option_index = i
+            }
         }
         result.push(arg);
     }