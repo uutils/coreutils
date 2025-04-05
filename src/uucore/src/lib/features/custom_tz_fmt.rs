508ba25e33b187ea0600080430481eb7cb8d8947
Nicolas Boichat <nicolas@boichat.ch>
2025-04-05 11:58:16 +0200

    uucore: custom_tz_fmt: Add abbreviation tests
    This function will still need some love, but at least with this,
    we have basic tests.

--- src/uucore/src/lib/features/custom_tz_fmt.rs
+++ src/uucore/src/lib/features/custom_tz_fmt.rs
@@ -3,6 +3,8 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+// spell-checker:ignore (misc) WARST zoneinfo
+
 use chrono::{TimeZone, Utc};
 use chrono_tz::{OffsetName, Tz};
 use iana_time_zone::get_timezone;
@@ -69,5 +71,42 @@ mod tests {
             "%Y-%m-%d %H-%M-%S.%f"
         );
         assert_eq!(custom_time_format("%Z"), timezone_abbreviation());
+    }
+
+    #[test]
+    fn test_timezone_abbreviation() {
+        // Test if a timezone abbreviation is one of the values in ok_abbr.
+        // TODO(#7659): We should modify this test to 2 fixed dates, one that falls in
+        // daylight savings, and the other not. But right now the abbreviation depends
+        // on the current time.
+        fn test_zone(zone: &str, ok_abbr: &[&str]) {
+            unsafe {
+                std::env::set_var("TZ", zone);
+            }
+            let abbr = timezone_abbreviation();
+            assert!(
+                ok_abbr.contains(&abbr.as_str()),
+                "Timezone {zone} abbreviation {abbr} is not contained within [{}].",
+                ok_abbr.join(", ")
+            )
+        }
+
+        // Test a few random timezones.
+        test_zone("US/Pacific", &["PST", "PDT"]);
+        test_zone("Europe/Zurich", &["CEST", "CET"]);
+        test_zone("Africa/Cairo", &["EET", "EEST"]); // spell-checker:disable-line
+        test_zone("Asia/Taipei", &["CST"]);
+        test_zone("Australia/Sydney", &["AEDT", "AEST"]); // spell-checker:disable-line
+        // Looks like Pacific/Tahiti is provided in /usr/share/zoneinfo, but not in chrono-tz (yet).
+        //test_zone("Pacific/Tahiti", &["-10"]); // No abbreviation?
+        test_zone("Antarctica/South_Pole", &["NZDT", "NZST"]); // spell-checker:disable-line
+
+        // TODO: This is not fully exhaustive, we should understand how to handle
+        // invalid TZ values and more complex POSIX-specified values:
+        // https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+        // Examples:
+        //test_zone("WART4WARST,J1/0,J365/25", &["WART", "WARST"])
+        //test_zone(":Europe/Zurich", &["CEST", "CET"]);
+        //test_zone("invalid", &["invalid"]);
     }
 }
