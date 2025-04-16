4651a58b82fa06348211fcf9b4e2dff931176410
Sylvestre Ledru <sylvestre@debian.org>
2025-04-16 22:03:42 +0200

    ls: add selinux support

--- tests/by-util/test_ls.rs
+++ tests/by-util/test_ls.rs
@@ -4185,6 +4185,26 @@ fn test_ls_context2() {
 
 #[test]
 #[cfg(feature = "feat_selinux")]
+fn test_ls_context_long() {
+    if !uucore::selinux::is_selinux_enabled() {
+        return;
+    }
+    let scene = TestScenario::new(util_name!());
+    let at = &scene.fixtures;
+    at.touch("foo");
+    for c_flag in ["-Zl", "-Zal"] {
+        let result = scene.ucmd().args(&[c_flag, "foo"]).succeeds();
+
+        let line: Vec<_> = result.stdout_str().split(" ").collect();
+        assert!(line[0].ends_with("."));
+        assert!(line[4].starts_with("unconfined_u"));
+        let s: Vec<_> = line[4].split(":").collect();
+        assert!(s.len() == 4);
+    }
+}
+
+#[test]
+#[cfg(feature = "feat_selinux")]
 fn test_ls_context_format() {
     use selinux::{self, KernelSupport};
     if selinux::kernel_support() == KernelSupport::Unsupported {
