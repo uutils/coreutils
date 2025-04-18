0c3b9962522bbaa9b1632df369bea9944b565dc5
Jeffrey Finkelstein <jeffrey.finkelstein@protonmail.com>
2025-04-17 20:31:26 -0400

    cp: use uucore::fs::make_fifo()
    Use `uucore::fs::make_fifo()` when copying a named pipe in `cp`.

--- src/uu/cp/src/cp.rs
+++ src/uu/cp/src/cp.rs
@@ -7,13 +7,9 @@
 use quick_error::quick_error;
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
-#[cfg(not(windows))]
-use std::ffi::CString;
 use std::ffi::OsString;
 use std::fs::{self, Metadata, OpenOptions, Permissions};
 use std::io;
-#[cfg(unix)]
-use std::os::unix::ffi::OsStrExt;
 #[cfg(unix)]
 use std::os::unix::fs::{FileTypeExt, PermissionsExt};
 use std::path::{Path, PathBuf, StripPrefixError};
@@ -23,13 +19,13 @@ use uucore::fsxattr::copy_xattrs;
 use clap::{Arg, ArgAction, ArgMatches, Command, builder::ValueParser};
 use filetime::FileTime;
 use indicatif::{ProgressBar, ProgressStyle};
-#[cfg(unix)]
-use libc::mkfifo;
 use quick_error::ResultExt;
 
 use platform::copy_on_write;
 use uucore::display::Quotable;
 use uucore::error::{UClapError, UError, UResult, UUsageError, set_exit_code};
+#[cfg(unix)]
+use uucore::fs::make_fifo;
 use uucore::fs::{
     FileInformation, MissingHandling, ResolveMode, are_hardlinks_to_same_file, canonicalize,
     get_filename, is_symlink_loop, normalize_path, path_ends_with_terminator,
@@ -2534,12 +2530,7 @@ fn copy_fifo(dest: &Path, overwrite: OverwriteMode, debug: bool) -> CopyResult<(
         fs::remove_file(dest)?;
     }
 
-    let name = CString::new(dest.as_os_str().as_bytes()).unwrap();
-    let err = unsafe { mkfifo(name.as_ptr(), 0o666) };
-    if err == -1 {
-        return Err(format!("cannot create fifo {}: File exists", dest.quote()).into());
-    }
-    Ok(())
+    make_fifo(dest).map_err(|_| format!("cannot create fifo {}: File exists", dest.quote()).into())
 }
 
 fn copy_link(
