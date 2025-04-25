69e3058ce01813ecc0c70de9f1bb1636f51f6692
Teemu Pätsi <frendsick@gmail.com>
2025-04-25 21:06:14 +0300

    expr: Remove fixed FixME comment from test

--- tests/by-util/test_expr.rs
+++ tests/by-util/test_expr.rs
@@ -274,11 +274,6 @@ fn test_length_mb() {
 
 #[test]
 fn test_regex() {
-    // FixME: [2022-12-19; rivy] test disabled as it currently fails due to 'oniguruma' bug (see GH:kkos/oniguruma/issues/279)
-    // new_ucmd!()
-    //     .args(&["a^b", ":", "a^b"])
-    //     .succeeds()
-    //     .stdout_only("3\n");
     new_ucmd!()
         .args(&["a^b", ":", "a\\^b"])
         .succeeds()
