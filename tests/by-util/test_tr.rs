uutils/coreutils

commit 3e4221a4615e800d6010d564984b93d2d9e6abfb

tr: generate an error for real if the input is a directory

src/uucore/src/lib/features/fs.rs
@@ -20,6 +20,7 @@ use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::fs::read_dir;
 use std::hash::Hash;
+use std::io::Stdin;
 use std::io::{Error, ErrorKind, Result as IOResult};
 #[cfg(unix)]
 use std::os::unix::{fs::MetadataExt, io::AsRawFd};
@@ -719,6 +720,34 @@ pub fn path_ends_with_terminator(path: &Path) -> bool {
         .encode_wide()
         .last()
         .map_or(false, |wide| wide == b'/'.into() || wide == b'\\'.into())
+}
+
+/// Checks if the standard input (stdin) is a directory.
+///
+/// # Arguments
+///
+/// * `stdin` - A reference to the standard input handle.
+///
+/// # Returns
+///
+/// * `bool` - Returns `true` if stdin is a directory, `false` otherwise.
+pub fn is_stdin_directory(stdin: &Stdin) -> bool {
+    #[cfg(unix)]
+    {
+        use nix::sys::stat::fstat;
+        let mode = fstat(stdin.as_raw_fd()).unwrap().st_mode as mode_t;
+        has!(mode, S_IFDIR)
+    }
+
+    #[cfg(windows)]
+    {
+        use std::os::windows::io::AsRawHandle;
+        let handle = stdin.as_raw_handle();
+        if let Ok(metadata) = fs::metadata(format!("{}", handle as usize)) {
+            return metadata.is_dir();
+        }
+        false
+    }
 }
 
 pub mod sane_blksize {

tests/by-util/test_tr.rs
@@ -20,6 +20,14 @@ fn test_invalid_input() {
         .fails()
         .code_is(1)
         .stderr_contains("tr: extra operand '<'");
+    #[cfg(unix)]
+    new_ucmd!()
+        .args(&["1", "1"])
+        // will test "tr 1 1 < ."
+        .set_stdin(std::process::Stdio::from(std::fs::File::open(".").unwrap()))
+        .fails()
+        .code_is(1)
+        .stderr_contains("tr: read error: Is a directory");
 }
 
 #[test]

src/uu/tr/Cargo.toml
@@ -19,7 +19,7 @@ path = "src/tr.rs"
 [dependencies]
 nom = { workspace = true }
 clap = { workspace = true }
-uucore = { workspace = true }
+uucore = { workspace = true, features = ["fs"] }
 
 [[bin]]
 name = "tr"

src/uu/tr/src/tr.rs
@@ -8,17 +8,17 @@
 mod operation;
 mod unicode_table;
 
+use crate::operation::DeleteOperation;
 use clap::{crate_version, value_parser, Arg, ArgAction, Command};
 use operation::{
     translate_input, Sequence, SqueezeOperation, SymbolTranslator, TranslateOperation,
 };
 use std::ffi::OsString;
 use std::io::{stdin, stdout, BufWriter};
-use uucore::{format_usage, help_about, help_section, help_usage, os_str_as_bytes, show};
-
-use crate::operation::DeleteOperation;
 use uucore::display::Quotable;
 use uucore::error::{UResult, USimpleError, UUsageError};
+use uucore::fs::is_stdin_directory;
+use uucore::{format_usage, help_about, help_section, help_usage, os_str_as_bytes, show};
 
 const ABOUT: &str = help_about!("tr.md");
 const AFTER_HELP: &str = help_section!("after help", "tr.md");
@@ -125,6 +125,10 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
         truncate_set1_flag && translating,
         translating,
     )?;
+
+    if is_stdin_directory(&stdin) {
+        return Err(USimpleError::new(1, "read error: Is a directory"));
+    }
 
     // '*_op' are the operations that need to be applied, in order.
     if delete_flag {

commit 5d4b286bd9979f0084e4dbc09180e4684347bed5

tr: Fix regression causing read error with sockets
The test used for determining if a file descriptor
tested with fstat points to a directory is traditionally
done by using the S_IFMT mask[^1][^2], e.g.:
    #define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
In Rust, this translates to 'm & libc::S_IFMT == libc::S_IFDIR'.
is_stdin_directory() uses 'has!(mode, S_IFDIR)', which is
**not** equivalent and causes non-directory sockets to be
incorrectly recognized as directories. This causes uu-tr
to break when used with all sockets created with socketpair,
including ksh93 pipes[^3]. Below is an example Rust program
demonstrating why the current check is bogus:
  fn main() {
     use nix::sys::socket::{socketpair, SockFlag, AddressFamily, SockType};
     use nix::sys::stat::fstat;
     use libc::{S_IFDIR, S_IFMT, mode_t};
     let (_fd1, fd2) = socketpair(AddressFamily::Unix, SockType::Stream, None, SockFlag::empty()).unwrap();
     let mode = fstat(&fd2).unwrap().st_mode as mode_t;
     if mode & S_IFMT == S_IFDIR {   // Equivalent to S_ISDIR()
         println!("Not reached");    // Not a dir, so unreachable
     }
     if mode & S_IFDIR == S_IFDIR {  // Bogus check for S_IFDIR
         println!("BAD");
     }
  }

- is_stdin_directory(): Fix the regression introduced in 3e4221a4
  by replacing the bogus check with one equivalent to S_ISDIR().
- test_tr.rs: Add a regression test for this bug.

[^1]: https://sourceware.org/git/?p=glibc.git;a=blob;f=io/sys/stat.h;h=4bea9e9a#l123
[^2]: https://git.musl-libc.org/cgit/musl/tree/include/sys/stat.h?id=047a1639#n51
[^3]: https://github.com/ksh93/ksh/blob/cc5e0692/src/cmd/ksh93/sh/io.c#L98-L102

Fixes https://github.com/uutils/coreutils/issues/7658

Cargo.toml
@@ -524,7 +524,7 @@ hex-literal = "1.0.0"
 rstest = { workspace = true }
 
 [target.'cfg(unix)'.dev-dependencies]
-nix = { workspace = true, features = ["process", "signal", "user", "term"] }
+nix = { workspace = true, features = ["process", "signal", "socket", "user", "term"] }
 rlimit = "0.10.1"
 xattr = { workspace = true }
 

src/uucore/src/lib/features/fs.rs
@@ -720,7 +720,7 @@ pub fn is_stdin_directory(stdin: &Stdin) -> bool {
     {
         use nix::sys::stat::fstat;
         let mode = fstat(stdin.as_fd()).unwrap().st_mode as mode_t;
-        has!(mode, S_IFDIR)
+        return mode & S_IFMT == S_IFDIR;
     }
 
     #[cfg(windows)]

tests/by-util/test_tr.rs
@@ -1556,3 +1556,18 @@ fn test_failed_write_is_reported() {
         .fails()
         .stderr_is("tr: write error: No space left on device\n");
 }
+
+#[cfg(not(windows))]
+#[test]
+fn test_stdin_is_socket() {
+    use nix::sys::socket::{socketpair, SockFlag, AddressFamily, SockType};
+    use nix::unistd::write;
+
+    let (fd1, fd2) = socketpair(AddressFamily::Unix, SockType::Stream, None, SockFlag::empty()).unwrap();
+    write(fd1, b"::").unwrap();
+    new_ucmd!()
+        .args(&[":", ";"])
+        .set_stdin(fd2)
+        .succeeds()
+        .stdout_is(";;");
+}