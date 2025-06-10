7fe550afed54d2e7a8d75a8c60d07dd07269445e
Sylvestre Ledru <sylvestre@debian.org>
2025-06-11 00:28:14 +0200

    l10n: port csplit for translation + add french

--- src/uu/csplit/src/csplit.rs
+++ src/uu/csplit/src/csplit.rs
@@ -85,7 +85,10 @@ impl<T: BufRead> Iterator for LinesWithNewlines<T> {
     fn next(&mut self) -> Option<Self::Item> {
         fn ret(v: Vec<u8>) -> io::Result<String> {
             String::from_utf8(v).map_err(|_| {
-                io::Error::new(ErrorKind::InvalidData, "stream did not contain valid UTF-8")
+                io::Error::new(
+                    ErrorKind::InvalidData,
+                    get_message("csplit-stream-not-utf8"),
+                )
             })
         }
 
@@ -115,7 +118,7 @@ where
     T: BufRead,
 {
     let enumerated_input_lines = LinesWithNewlines::new(input)
-        .map(|line| line.map_err_context(|| "read error".to_string()))
+        .map(|line| line.map_err_context(|| get_message("csplit-read-error")))
         .enumerate();
     let mut input_iter = InputSplitter::new(enumerated_input_lines);
     let mut split_writer = SplitWriter::new(options);
@@ -283,7 +286,7 @@ impl SplitWriter<'_> {
                     current_writer.write_all(bytes)?;
                     self.size += bytes.len();
                 }
-                None => panic!("trying to write to a split that was not created"),
+                None => panic!("{}", get_message("csplit-write-split-not-created")),
             }
         }
         Ok(())
@@ -638,26 +641,26 @@ pub fn uu_app() -> Command {
                 .short('b')
                 .long(options::SUFFIX_FORMAT)
                 .value_name("FORMAT")
-                .help("use sprintf FORMAT instead of %02d"),
+                .help(get_message("csplit-help-suffix-format")),
         )
         .arg(
             Arg::new(options::PREFIX)
                 .short('f')
                 .long(options::PREFIX)
                 .value_name("PREFIX")
-                .help("use PREFIX instead of 'xx'"),
+                .help(get_message("csplit-help-prefix")),
         )
         .arg(
             Arg::new(options::KEEP_FILES)
                 .short('k')
                 .long(options::KEEP_FILES)
-                .help("do not remove output files on errors")
+                .help(get_message("csplit-help-keep-files"))
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new(options::SUPPRESS_MATCHED)
                 .long(options::SUPPRESS_MATCHED)
-                .help("suppress the lines matching PATTERN")
+                .help(get_message("csplit-help-suppress-matched"))
                 .action(ArgAction::SetTrue),
         )
         .arg(
@@ -665,7 +668,7 @@ pub fn uu_app() -> Command {
                 .short('n')
                 .long(options::DIGITS)
                 .value_name("DIGITS")
-                .help("use specified number of digits instead of 2"),
+                .help(get_message("csplit-help-digits")),
         )
         .arg(
             Arg::new(options::QUIET)
@@ -673,14 +676,14 @@ pub fn uu_app() -> Command {
                 .long(options::QUIET)
                 .visible_short_alias('s')
                 .visible_alias("silent")
-                .help("do not print counts of output file sizes")
+                .help(get_message("csplit-help-quiet"))
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new(options::ELIDE_EMPTY_FILES)
                 .short('z')
                 .long(options::ELIDE_EMPTY_FILES)
-                .help("remove empty output files")
+                .help(get_message("csplit-help-elide-empty-files"))
                 .action(ArgAction::SetTrue),
         )
         .arg(
--- src/uu/csplit/src/csplit_error.rs
+++ src/uu/csplit/src/csplit_error.rs
@@ -2,38 +2,40 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+
+use std::collections::HashMap;
 use std::io;
 use thiserror::Error;
-
 use uucore::display::Quotable;
 use uucore::error::UError;
+use uucore::locale::{get_message, get_message_with_args};
 
 /// Errors thrown by the csplit command
 #[derive(Debug, Error)]
 pub enum CsplitError {
     #[error("IO error: {}", _0)]
     IoError(#[from] io::Error),
-    #[error("{}: line number out of range", ._0.quote())]
+    #[error("{}", get_message_with_args("csplit-error-line-out-of-range", HashMap::from([("pattern".to_string(), _0.quote().to_string())])))]
     LineOutOfRange(String),
-    #[error("{}: line number out of range on repetition {}", ._0.quote(), _1)]
+    #[error("{}", get_message_with_args("csplit-error-line-out-of-range-on-repetition", HashMap::from([("pattern".to_string(), _0.quote().to_string()), ("repetition".to_string(), _1.to_string())])))]
     LineOutOfRangeOnRepetition(String, usize),
-    #[error("{}: match not found", ._0.quote())]
+    #[error("{}", get_message_with_args("csplit-error-match-not-found", HashMap::from([("pattern".to_string(), _0.quote().to_string())])))]
     MatchNotFound(String),
-    #[error("{}: match not found on repetition {}", ._0.quote(), _1)]
+    #[error("{}", get_message_with_args("csplit-error-match-not-found-on-repetition", HashMap::from([("pattern".to_string(), _0.quote().to_string()), ("repetition".to_string(), _1.to_string())])))]
     MatchNotFoundOnRepetition(String, usize),
-    #[error("0: line number must be greater than zero")]
+    #[error("{}", get_message("csplit-error-line-number-is-zero"))]
     LineNumberIsZero,
-    #[error("line number '{}' is smaller than preceding line number, {}", _0, _1)]
+    #[error("{}", get_message_with_args("csplit-error-line-number-smaller-than-previous", HashMap::from([("current".to_string(), _0.to_string()), ("previous".to_string(), _1.to_string())])))]
     LineNumberSmallerThanPrevious(usize, usize),
-    #[error("{}: invalid pattern", ._0.quote())]
+    #[error("{}", get_message_with_args("csplit-error-invalid-pattern", HashMap::from([("pattern".to_string(), _0.quote().to_string())])))]
     InvalidPattern(String),
-    #[error("invalid number: {}", ._0.quote())]
+    #[error("{}", get_message_with_args("csplit-error-invalid-number", HashMap::from([("number".to_string(), _0.quote().to_string())])))]
     InvalidNumber(String),
-    #[error("incorrect conversion specification in suffix")]
+    #[error("{}", get_message("csplit-error-suffix-format-incorrect"))]
     SuffixFormatIncorrect,
-    #[error("too many % conversion specifications in suffix")]
+    #[error("{}", get_message("csplit-error-suffix-format-too-many-percents"))]
     SuffixFormatTooManyPercents,
-    #[error("{} is not a regular file", ._0.quote())]
+    #[error("{}", get_message_with_args("csplit-error-not-regular-file", HashMap::from([("file".to_string(), _0.quote().to_string())])))]
     NotRegularFile(String),
     #[error("{}", _0)]
     UError(Box<dyn UError>),
