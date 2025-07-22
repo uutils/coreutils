abb53000688004801a2d6121ec4320ba2456f3a3
Julian Andres Klode <julian.klode@canonical.com>
2025-07-22 09:40:36 +0200

    install, mkdir: Handle dir/./ like dir/.
    The dir/. special case was handled correctly, but dir/./ was not,
    breaking gcc builds, which run:
    
    ../../../../src/libstdc++-v3/../install-sh -c -d debug/./
    
    Bug-Ubuntu: https://bugs.launchpad.net/bugs/2117466

--- src/uucore/src/lib/features/fs.rs
+++ src/uucore/src/lib/features/fs.rs
@@ -534,12 +534,12 @@ pub fn display_permissions_unix(mode: mode_t, display_file_type: bool) -> String
     result
 }
 
-/// For some programs like install or mkdir, dir/. can be provided
+/// For some programs like install or mkdir, dir/. or dir/./ can be provided
 /// Special case to match GNU's behavior:
-/// install -d foo/. should work and just create foo/
+/// install -d foo/. (and foo/./) should work and just create foo/
 /// std::fs::create_dir("foo/."); fails in pure Rust
 pub fn dir_strip_dot_for_creation(path: &Path) -> PathBuf {
-    if path.to_string_lossy().ends_with("/.") {
+    if path.to_string_lossy().ends_with("/.") || path.to_string_lossy().ends_with("/./") {
         // Do a simple dance to strip the "/."
         Path::new(&path).components().collect::<PathBuf>()
     } else {
