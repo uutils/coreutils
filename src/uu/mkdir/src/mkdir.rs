f6f9ca744910e88c947df39d850637f84d9f5eda
Nicolas Boichat <nicolas@boichat.ch>
2025-07-22 13:53:48 +0800

    uucore: Move strip_minus_from_mode to mkdir
    It's only used there anyway.

--- src/uu/mkdir/src/mkdir.rs
+++ src/uu/mkdir/src/mkdir.rs
@@ -85,9 +85,25 @@ fn strip_minus_from_mode(_args: &mut [String]) -> bool {
     false
 }
 
+// Iterate 'args' and delete the first occurrence
+// of a prefix '-' if it's associated with MODE
+// e.g. "chmod -v -xw -R FILE" -> "chmod -v xw -R FILE"
 #[cfg(not(windows))]
 fn strip_minus_from_mode(args: &mut Vec<String>) -> bool {
-    mode::strip_minus_from_mode(args)
+    for arg in args {
+        if arg == "--" {
+            break;
+        }
+        if let Some(arg_stripped) = arg.strip_prefix('-') {
+            if let Some('r' | 'w' | 'x' | 'X' | 's' | 't' | 'u' | 'g' | 'o' | '0'..='7') =
+                arg.chars().nth(1)
+            {
+                *arg = arg_stripped.to_string();
+                return true;
+            }
+        }
+    }
+    false
 }
 
 #[uucore::main]
