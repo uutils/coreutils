5da99e21b82ab5e62bdd02bc083ae1f982c36393
Nicolas Boichat <nicolas@boichat.ch>
2025-07-31 19:42:59 +0800

    od: Start adding support for f128 (ExtendedBigDecimal-backed)
    There is no (stable) support for f128 (yet?), and we need to parse
    f80 anyway, so let's back the datatype with an ExtendedBigDecimal,
    for now.

--- src/uu/od/src/formatter_item_info.rs
+++ src/uu/od/src/formatter_item_info.rs
@@ -2,9 +2,10 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore (ToDO) formatteriteminfo
+// spell-checker:ignore (ToDO) formatteriteminfo extendedbigdecimal
 
 use std::fmt;
+use uucore::extendedbigdecimal::ExtendedBigDecimal;
 
 #[allow(clippy::enum_variant_names)]
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -12,6 +13,7 @@ pub enum FormatWriter {
     IntWriter(fn(u64) -> String),
     FloatWriter(fn(f64) -> String),
     BFloatWriter(fn(f64) -> String),
+    ExtendedBigDecimalWriter(fn(&ExtendedBigDecimal) -> String),
     MultibyteWriter(fn(&[u8]) -> String),
 }
 
@@ -29,6 +31,10 @@ impl fmt::Debug for FormatWriter {
             Self::BFloatWriter(ref p) => {
                 f.write_str("BFloatWriter:")?;
                 fmt::Pointer::fmt(p, f)
+            }
+            Self::ExtendedBigDecimalWriter(ref p) => {
+                f.write_str("ExtendedBigDecimalWriter:")?;
+                fmt::Pointer::fmt(&(*p as *const ()), f)
             }
             Self::MultibyteWriter(ref p) => {
                 f.write_str("MultibyteWriter:")?;
