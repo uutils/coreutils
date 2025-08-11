8f2506cbc8a3202c61f19a23e348e4949f075a86
Nicolas Boichat <nicolas@boichat.ch>
2025-08-11 14:47:02 +0800

    env: Simplify let-chain
    We can just check if the value is Some(true).

--- src/uu/env/src/env.rs
+++ src/uu/env/src/env.rs
@@ -519,9 +519,7 @@ impl EnvAppData {
         self.do_input_debug_printing = self
             .do_input_debug_printing
             .or(Some(matches.get_count("debug") >= 2));
-        if let Some(value) = self.do_input_debug_printing
-            && value
-        {
+        if self.do_input_debug_printing == Some(true) {
             debug_print_args(&original_args);
             self.do_input_debug_printing = Some(false);
         }
