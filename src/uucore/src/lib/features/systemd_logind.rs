a0911d513f74676c602b83bdd5faf28699348dee
Etienne Cordonnier <ecordonnier@snap.com>
2025-08-22 12:55:22 +0200

    fix cspell
    Signed-off-by: Etienne Cordonnier <ecordonnier@snap.com>

--- src/uucore/src/lib/features/systemd_logind.rs
+++ src/uucore/src/lib/features/systemd_logind.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 //
-// spell-checker:ignore logind libsystemd btime unref RAII
+// spell-checker:ignore logind libsystemd btime unref RAII testuser GETPW sysconf
 
 //! Systemd-logind support for reading login records
 //!
