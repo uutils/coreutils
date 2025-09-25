3f4eda6033542450d6ba1ea2081777f69db3e634
Guillem L. Jara <4lon3ly0@tutanota.com>
2025-09-25 03:31:26 +0200

    cat: add colorization functionality
    Specifically, add a hidden Easter egg that makes it behave like the notorious `lolcat` if the `--lol` flag is passed.

---
+++ src/uu/cat/src/colors.rs
@@ -0,0 +1,253 @@
+use std::env::var_os;
+use std::f64::consts::PI;
+use std::ffi::OsStr;
+use std::io::{Result as IOResult, Write};
+
+use crate::OutputState;
+
+static TRUECOLOR_ESCAPE_START: &'static str = "\x1b[38;2";
+static ANSI_ESCAPE_START: &'static str = "\x1b[38;5;";
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum ColorMode {
+    TrueColor,
+    ANSI256,
+    ANSI,
+}
+
+impl ColorMode {
+    fn conv_color(&self, color: [f64; 3]) -> Color {
+        let fit_linear_curve = |x: f64| (127. * x + 128.) as u8;
+        let retrofit_ansi = |color: [f64; 3]| {
+            let ratio = [36, 6, 1];
+            let ascii_color_offset = 16u16;
+            color
+                .into_iter()
+                .map(fit_linear_curve)
+                .zip(ratio)
+                .fold(ascii_color_offset, |acc, (c, m)| {
+                    acc + ((6. * (c as f64 / 256.)).floor() as u16) * m
+                })
+        };
+        match self {
+            ColorMode::TrueColor => Color::Color24b(color.map(fit_linear_curve)),
+            ColorMode::ANSI256 => Color::ANSI256(retrofit_ansi(color)),
+            ColorMode::ANSI => Color::ANSI(retrofit_ansi(color) as u8),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Color {
+    Color24b([u8; 3]),
+    ANSI256(u16),
+    ANSI(u8),
+}
+
+impl Color {
+    fn format_char(&self, buf: &mut Vec<u8>, ch: u8) {
+        // avoid rust format machinery
+        let format_num = |b: &mut Vec<u8>, mut n| {
+            let ascii_num_radix = 48;
+            let buf_len = b.len();
+            if n == 0 {
+                b.push(ascii_num_radix);
+                return;
+            };
+            while n > 0 {
+                b.push(ascii_num_radix + (n % 10) as u8);
+                n /= 10;
+            }
+            (&mut b[buf_len..]).reverse();
+        };
+
+        // format according to escape sequence needed
+        match *self {
+            Color::Color24b(color) => {
+                buf.truncate(TRUECOLOR_ESCAPE_START.len());
+                for c in color {
+                    buf.push(b';');
+                    format_num(buf, c as usize);
+                }
+                buf.push(b'm');
+                buf.push(ch);
+            }
+            Color::ANSI256(color) => {
+                buf.truncate(ANSI_ESCAPE_START.len());
+                format_num(buf, color as usize);
+                buf.push(b'm');
+                buf.push(ch);
+            }
+            Color::ANSI(color) => {
+                buf.truncate(ANSI_ESCAPE_START.len());
+                format_num(buf, color as usize);
+                buf.push(b'm');
+                buf.push(ch);
+            }
+        }
+    }
+}
+
+impl ColorMode {
+    pub fn new() -> Self {
+        if cfg!(target_os = "windows")
+            || var_os("COLORTERM")
+                .map(|ref x| x == OsStr::new("truecolor") || x == OsStr::new("24bit"))
+                .unwrap_or(false)
+            || var_os("CI").is_some()
+        {
+            ColorMode::TrueColor
+        } else if var_os("TERM")
+            .map(|ref x| x == OsStr::new("xterm-256color"))
+            .unwrap_or(false)
+        {
+            ColorMode::ANSI256
+        } else {
+            ColorMode::ANSI
+        }
+    }
+}
+
+/// This is a wrapper over a a Writer that
+/// intersperses color escape codes in between
+/// written characters, assuming no line breaks.
+pub struct ColorWriter<'w, W: Write> {
+    inner: &'w mut W,
+    mode: ColorMode,
+    buffer: Vec<u8>,
+    state: &'w mut OutputState,
+    terminal_cursor: usize,
+}
+
+impl<'w, W: Write> ColorWriter<'w, W> {
+    const COLOR_DIFFUSION: f64 = 0.06;
+
+    pub fn new(inner: &'w mut W, mode: ColorMode, state: &'w mut OutputState) -> Self {
+        Self {
+            inner,
+            mode,
+            buffer: Vec::from(match mode {
+                ColorMode::TrueColor => TRUECOLOR_ESCAPE_START,
+                _ => ANSI_ESCAPE_START,
+            }),
+            state,
+            terminal_cursor: 0,
+        }
+    }
+
+    /// Compute color in sinus-image range; heavily based on
+    /// https://github.com/ur0/lolcat; MIT-licensed and
+    /// co-copyright of Umang Raghuvanshi et al.
+    fn get_color(&self) -> Color {
+        let color = Self::COLOR_DIFFUSION * self.state.color_seed[0];
+        let two_thirds = 2. / 3.;
+        let rgb = [
+            color,
+            color + (PI * two_thirds),
+            color + (PI * 2. * two_thirds),
+        ]
+        .map(f64::sin);
+
+        self.mode.conv_color(rgb)
+    }
+
+    fn next_col(&mut self) {
+        self.state.color_seed[0] += 1.;
+    }
+
+    fn next_row(&mut self) {
+        self.state.color_seed[1] += 1.;
+        self.state.color_seed[0] = self.state.color_seed[1];
+        self.terminal_cursor = 0;
+    }
+
+    /// Along with the color gradient algorithm,
+    /// this escape sequence parser is heavily based
+    /// on https://github.com/ur0/lolcat; MIT-licensed
+    /// and co-copyright of Umang Raghuvanshi et al.
+    // Beware of the following spaghetti.
+    fn parse_escape_seq<'a, 'b>(
+        &self,
+        chars: &'a mut impl Iterator<Item = &'b u8>,
+    ) -> Result<String, ()> {
+        let mut buf = String::with_capacity(16);
+        buf.push('\x1b');
+
+        let mut next_ch = || {
+            let Some(ch) = chars.next().map(|x| *x as char) else {
+                return Err(());
+            };
+            buf.push(ch);
+            Ok(ch)
+        };
+
+        match next_ch()? {
+            '[' => 'l1: loop {
+                match next_ch()? {
+                    '\x30'..='\x3F' => continue 'l1,
+                    '\x20'..='\x2F' => {
+                        'l2: loop {
+                            match next_ch()? {
+                                '\x20'..='\x2F' => continue 'l2,
+                                '\x40'..='\x7E' => break 'l2,
+                                _ => return Err(()),
+                            }
+                        }
+                        break 'l1;
+                    }
+                    '\x40'..='\x7E' => break 'l1,
+                    _ => return Err(()),
+                }
+            },
+            '\x20'..='\x2F' => 'l2: loop {
+                match next_ch()? {
+                    '\x20'..='\x2F' => continue 'l2,
+                    '\x30'..='\x7E' => break 'l2,
+                    _ => return Err(()),
+                }
+            },
+            // Unsupported, obscure escape sequences
+            '\x30'..='\x3F' | '\x40'..='\x5F' | '\x60'..='\x7E' => return Err(()),
+            // Assume the sequence is just one character otherwise
+            _ => (),
+        };
+        Ok(buf)
+    }
+}
+
+impl<'w, W: Write> Write for ColorWriter<'w, W> {
+    fn flush(&mut self) -> IOResult<()> {
+        // reset colors after flush
+        self.inner.write_all(b"\x1b[39m")?;
+        self.inner.flush()
+    }
+
+    fn write(&mut self, buf: &[u8]) -> IOResult<usize> {
+        let mut chars = buf.iter();
+
+        while let Some(&ch) = chars.next() {
+            match ch {
+                // relay escape sequences verbatim
+                b'\x1b' => {
+                    let Ok(buf) = self.parse_escape_seq(&mut chars) else {
+                        continue;
+                    };
+                    self.inner.write_all(buf.as_bytes())?;
+                }
+                // skip colorization of whitespaces or tabs
+                c @ (b'\x20' | b'\x09' | b'\x0b') => {
+                    self.inner.write(&[c])?;
+                }
+                // If not an escape sequence or a newline, print the
+                // color escape sequence and then the character
+                _ => {
+                    self.get_color().format_char(&mut self.buffer, ch);
+                    self.inner.write_all(&self.buffer)?;
+                }
+            };
+            self.next_col();
+        }
+        self.next_row();
+        self.inner.write_all(b"\x1b[39m").map(|_| buf.len())
+    }
+}
