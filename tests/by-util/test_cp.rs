88602e4ca4843b0f43110992f3aaa5cddd33e396
Anonymous-AAA <alenantonyme@gmail.com>
2025-09-26 13:23:17 +0530

    applied cargo fmt

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -7082,13 +7082,11 @@ fn test_cp_no_dereference_symlink_with_parents() {
 
 #[test]
 #[cfg(unix)]
-fn test_cp_recursive_files_ending_in_backslash(){
+fn test_cp_recursive_files_ending_in_backslash() {
     let ts = TestScenario::new(util_name!());
     let at = &ts.fixtures;
     at.mkdir("a");
     at.touch("a/foo\\");
-    ts.ucmd()
-        .args(&["-r","a","b"])
-        .succeeds();  
+    ts.ucmd().args(&["-r", "a", "b"]).succeeds();
     assert!(at.file_exists("b/foo\\"));
 }
