2919027841a699381ef42c5a0e261e897244efd9
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-10-18 21:25:23 -0700

    uudoc: move manpage generation away from the binary

--- tests/by-util/test_base64.rs
+++ tests/by-util/test_base64.rs
@@ -5,7 +5,6 @@
 #[cfg(target_os = "linux")]
 use uutests::at_and_ucmd;
 use uutests::new_ucmd;
-use uutests::util::TestScenario;
 
 #[test]
 #[cfg(target_os = "linux")]
@@ -235,43 +234,4 @@ cyBvdmVyIHRoZSBsYXp5IGRvZy4=
 ",
             // cSpell:enable
         );
-}
-
-// Prevent regression to:
-//
-// ❯ coreutils manpage base64 | rg --fixed-strings -- 'base32'
-// The data are encoded as described for the base32 alphabet in RFC 4648.
-// to the bytes of the formal base32 alphabet. Use \-\-ignore\-garbage
-// The data are encoded as described for the base32 alphabet in RFC 4648.
-// to the bytes of the formal base32 alphabet. Use \-\-ignore\-garbage
-#[test]
-fn test_manpage() {
-    use std::process::{Command, Stdio};
-    unsafe {
-        // force locale to english to avoid issues with manpage output
-        std::env::set_var("LANG", "C");
-    }
-
-    let test_scenario = TestScenario::new("");
-
-    let child = Command::new(&test_scenario.bin_path)
-        .arg("manpage")
-        .arg("base64")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let output = child.wait_with_output().unwrap();
-
-    assert_eq!(output.status.code().unwrap(), 0);
-
-    assert!(output.stderr.is_empty());
-
-    let stdout_str = std::str::from_utf8(&output.stdout).unwrap();
-
-    assert!(stdout_str.contains("base64 alphabet"));
-
-    assert!(!stdout_str.to_ascii_lowercase().contains("base32"));
 }
