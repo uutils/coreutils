8dd81de724cdecba4e82142a0cec08736b9d67d5
E <79379754+oech3@users.noreply.github.com>
2025-10-21 08:00:57 +0900

    build-gnu.sh: Force build SELINUX_PROGS for tests

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -136,6 +136,8 @@ cd -
 
 # Pass the feature flags to make, which will pass them to cargo
 "${MAKE}" PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}"
+# Foece-add SELINUX_PROGS for tests
+"${MAKE}" PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}" UTILS="runcon chcon"
 touch g
 echo "stat with selinux support"
 ./target/debug/stat -c%C g || true
