b32244bb572908ee1ce938512903fab00d0e6b0f
mattsu <matsui.atsushi.home@gmail.com>
2025-10-24 19:48:51 +0900

    refactor(install): remove unused std::fs import and reorder path import
    Removed the unused `std::fs` import and repositioned `std::path::Path` after the conditional block for better import organization in the mode module.

--- src/uu/install/src/mode.rs
+++ src/uu/install/src/mode.rs
@@ -3,9 +3,9 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 use std::fs;
-use std::path::Path;
 #[cfg(all(unix, not(target_os = "redox")))]
 use std::os::fd::{AsFd, AsRawFd};
+use std::path::Path;
 #[cfg(all(unix, not(target_os = "redox")))]
 use uucore::libc;
 #[cfg(not(windows))]
