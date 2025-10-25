32dc79befe992bdcea8fc2dc59e8ddd90f10b3df
Mahdi Ali-Raihan <asder8215@gmail.com>
2025-10-24 21:25:22 -0400

    rebasing with main

--- src/uu/mv/src/mv.rs
+++ src/uu/mv/src/mv.rs
@@ -675,10 +675,8 @@ fn move_files_into_dir(files: &[PathBuf], target_dir: &Path, options: &Options) 
         ) {
             Err(e) if e.to_string().is_empty() => set_exit_code(1),
             Err(e) => {
-                let e = e.map_err_context(|| {
-                    translate!("mv-error-cannot-overwrite")
-                });
-                match display_manager {
+                let e = e.map_err_context(|| translate!("mv-error-cannot-overwrite"));
+                match multi_progress {
                     Some(ref pb) => pb.suspend(|| show!(e)),
                     None => show!(e),
                 }
@@ -756,7 +754,9 @@ fn rename(
             if is_empty_dir(to) {
                 fs::remove_dir(to)?;
             } else {
-                return Err(io::Error::other(translate!("mv-error-directory-not-empty", "target" => to.quote())));
+                return Err(io::Error::other(
+                    translate!("mv-error-directory-not-empty", "target" => to.quote()),
+                ));
             }
         }
     }
