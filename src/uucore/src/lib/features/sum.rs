42f4298b4c0c0c479904e29bb61bdf7a9ee2fbbf
naoNao89 <90588855+naoNao89@users.noreply.github.com>
2025-10-26 11:48:55 +0700

    style: add Hdlc to spell-checker ignore list
    Fix cspell warning for Crc32IsoHdlc by adding Hdlc to the
    spell-checker:ignore comment. Hdlc is a legitimate acronym
    for High-Level Data Link Control used in CRC algorithms.

--- src/uucore/src/lib/features/sum.rs
+++ src/uucore/src/lib/features/sum.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore memmem algo PCLMULQDQ refin xorout
+// spell-checker:ignore memmem algo PCLMULQDQ refin xorout Hdlc
 
 //! Implementations of digest functions, like md5 and sha1.
 //!
