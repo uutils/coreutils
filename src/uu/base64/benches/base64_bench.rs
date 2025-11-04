d403ba06ff51139916fdb115120db9b43577f999
Sylvestre Ledru <sylvestre@debian.org>
2025-11-04 07:35:30 +0100

    Revert "Fix base64 benchmarks (#9082)"
    This reverts commit 975e18c032deb3f62272da3d2526aa37113b054c.

--- src/uu/base64/benches/base64_bench.rs
+++ src/uu/base64/benches/base64_bench.rs
@@ -6,24 +6,22 @@
 use divan::{Bencher, black_box};
 use std::ffi::OsString;
 use uu_base64::uumain;
-use uucore::benchmark::{create_test_file, run_util_function, setup_test_file, text_data};
+use uucore::benchmark::{create_test_file, run_util_function, text_data};
 
 fn create_tmp_file(size_mb: usize) -> String {
+    let temp_dir = tempfile::tempdir().unwrap();
     let data = text_data::generate_by_size(size_mb, 80);
-    let file_path = setup_test_file(&data);
+    let file_path = create_test_file(&data, temp_dir.path());
     String::from(file_path.to_str().unwrap())
 }
 
 /// Benchmark for base64 encoding
 #[divan::bench()]
 fn b64_encode_synthetic(bencher: Bencher) {
-    let file_path_str = &create_tmp_file(50);
+    let file_path_str = &create_tmp_file(5_000);
 
     bencher.bench(|| {
-        black_box(run_util_function(
-            uumain,
-            &["-o", "/dev/null", file_path_str],
-        ));
+        black_box(run_util_function(uumain, &[file_path_str]));
     });
 }
 
@@ -31,25 +29,20 @@ fn b64_encode_synthetic(bencher: Bencher) {
 #[divan::bench()]
 fn b64_decode_synthetic(bencher: Bencher) {
     let temp_dir = tempfile::tempdir().unwrap();
-    let file_path_str = &create_tmp_file(50);
+    let file_path_str = &create_tmp_file(5_000);
     let in_file = create_test_file(b"", temp_dir.path());
     let in_file_str = in_file.to_str().unwrap();
     uumain(
         [
-            OsString::from(uucore::util_name()),
-            OsString::from("-o"),
-            OsString::from(in_file_str),
             OsString::from(file_path_str),
+            OsString::from(format!(">{in_file_str}")),
         ]
         .iter()
         .map(|x| (*x).clone()),
     );
 
     bencher.bench(|| {
-        black_box(run_util_function(
-            uumain,
-            &["-d", "-o", "/dev/null", in_file_str],
-        ));
+        black_box(run_util_function(uumain, &["-d", in_file_str]));
     });
 }
 
@@ -57,25 +50,20 @@ fn b64_decode_synthetic(bencher: Bencher) {
 #[divan::bench()]
 fn b64_decode_ignore_garbage_synthetic(bencher: Bencher) {
     let temp_dir = tempfile::tempdir().unwrap();
-    let file_path_str = &create_tmp_file(50);
+    let file_path_str = &create_tmp_file(5_000);
     let in_file = create_test_file(b"", temp_dir.path());
     let in_file_str = in_file.to_str().unwrap();
     uumain(
         [
-            OsString::from(uucore::util_name()),
-            OsString::from("-o"),
-            OsString::from(in_file_str),
             OsString::from(file_path_str),
+            OsString::from(format!(">{in_file_str}")),
         ]
         .iter()
         .map(|x| (*x).clone()),
     );
 
     bencher.bench(|| {
-        black_box(run_util_function(
-            uumain,
-            &["-d", "-i", "-o", "/dev/null", in_file_str],
-        ));
+        black_box(run_util_function(uumain, &["-d", "-i", in_file_str]));
     });
 }
 
