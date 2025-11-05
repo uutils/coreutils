08e68b06fd01dca612872fa73f7fb1fa27c573d7
Suprun <hello@syoma.website>
2025-11-05 23:34:10 +0400

    tests/printenv: add more tests

--- tests/by-util/test_printenv.rs
+++ tests/by-util/test_printenv.rs
@@ -18,15 +18,41 @@ fn test_get_all() {
 fn test_get_var() {
     new_ucmd!()
         .env("KEY", "VALUE")
+        .env("FOO", "BAR")
         .arg("KEY")
         .succeeds()
-        .stdout_contains("VALUE\n");
+        .stdout_contains("VALUE\n")
+        .stdout_does_not_contain("BAR");
 }
 
 #[test]
 fn test_ignore_equal_var() {
     // tested by gnu/tests/misc/printenv.sh
     new_ucmd!().env("a=b", "c").arg("a=b").fails().no_stdout();
+}
+
+#[test]
+fn test_silent_error_equal_var() {
+    // printenv should ignore variables with equal signs e.g. a=b=c
+    new_ucmd!()
+        .env("KEY", "VALUE")
+        .env("a=b", "c")
+        .arg("KEY")
+        .arg("a=b")
+        .fails_with_code(1)
+        .stdout_contains("VALUE\n")
+        .stdout_does_not_contain("c");
+}
+
+#[test]
+fn test_silent_error_not_present() {
+    // printenv should ignore unspecified variables, not panic on them
+    new_ucmd!()
+        .env("KEY", "VALUE")
+        .arg("FOO")
+        .arg("KEY")
+        .fails_with_code(1)
+        .stdout_contains("VALUE\n");
 }
 
 #[test]
@@ -39,4 +65,36 @@ fn test_invalid_option_exit_code() {
         .code_is(2)
         .stderr_contains("unexpected argument")
         .stderr_contains("For more information, try '--help'");
+}
+
+#[test]
+fn test_null_separator() {
+    // printenv should use \x00 as separator
+    new_ucmd!()
+        .env("HOME", "FOO")
+        .env("KEY", "VALUE")
+        .arg("-0")
+        .succeeds()
+        .stdout_contains("HOME=FOO\x00")
+        .stdout_contains("KEY=VALUE\x00");
+
+    new_ucmd!()
+        .env("HOME", "FOO")
+        .env("KEY", "VALUE")
+        .arg("--null")
+        .succeeds()
+        .stdout_contains("HOME=FOO\x00")
+        .stdout_contains("KEY=VALUE\x00");
+
+    new_ucmd!()
+        .env("HOME", "FOO")
+        .env("KEY", "VALUE")
+        .env("FOO", "BAR")
+        .arg("-0")
+        .arg("HOME")
+        .arg("KEY")
+        .succeeds()
+        .stdout_contains("FOO\x00")
+        .stdout_contains("VALUE\x00")
+        .stdout_does_not_contain("BAR");
 }
