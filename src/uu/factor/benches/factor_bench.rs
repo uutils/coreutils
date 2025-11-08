d977e299a03d63331f823e47cf7942a115d9a088
Mahdi Ali-Raihan <asder8215@gmail.com>
2025-11-08 13:07:27 -0500

    factor/bench: reduced range on u64 and u128 benchmarking and remove single u64/u128/big_uint benchmark tests

--- src/uu/factor/benches/factor_bench.rs
+++ src/uu/factor/benches/factor_bench.rs
@@ -7,20 +7,12 @@ use divan::{Bencher, black_box};
 use uu_factor::uumain;
 use uucore::benchmark::run_util_function;
 
-/// Benchmark one u64 digit
-#[divan::bench(args = [1000000])]
-fn factor_single_u64(bencher: Bencher, num: u64) {
-    bencher.with_inputs(|| num).bench_values(|single_u64| {
-        black_box(run_util_function(uumain, &[&single_u64.to_string()]));
-    });
-}
-
 /// Benchmark multiple u64 digits
-#[divan::bench(args = [(2, 50001)])]
-fn factor_multiple_u64s(bencher: Bencher, (start_num, end_num): (u64, u64)) {
+#[divan::bench(args = [(2)])]
+fn factor_multiple_u64s(bencher: Bencher, start_num: u64) {
     bencher
-        // this is a range of 50000 million different u128 integers
-        .with_inputs(|| (start_num, end_num))
+        // this is a range of 5000 different u128 integers
+        .with_inputs(|| (start_num, start_num + 2500))
         .bench_values(|(start_u64, end_u64)| {
             for u64_digit in start_u64..=end_u64 {
                 black_box(run_util_function(uumain, &[&u64_digit.to_string()]));
@@ -28,21 +20,13 @@ fn factor_multiple_u64s(bencher: Bencher, (start_num, end_num): (u64, u64)) {
         });
 }
 
-/// Benchmark one u128 digit
-#[divan::bench(args = [18446744073709551616])]
-fn factor_single_u128(bencher: Bencher, num: u128) {
-    bencher.with_inputs(|| num).bench_values(|single_u128| {
-        black_box(run_util_function(uumain, &[&single_u128.to_string()]));
-    });
-}
-
 /// Benchmark multiple u128 digits
-#[divan::bench(args = [(18446744073709551616, 18446744073709601616)])]
-fn factor_multiple_u128s(bencher: Bencher, (start_num, end_num): (u128, u128)) {
+#[divan::bench(args = [(18446744073709551616)])]
+fn factor_multiple_u128s(bencher: Bencher, start_num: u128) {
     bencher
         .with_inputs(|| {
-            // this is a range of 50000 million different u128 integers
-            (start_num, end_num)
+            // this is a range of 1000 different u128 integers
+            (start_num, start_num + 1000)
         })
         .bench_values(|(start_u128, end_u128)| {
             for u128_digit in start_u128..=end_u128 {
@@ -51,22 +35,13 @@ fn factor_multiple_u128s(bencher: Bencher, (start_num, end_num): (u128, u128)) {
         });
 }
 
-/// Benchmark single > u128::MAX digits
-#[divan::bench]
-fn factor_single_big_uint(bencher: Bencher) {
-    // max u128 value is 340_282_366_920_938_463_463_374_607_431_768_211_455
-    bencher
-        .with_inputs(|| "340_282_366_920_938_463_463_374_607_431_768_211_456")
-        .bench_values(|single_big_uint| {
-            black_box(run_util_function(uumain, &[single_big_uint]));
-        });
-}
-
 /// Benchmark multiple > u128::MAX digits
 #[divan::bench]
 fn factor_multiple_big_uint(bencher: Bencher) {
     // max u128 value is 340_282_366_920_938_463_463_374_607_431_768_211_455
     bencher
+        // this is a range of 3 different BigUints. The range is small due to
+        // some BigUints being unable to be factorized into prime numbers properly
         .with_inputs(|| (768_211_459_u64, 768_211_461_u64))
         .bench_values(|(start_big_uint, end_big_uint)| {
             for digit in start_big_uint..=end_big_uint {
