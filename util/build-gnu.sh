dfc712cf421026f215bbed5791f4b739a2516e46
E <79379754+oech3@users.noreply.github.com>
2025-11-08 19:01:28 +0900

    build-gnu.sh omit uu_chcon

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -134,7 +134,7 @@ else
 fi
 cd -
 # Add missing SELINUX_PROGS for tests
-cargo build --profile="${UU_MAKE_PROFILE}" -p uu_runcon -p uu_chcon
+cargo build --profile="${UU_MAKE_PROFILE}" -p uu_runcon #-p uu_chcon
 # Pass the feature flags to make, which will pass them to cargo
 "${MAKE}" PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}"
 touch g
