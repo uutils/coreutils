d425bf7f8fe7ff54540051e6985f5a5c7732c7e3
mattsu <matsui.atsushi.home@gmail.com>
2025-11-10 22:05:52 +0900

    chore(base32): extend spell-checker ignore list with test token
    - Add "hellohello" to ignored words to prevent false positives
    - Keeps spell-checker from flagging known test data in base_common.rs

--- src/uu/base32/src/base_common.rs
+++ src/uu/base32/src/base_common.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore hexupper lsbf msbf unpadded nopad aGVsbG8sIHdvcmxkIQ
+// spell-checker:ignore hexupper lsbf msbf unpadded nopad aGVsbG8sIHdvcmxkIQ hellohello
 
 use clap::{Arg, ArgAction, Command};
 use std::ffi::OsString;
