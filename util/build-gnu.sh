a02310268b2f96bf58942f4e4dfcad1538375a02
oech3 <79379754+oech3@users.noreply.github.com>
2025-11-12 20:04:26 +0900

    copy

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -132,9 +132,9 @@ cd -
 
 # Pass the feature flags to make, which will pass them to cargo
 "${MAKE}" MULTICALL=y PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}"
-# Hardlink binaries
+# copy binaries
 for binary in $(${UU_BUILD_DIR}/coreutils --list); do
-    (cd ${UU_BUILD_DIR} && ln -vf coreutils "$binary")
+    (cd ${UU_BUILD_DIR} && cp -vf coreutils "$binary")
 done
 echo -e '#!/bin/sh\nexec $(dirname $0)/install "$@"' > "${UU_BUILD_DIR}/ginstall" && chmod +x "${UU_BUILD_DIR}/ginstall" # The GNU tests rename this script before running, to avoid confusion with the make target
 touch g
