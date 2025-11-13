13f0fa48b336ce2eb9766a75dc795fe5401767d4
oech3 <79379754+oech3@users.noreply.github.com>
2025-11-13 17:30:42 +0900

    Avoid ||:

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -136,10 +136,10 @@ cd -
 ln ${UU_BUILD_DIR}/install ${UU_BUILD_DIR}/ginstall
 "${MAKE}" SKIP_UTILS=install MULTICALL=y PROFILE="${UU_MAKE_PROFILE}" CARGOFLAGS="${CARGO_FEATURE_FLAGS}"
 
-chcon -t bin_t "${UU_BUILD_DIR}/coreutils" || :
+[ SELINUX_ENABLED = 1 ] && chcon -t bin_t "${UU_BUILD_DIR}/coreutils"
 for binary in $(${UU_BUILD_DIR}/coreutils --list); do
     (cd ${UU_BUILD_DIR} && cp -v --remove-destination coreutils "$binary")
-    chcon -t bin_t "${UU_BUILD_DIR}/${binary}" || :
+    [ SELINUX_ENABLED = 1 ] && chcon -t bin_t "${UU_BUILD_DIR}/${binary}"
 done
 touch g
 echo "stat with selinux support"
