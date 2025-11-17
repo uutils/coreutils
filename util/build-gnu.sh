8941074cb9d59c6d2f9196562f80e4ea076c8afc
oech3 <79379754+oech3@users.noreply.github.com>
2025-11-18 00:20:39 +0900

    build-gnu.sh: Do not symlink missing bins to false

--- util/build-gnu.sh
+++ util/build-gnu.sh
@@ -121,15 +121,6 @@ test -f "${UU_BUILD_DIR}/[" || (cd ${UU_BUILD_DIR} && ln -s "test" "[")
 
 cd "${path_GNU}" && echo "[ pwd:'${PWD}' ]"
 
-# Any binaries that aren't built become `false` so their tests fail
-for binary in $(./build-aux/gen-lists-of-programs.sh --list-progs); do
-    bin_path="${UU_BUILD_DIR}/${binary}"
-    test -f "${bin_path}" || {
-        echo "'${binary}' was not built with uutils, using the 'false' program"
-        cp "${UU_BUILD_DIR}/false" "${bin_path}"
-    }
-done
-
 if test -f gnu-built; then
     echo "GNU build already found. Skip"
     echo "'rm -f $(pwd)/gnu-built' to force the build"
