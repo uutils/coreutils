29d90eab12bd6abdf61672e3364ac7d3991a0522
Sylvestre Ledru <sylvestre@debian.org>
2025-11-19 08:32:45 +0100

    add fullwidth to the spell ignore list

--- tests/by-util/test_fold.rs
+++ tests/by-util/test_fold.rs
@@ -2,6 +2,8 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+// spell-checker:ignore fullwidth
+
 use uutests::new_ucmd;
 
 #[test]
