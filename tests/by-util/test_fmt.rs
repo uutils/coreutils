2c617d4af28160e8d3be9668fc814fb86a50ae5c
mattsu <matsui.atsushi.home@gmail.com>
2025-11-19 16:44:28 +0900

    chore: add FFFD to spell-checker ignore list in fmt test.

--- tests/by-util/test_fmt.rs
+++ tests/by-util/test_fmt.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore plass samp
+// spell-checker:ignore plass samp FFFD
 #[cfg(target_os = "linux")]
 use std::os::unix::ffi::OsStringExt;
 use uutests::new_ucmd;
