de9df127fc28bb265582cd30530fddf820ce57cf
mattsu <matsui.atsushi.home@gmail.com>
2025-11-19 22:21:33 +0900

    feat(cspell): add ERANGE to jargon wordlist
    Added "ERANGE" to the dictionary to prevent spell checker flagging it as a misspelling, as it's a valid errno constant from C libraries.

--- src/uu/od/src/input_decoder.rs
+++ src/uu/od/src/input_decoder.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore bfloat multifile
+// spell-checker:ignore bfloat multifile mant
 
 use half::{bf16, f16};
 use std::io;
