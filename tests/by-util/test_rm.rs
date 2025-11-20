185fe36a740ad11bfdc12567f2096d8137804b9c
mattsu <matsui.atsushi.home@gmail.com>
2025-11-20 17:35:00 +0900

    chore: Add 'volname' to spell-checker ignore list.

--- tests/by-util/test_rm.rs
+++ tests/by-util/test_rm.rs
@@ -2,6 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+// spell-checker:ignore volname
 #![allow(clippy::stable_sort_primitive)]
 
 use std::process::Stdio;
