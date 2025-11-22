9dc7d227f9ed67607e612b6c826dbde8f296af4c
Christopher Dryden <christopher.paul.dryden@gmail.com>
2025-11-22 01:39:27 +0000

    One more missing spellcheck entry

--- src/uu/users/src/users.rs
+++ src/uu/users/src/users.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (paths) wtmp
+// spell-checker:ignore (paths) wtmp ESRCH
 
 use std::ffi::OsString;
 use std::path::Path;
