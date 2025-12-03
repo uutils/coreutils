2ab13b5012f8e027129c034902aadcffa32b964d
mattsu <matsui.atsushi.home@gmail.com>
2025-12-03 20:55:06 +0900

    test(chmod): add spell-check ignore for dirfd, subdirs, openat, FDCWD
    Added a spell-checker ignore directive in the chmod test file to suppress false positives for legitimate technical terms used in Unix API calls.

--- tests/by-util/test_chmod.rs
+++ tests/by-util/test_chmod.rs
@@ -2,6 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+// spell-checker:ignore (words) dirfd subdirs openat FDCWD
 
 use std::fs::{OpenOptions, Permissions, metadata, set_permissions};
 use std::os::unix::fs::{OpenOptionsExt, PermissionsExt};
