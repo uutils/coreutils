45a33e8db2fb120c55cad1a4967f06f05564847c
Christopher Dryden <christopher.paul.dryden@gmail.com>
2025-12-10 18:16:24 +0000

    Spellcheck fixes

--- src/uu/mv/src/mv.rs
+++ src/uu/mv/src/mv.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (ToDO) sourcepath targetpath nushell canonicalized
+// spell-checker:ignore (ToDO) sourcepath targetpath nushell canonicalized unwriteable
 
 mod error;
 #[cfg(unix)]
