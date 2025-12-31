2f005da192267caa6f1a877b214ed4eb206a54b3
mattsu <matsui.atsushi.home@gmail.com>
2025-12-31 13:22:06 +0900

    chore(test): add ENOSPC to spell-checker ignore list in cat tests
    - Prevents spell-checker from flagging ENOSPC as a misspelling, as it's a valid errno constant (e.g., "No space left on device") used in the test code.

--- tests/by-util/test_cat.rs
+++ tests/by-util/test_cat.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore NOFILE nonewline cmdline
+// spell-checker:ignore NOFILE nonewline cmdline　ENOSPC
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
 use rlimit::Resource;
