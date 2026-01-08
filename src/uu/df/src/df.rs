16b78572179b8365bfa8c7659cba494b0ccf9be9
Sylvestre Ledru <sylvestre@debian.org>
2026-01-08 14:35:32 +0100

    add "sysfs" to the spell-checker ignore

--- src/uu/df/src/df.rs
+++ src/uu/df/src/df.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore itotal iused iavail ipcent pcent tmpfs squashfs lofs
+// spell-checker:ignore itotal iused iavail ipcent pcent tmpfs squashfs lofs sysfs
 mod blocks;
 mod columns;
 mod filesystem;
