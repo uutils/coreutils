9753c1205b4aeaa98e5f561762af8d0de8700a1e
Jake Abendroth <contact@jakea.net>
2026-01-09 04:10:16 -0800

    fix(spell-checker): add mkdirat, CREAT, WRONLY, and testdir to ignore list

--- tests/by-util/test_install.rs
+++ tests/by-util/test_install.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore (words) helloworld nodir objdump n'source nconfined
+// spell-checker:ignore (words) helloworld nodir objdump n'source nconfined testdir
 
 #[cfg(not(target_os = "openbsd"))]
 use filetime::FileTime;
