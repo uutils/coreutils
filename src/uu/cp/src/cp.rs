d8668180203df108af6bc155e1474377218748c5
mattsu <matsui.atsushi.home@gmail.com>
2026-01-14 21:36:28 +0900

    chore(spell-checker): add 'devnull' to ignore lists in cp and printf
    Add 'devnull' to the spell-checker ignore comments in src/uu/cp/src/cp.rs and src/uu/printf/src/printf.rs to prevent false positives, as 'devnull' refers to the /dev/null device and is a valid technical term in the codebase.

--- src/uu/cp/src/cp.rs
+++ src/uu/cp/src/cp.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore (ToDO) copydir ficlone fiemap ftruncate linkgs lstat nlink nlinks pathbuf pwrite reflink strs xattrs symlinked deduplicated advcpmv nushell IRWXG IRWXO IRWXU IRWXUGO IRWXU IRWXG IRWXO IRWXUGO
+// spell-checker:ignore (ToDO) copydir ficlone fiemap ftruncate linkgs lstat nlink nlinks pathbuf pwrite reflink strs xattrs symlinked deduplicated advcpmv nushell IRWXG IRWXO IRWXU IRWXUGO IRWXU IRWXG IRWXO IRWXUGO devnull
 
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
