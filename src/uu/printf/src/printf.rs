dd6622ed06353f6a059be71b312bfeadaaa6d669
mattsu <matsui.atsushi.home@gmail.com>
2026-01-14 21:36:28 +0900

    chore(spell-checker): add 'devnull' to ignore lists in cp and printf
    Add 'devnull' to the spell-checker ignore comments in src/uu/cp/src/cp.rs and src/uu/printf/src/printf.rs to prevent false positives, as 'devnull' refers to the /dev/null device and is a valid technical term in the codebase.

--- src/uu/printf/src/printf.rs
+++ src/uu/printf/src/printf.rs
@@ -2,6 +2,9 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
+
+// spell-checker:ignore (ToDO) devnull
+
 use clap::{Arg, ArgAction, Command};
 use std::ffi::OsString;
 use std::io::stdout;
