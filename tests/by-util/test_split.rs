da7437406b621da980730bdb983f68f70c5fcb89
Christopher Dryden <christopher.paul.dryden@gmail.com>
2026-01-15 02:50:29 +0000

    split: increase memory limit in streaming test for cross-compiled platforms

--- tests/by-util/test_split.rs
+++ tests/by-util/test_split.rs
@@ -1993,16 +1993,16 @@ fn test_split_separator_same_multiple() {
 fn test_number_n_chunks_streaming() {
     let (at, mut ucmd) = at_and_ucmd!();
 
-    // 50MB file, 40MB memory limit, split into 2x25MB chunks
-    let mut f = std::fs::File::create(at.plus("fifty_mb.bin")).unwrap();
-    f.write_all(&vec![0u8; 50 * 1024 * 1024]).unwrap();
+    // 100MB file, 100MB memory limit, split into 2x50MB chunks
+    let mut f = std::fs::File::create(at.plus("hundred_mb.bin")).unwrap();
+    f.write_all(&vec![0u8; 100 * 1024 * 1024]).unwrap();
 
-    ucmd.args(&["--number=2", "fifty_mb.bin"])
-        .limit(Resource::AS, 40 * 1024 * 1024, 40 * 1024 * 1024)
+    ucmd.args(&["--number=2", "hundred_mb.bin"])
+        .limit(Resource::AS, 100 * 1024 * 1024, 100 * 1024 * 1024)
         .succeeds();
 
-    assert_eq!(at.metadata("xaa").len(), 25 * 1024 * 1024);
-    assert_eq!(at.metadata("xab").len(), 25 * 1024 * 1024);
+    assert_eq!(at.metadata("xaa").len(), 50 * 1024 * 1024);
+    assert_eq!(at.metadata("xab").len(), 50 * 1024 * 1024);
 }
 
 #[test]
