3aa889dd23e5e06ad6f5de5dd94f1e114827139c
Sylvestre Ledru <sylvestre@debian.org>
2026-01-17 09:29:08 +0100

    Add PIPESTATUS to spell-checker ignore list

--- tests/by-util/test_sort.rs
+++ tests/by-util/test_sort.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (words) ints (linux) NOFILE dfgi
+// spell-checker:ignore (words) ints (linux) NOFILE dfgi PIPESTATUS
 #![allow(clippy::cast_possible_wrap)]
 
 use std::env;
