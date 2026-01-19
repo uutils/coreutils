12f19bd0bd046d3e43be97ab523faccb314927ca
max-amb <max_a@e.email>
2026-01-19 12:06:09 +0000

    fixed permission race due to truncation pre modification of perms

--- src/uu/cp/src/platform/linux.rs
+++ src/uu/cp/src/platform/linux.rs
@@ -100,11 +100,12 @@ where
 
             // The file actually already exists
             if let Some(mut desired_permissions) = dest_permissions {
-                let dst = OpenOptions::new().write(true).truncate(true).open(&dest)?;
                 // This will be reset to the correct permissions later, this is defensive as it is
                 // the most restrictive
+                let dst = OpenOptions::new().write(true).open(&dest)?;
                 desired_permissions.set_mode(0o600);
                 dst.set_permissions(desired_permissions)?;
+                dst.set_len(0)?; // Equivalent to truncate
                 dst
             } else {
                 // If a symlink exists in the position we want to write the file to, and it symlinks to
