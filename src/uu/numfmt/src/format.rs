d8a2e6ebde867d955a71ee0cb1ef206a536e8eeb
mattsu <matsui.atsushi.home@gmail.com>
2026-01-27 23:03:27 +0900

    fix: correct typo in spell-checker ignore comment
    Fixed a typo in the spell-checker ignore comment where 'localeconv' was incorrectly written as 'localeconv　' with an extra space character.

--- src/uu/numfmt/src/format.rs
+++ src/uu/numfmt/src/format.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore powf
+// spell-checker:ignore powf　localeconv
 use std::ffi::CStr;
 use std::io::Write;
 use std::sync::Once;
