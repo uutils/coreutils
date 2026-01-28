bc63bbb581a9797c48343e54277af21bd87ce68c
oech3 <79379754+oech3@users.noreply.github.com>
2026-01-28 22:51:36 +0900

    cksum: Fix -a blake3/shake*

--- src/uucore/src/lib/features/checksum/mod.rs
+++ src/uucore/src/lib/features/checksum/mod.rs
@@ -114,6 +114,12 @@ impl AlgoKind {
             ALGORITHM_OPTIONS_SHA256 => Sha256,
             ALGORITHM_OPTIONS_SHA384 => Sha384,
             ALGORITHM_OPTIONS_SHA512 => Sha512,
+
+            // Extension
+            ALGORITHM_OPTIONS_BLAKE3 => Blake3,
+            ALGORITHM_OPTIONS_SHAKE128 => Shake128,
+            ALGORITHM_OPTIONS_SHAKE256 => Shake256,
+
             _ => return Err(ChecksumError::UnknownAlgorithm(algo.as_ref().to_string()).into()),
         })
     }
