1f6acdb3ae933688ea26ea1964560a5453161ca3
Michael Yang <michaely4ng@proton.me>
2026-02-14 16:24:33 +1100

    refactor(cp): simplify mode consts

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -7920,7 +7920,7 @@ fn test_cp_existing_no_preserve_dir() {
     scene.cmd("mkdir").arg("d1").succeeds();
     scene.cmd("mkdir").arg("d2").succeeds();
     scene.cmd("chmod").arg("770").arg("d1").succeeds();
-    scene.cmd("chmod").arg("700").arg("d2").succeeds();
+    scene.cmd("chmod").arg("707").arg("d2").succeeds();
     let d2_mode = at.metadata("d2").mode();
 
     scene
@@ -7978,7 +7978,7 @@ fn test_cp_existing_default_dir() {
     scene.cmd("mkdir").arg("d1").succeeds();
     scene.cmd("mkdir").arg("d2").succeeds();
     scene.cmd("chmod").arg("770").arg("d1").succeeds();
-    scene.cmd("chmod").arg("700").arg("d2").succeeds();
+    scene.cmd("chmod").arg("707").arg("d2").succeeds();
     let d2_mode = at.metadata("d2").mode();
 
     scene
@@ -8036,7 +8036,7 @@ fn test_cp_existing_preserve_dir() {
     scene.cmd("mkdir").arg("d1").succeeds();
     scene.cmd("mkdir").arg("d2").succeeds();
     scene.cmd("chmod").arg("770").arg("d1").succeeds();
-    scene.cmd("chmod").arg("700").arg("d2").succeeds();
+    scene.cmd("chmod").arg("707").arg("d2").succeeds();
     let d1_mode = at.metadata("d1").mode();
 
     scene
