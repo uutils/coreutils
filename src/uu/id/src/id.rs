b33cc1d9e7563e04e3fc011217ace9ba448baef7
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-02-17 15:17:54 +0000

    id: add selinux/smack cfg alias

--- src/uu/id/src/id.rs
+++ src/uu/id/src/id.rs
@@ -63,9 +63,9 @@ macro_rules! cstr2cow {
 }
 
 fn get_context_help_text() -> String {
-    #[cfg(not(any(feature = "selinux", feature = "smack")))]
+    #[cfg(not(any(selinux, smack)))]
     return translate!("id-context-help-disabled");
-    #[cfg(any(feature = "selinux", feature = "smack"))]
+    #[cfg(any(selinux, smack))]
     return translate!("id-context-help-enabled");
 }
 
@@ -99,9 +99,9 @@ struct State {
     rflag: bool,  // --real
     zflag: bool,  // --zero
     cflag: bool,  // --context
-    #[cfg(feature = "selinux")]
+    #[cfg(selinux)]
     selinux_supported: bool,
-    #[cfg(feature = "smack")]
+    #[cfg(smack)]
     smack_supported: bool,
     ids: Option<Ids>,
     // The behavior for calling GNU's `id` and calling GNU's `id $USER` is similar but different.
@@ -141,9 +141,9 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
         zflag: matches.get_flag(options::OPT_ZERO),
         cflag: matches.get_flag(options::OPT_CONTEXT),
 
-        #[cfg(feature = "selinux")]
+        #[cfg(selinux)]
         selinux_supported: uucore::selinux::is_selinux_enabled(),
-        #[cfg(feature = "smack")]
+        #[cfg(smack)]
         smack_supported: uucore::smack::is_smack_enabled(),
         user_specified: !users.is_empty(),
         ids: None,
@@ -179,7 +179,7 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
 
     if state.cflag {
         // SELinux context
-        #[cfg(feature = "selinux")]
+        #[cfg(selinux)]
         if state.selinux_supported {
             if let Ok(context) = selinux::SecurityContext::current(false) {
                 let bytes = context.as_bytes();
@@ -193,7 +193,7 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
         }
 
         // SMACK label
-        #[cfg(feature = "smack")]
+        #[cfg(smack)]
         if state.smack_supported {
             match uucore::smack::get_smack_label_for_self() {
                 Ok(label) => {
@@ -702,7 +702,7 @@ fn id_print(state: &State, groups: &[u32]) -> io::Result<()> {
             .join(",")
     )?;
 
-    #[cfg(feature = "selinux")]
+    #[cfg(selinux)]
     if state.selinux_supported
         && !state.user_specified
         && std::env::var_os("POSIXLY_CORRECT").is_none()
@@ -714,7 +714,7 @@ fn id_print(state: &State, groups: &[u32]) -> io::Result<()> {
         }
     }
 
-    #[cfg(feature = "smack")]
+    #[cfg(smack)]
     if state.smack_supported
         && !state.user_specified
         && std::env::var_os("POSIXLY_CORRECT").is_none()
