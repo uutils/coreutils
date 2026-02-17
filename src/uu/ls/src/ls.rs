598222a1edbde6ed811d8c8e8a4bb681c194b451
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-02-17 16:09:16 +0000

    ls: add selinux/smack cfg alias

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -375,9 +375,9 @@ pub struct Config {
     time_format_recent: String,        // Time format for recent dates
     time_format_older: Option<String>, // Time format for older dates (optional, if not present, time_format_recent is used)
     context: bool,
-    #[cfg(all(feature = "selinux", any(target_os = "linux", target_os = "android")))]
+    #[cfg(selinux)]
     selinux_supported: bool,
-    #[cfg(all(feature = "smack", target_os = "linux"))]
+    #[cfg(smack)]
     smack_supported: bool,
     group_directories_first: bool,
     line_ending: LineEnding,
@@ -1237,9 +1237,9 @@ impl Config {
             time_format_recent,
             time_format_older,
             context,
-            #[cfg(all(feature = "selinux", any(target_os = "linux", target_os = "android")))]
+            #[cfg(selinux)]
             selinux_supported: uucore::selinux::is_selinux_enabled(),
-            #[cfg(all(feature = "smack", target_os = "linux"))]
+            #[cfg(smack)]
             smack_supported: uucore::smack::is_smack_enabled(),
             group_directories_first: options.get_flag(options::GROUP_DIRECTORIES_FIRST),
             line_ending: LineEnding::from_zero_flag(options.get_flag(options::ZERO)),
@@ -3595,7 +3595,7 @@ fn get_security_context<'a>(
         }
     }
 
-    #[cfg(all(feature = "selinux", any(target_os = "linux", target_os = "android")))]
+    #[cfg(selinux)]
     if config.selinux_supported {
         match selinux::SecurityContext::of_path(path, must_dereference, false) {
             Err(_r) => {
@@ -3633,7 +3633,7 @@ fn get_security_context<'a>(
         }
     }
 
-    #[cfg(all(feature = "smack", target_os = "linux"))]
+    #[cfg(smack)]
     if config.smack_supported {
         // For SMACK, use the path to get the label
         // If must_dereference is true, we follow the symlink
