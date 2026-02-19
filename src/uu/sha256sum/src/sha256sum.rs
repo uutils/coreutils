5d505ca6b933e4f963f9bae2fd20bc51115dc661
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-02-19 06:33:28 +0000

    chore: use consistent copyright header in `.rs` files (#11020)

--- src/uu/sha256sum/src/sha256sum.rs
+++ src/uu/sha256sum/src/sha256sum.rs
@@ -1,1 +1,6 @@
+// This file is part of the uutils coreutils package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 uu_checksum_common::declare_standalone!("sha256sum", uucore::checksum::AlgoKind::Sha256);
