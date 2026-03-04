73aa0267a837f3a831e3db4364e5f6b53a5977e8
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2026-03-04 16:56:06 +0100

    cut: use map_err instead of match

--- src/uu/cut/src/cut.rs
+++ src/uu/cut/src/cut.rs
@@ -673,19 +673,17 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
         },
     };
 
-    let files: Vec<OsString> = matches
+    let mode = mode_parse.map_err(|e| USimpleError::new(1, e))?;
+
+    let files = matches
         .get_many::<OsString>(options::FILE)
         .unwrap_or_default()
         .cloned()
         .collect();
 
-    match mode_parse {
-        Ok(mode) => {
-            cut_files(files, &mode);
-            Ok(())
-        }
-        Err(e) => Err(USimpleError::new(1, e)),
-    }
+    cut_files(files, &mode);
+
+    Ok(())
 }
 
 pub fn uu_app() -> Command {
