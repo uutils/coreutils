c961ff859b63ee2879f0bf8439df43a6e332a813
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2026-03-21 13:38:00 +0100

    ls: add header to new files

--- src/uu/ls/src/display.rs
+++ src/uu/ls/src/display.rs
@@ -1,3 +1,8 @@
+// This file is part of the uutils coreutils package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 // spell-checker:ignore (ToDO) somegroup nlink tabsize dired subdired dtype colorterm stringly
 // spell-checker:ignore nohash strtime clocale
 
