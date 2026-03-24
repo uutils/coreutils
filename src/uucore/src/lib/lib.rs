fac58fbdb42abbbe44fcf360255f7480eadcade7
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-03-24 10:31:56 +0530

    refactor: replace manual From impls for CharByte with macro

--- src/uucore/src/lib/lib.rs
+++ src/uucore/src/lib/lib.rs
@@ -589,23 +589,10 @@ pub enum CharByte {
     Byte(u8),
 }
 
-impl From<char> for CharByte {
-    fn from(value: char) -> Self {
-        Self::Char(value)
-    }
-}
-
-impl From<u8> for CharByte {
-    fn from(value: u8) -> Self {
-        Self::Byte(value)
-    }
-}
-
-impl From<&u8> for CharByte {
-    fn from(value: &u8) -> Self {
-        Self::Byte(*value)
-    }
-}
+impl_from_for_enum!(CharByte:
+    char => Char,
+    u8 => Byte, ref
+);
 
 struct Utf8ChunkIterator<'a> {
     iter: Box<dyn Iterator<Item = CharByte> + 'a>,
