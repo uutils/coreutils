566836cde82bcdb37322b4cdb885a52866b9a8ba
oech3 <79379754+oech3@users.noreply.github.com>
2026-03-30 16:02:15 +0900

    cat: add bench (#11525)
    * cat: add bench
    
    * rename benchmark
    
    Co-authored-by: xtqqczze <45661989+xtqqczze@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: xtqqczze <45661989+xtqqczze@users.noreply.github.com>

---
+++ src/uu/cat/benches/cat_bench.rs
@@ -0,0 +1,19 @@
+use divan::{Bencher, black_box};
+use uu_cat::uumain;
+use uucore::benchmark::{run_util_function, setup_test_file};
+
+#[divan::bench(args = [10_000])]
+fn cat_default(bencher: Bencher, size_bytes: usize) {
+    let data = vec![b'a'; size_bytes];
+
+    let file_path = setup_test_file(&data);
+    let path_str = file_path.to_str().unwrap();
+
+    bencher.bench(|| {
+        black_box(run_util_function(uumain, &[path_str]));
+    });
+}
+
+fn main() {
+    divan::main();
+}
