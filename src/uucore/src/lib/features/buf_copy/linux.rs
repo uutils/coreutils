b6531f323baf69eadb25d304b24c8970b3b7f3b5
oech3 <79379754+oech3@users.noreply.github.com>
2026-04-04 03:15:39 +0900

    uucore: use 1 MiB pipe size at buf_copy

--- src/uucore/src/lib/features/buf_copy/linux.rs
+++ src/uucore/src/lib/features/buf_copy/linux.rs
@@ -7,7 +7,7 @@
 
 use crate::{
     error::UResult,
-    pipes::{pipe, splice, splice_exact},
+    pipes::{MAX_ROOTLESS_PIPE_SIZE, pipe, splice, splice_exact},
 };
 
 /// Buffer-based copying utilities for unix (excluding Linux).
@@ -28,7 +28,6 @@ pub trait FdWritable: Write + AsFd + AsRawFd {}
 
 impl<T> FdWritable for T where T: Write + AsFd + AsRawFd {}
 
-const SPLICE_SIZE: usize = 1024 * 128;
 const BUF_SIZE: usize = 1024 * 16;
 
 /// Conversion from a `nix::Error` into our `Error` which implements `UError`.
@@ -94,7 +93,7 @@ where
     let mut bytes: u64 = 0;
 
     loop {
-        match splice(&source, &pipe_wr, SPLICE_SIZE) {
+        match splice(&source, &pipe_wr, MAX_ROOTLESS_PIPE_SIZE) {
             Ok(n) => {
                 if n == 0 {
                     return Ok((bytes, false));
