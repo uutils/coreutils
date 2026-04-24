a866fcc9985ea7c84d139f7fa7e630f6d6b015a6
penelope zhong <penelope.zhong@proton.me>
2026-04-23 21:20:06 -0500

    pinky: fix panic when outputting to /dev/full (#10562)
    - fixes panic by replacing all println with writeln

--- src/uu/pinky/src/platform/openbsd.rs
+++ src/uu/pinky/src/platform/openbsd.rs
@@ -5,11 +5,12 @@
 // Specific implementation for OpenBSD: tool unsupported (utmpx not supported)
 
 use crate::uu_app;
+use std::io::{Write, stdout};
 use uucore::error::UResult;
 use uucore::translate;
 
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
     let _matches = uucore::clap_localization::handle_clap_result(uu_app(), args)?;
-    println!("{}", translate!("pinky-unsupported-openbsd"));
+    writeln!(stdout(), "{}", translate!("pinky-unsupported-openbsd"))?;
     Ok(())
 }
