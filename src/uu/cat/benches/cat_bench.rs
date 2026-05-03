38fc0556de72f9502aa7433bdd9861d4ee9e7913
oech3 <79379754+oech3@users.noreply.github.com>
2026-05-03 22:21:31 +0900

    try to use stdin at CodSpeed bench

--- src/uu/cat/benches/cat_bench.rs
+++ src/uu/cat/benches/cat_bench.rs
@@ -14,6 +14,29 @@ fn cat_default(bencher: Bencher, size_bytes: usize) {
     });
 }
 
+#[cfg(target_os = "linux")]
+#[divan::bench(args = [10_000])]
+fn cat_splice_pregenerated_pipe(bencher: Bencher, size_bytes: usize) {
+    let data = vec![b'a'; size_bytes];
+    let stdin_bak = rustix::io::dup(rustix::stdio::stdin()).unwrap();
+
+    bencher
+        .with_inputs(|| {
+            let (pipe_r, pipe_w) = rustix::pipe::pipe().unwrap();
+            // fcntl here if you want to bench 64 KiB ~ 1 MiB
+            let mut file = std::fs::File::from(pipe_r);
+            file.write_all(&data).unwrap();
+            drop(file);
+            pipe_r
+        })
+        .bench_local(|pipe_r| {
+            use rustix::stdio::dup2_stdin;
+            dup2_stdin(&pipe_r).unwrap();
+            black_box(run_util_function(uumain, &["cat"]));
+            dup2_stdin(&stdin_bak).unwrap();
+        });
+}
+
 fn main() {
     divan::main();
 }
