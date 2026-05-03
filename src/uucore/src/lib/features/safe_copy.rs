b1096cef4d622e4730300bf8ad07ff3e4ce42696
Sylvestre Ledru <sylvestre@debian.org>
2026-05-03 09:55:14 +0200

    Add additional spell-checker ignores for safe_copy.rs

--- src/uucore/src/lib/features/safe_copy.rs
+++ src/uucore/src/lib/features/safe_copy.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore TOCTOU NOFOLLOW
+// spell-checker:ignore TOCTOU NOFOLLOW CLOEXEC ELOOP RDONLY WRONLY
 
 //! Path-based primitives for security-sensitive file copies.
 //!
