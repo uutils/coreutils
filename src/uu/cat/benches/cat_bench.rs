a3e90d80309fdd00cfc31f22c9047f89109190a7
oech3 <79379754+oech3@users.noreply.github.com>
2026-05-03 22:28:54 +0900

    fix import

--- src/uu/cat/benches/cat_bench.rs
+++ src/uu/cat/benches/cat_bench.rs
@@ -1,7 +1,6 @@
 use divan::{Bencher, black_box};
 use uu_cat::uumain;
 use uucore::benchmark::{run_util_function, setup_test_file};
-
 #[divan::bench(args = [10_000, 10_000_000])]
 fn cat_default(bencher: Bencher, size_bytes: usize) {
     let data = vec![b'a'; size_bytes];
@@ -15,26 +14,20 @@ fn cat_default(bencher: Bencher, size_bytes: usize) {
 }
 
 #[cfg(target_os = "linux")]
-#[divan::bench(args = [10_000])]
-fn cat_splice_pregenerated_pipe(bencher: Bencher, size_bytes: usize) {
+#[divan::bench(args = [10_000, 10_000_000])]
+fn cat_stdin(bencher: Bencher, size_bytes: usize) {
     let data = vec![b'a'; size_bytes];
+    let file_path = setup_test_file(&data);
+    let file = std::fs::File::open(file_path).unwrap();
     let stdin_bak = rustix::io::dup(rustix::stdio::stdin()).unwrap();
 
-    bencher
-        .with_inputs(|| {
-            let (pipe_r, pipe_w) = rustix::pipe::pipe().unwrap();
-            // fcntl here if you want to bench 64 KiB ~ 1 MiB
-            let mut file = std::fs::File::from(pipe_r);
-            file.write_all(&data).unwrap();
-            drop(file);
-            pipe_r
-        })
-        .bench_local(|pipe_r| {
-            use rustix::stdio::dup2_stdin;
-            dup2_stdin(&pipe_r).unwrap();
-            black_box(run_util_function(uumain, &["cat"]));
-            dup2_stdin(&stdin_bak).unwrap();
-        });
+    bencher.bench_local(|| {
+        use rustix::stdio::dup2_stdin;
+        rustix::fs::seek(&file, rustix::fs::SeekFrom::Start(0)).unwrap();
+        dup2_stdin(&file).unwrap(); // should be 1 thread
+        black_box(run_util_function(uumain, &[]));
+        dup2_stdin(&stdin_bak).unwrap(); // should be 1 thread
+    });
 }
 
 fn main() {
