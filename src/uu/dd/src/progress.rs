77e5c40cefe5b013f9eebc70baf2cdeb5b8914d2
Sylvestre Ledru <sylvestre@debian.org>
2026-05-07 00:10:11 +0900

    dd: start replacing nix by rustix

--- src/uu/dd/src/progress.rs
+++ src/uu/dd/src/progress.rs
@@ -459,8 +459,9 @@ extern "C" fn sigusr1_handler(_: std::os::raw::c_int) {
 }
 
 #[cfg(target_os = "linux")]
-pub(crate) fn install_sigusr1_handler() -> Result<(), nix::errno::Errno> {
+pub(crate) fn install_sigusr1_handler() -> Result<(), rustix::io::Errno> {
     uucore::signals::install_signal_handler(nix::sys::signal::Signal::SIGUSR1, sigusr1_handler)
+        .map_err(|e| rustix::io::Errno::from_raw_os_error(e as i32))
 }
 
 /// Return a closure that can be used in its own thread to print progress info.
