0cd71c69bf11296738d60dc0e78873664b5f9c67
max-amb <max_a@e.email>
2026-05-07 10:54:13 +0100

    Forced no wasi test

--- tests/by-util/test_od.rs
+++ tests/by-util/test_od.rs
@@ -1345,7 +1345,7 @@ fn test_write_error_dev_full() {
 }
 
 #[test]
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", not(target_os = "wasi")))]
 fn test_hex_lowercase() {
     use regex::Regex;
     // Test verifies that the output hex byte offset is in lowercase
