5dd9f377dbba5bf52bbdd1c58eac410dae60b98d
oech3 <79379754+oech3@users.noreply.github.com>
2026-05-23 22:52:04 +0900

    pipes.rs: replace bool by Result<(),()>

--- src/uucore/src/lib/features/buf_copy/linux.rs
+++ src/uucore/src/lib/features/buf_copy/linux.rs
@@ -23,7 +23,7 @@ pub fn copy_stream(
 ) -> crate::error::UResult<()> {
     // If we're on Linux or Android, try to use the splice() system call
     // for faster writing. If it works, we're done.
-    if crate::pipes::splice_unbounded_auto(src, dest)? {
+    if crate::pipes::splice_unbounded_auto(src, dest)?.is_err() {
         // If the splice() call failed, fall back on writing "without buffering", or order of output would be wrong
         // unrelated for cp /dev/stdin since cp does not have multiple input? <https://github.com/uutils/coreutils/issues/5186>
         // RawWriter also removes io::copy's specialization
