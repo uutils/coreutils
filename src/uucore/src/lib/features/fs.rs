7f0053075eb61c0982674dbf143537a6316f04c8
oech3 <79379754+oech3@users.noreply.github.com>
2026-05-31 22:15:19 +0900

    remove some collapsible_if (#12539)
    * remove some collapsible_if
    
    * Use .ok() == .ok()
    
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>
    
    ---------
    
    Co-authored-by: oech3 <>
    Co-authored-by: Daniel Hofstetter <daniel.hofstetter@42dh.com>

--- src/uucore/src/lib/features/fs.rs
+++ src/uucore/src/lib/features/fs.rs
@@ -633,12 +633,7 @@ pub fn infos_refer_to_same_file(
     info1: IOResult<FileInformation>,
     info2: IOResult<FileInformation>,
 ) -> bool {
-    if let Ok(info1) = info1 {
-        if let Ok(info2) = info2 {
-            return info1 == info2;
-        }
-    }
-    false
+    info1.is_ok() && info1.ok() == info2.ok()
 }
 
 /// Converts absolute `path` to be relative to absolute `to` path.
