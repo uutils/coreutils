fa6cbd25d7b0c803e6aeb1977cf436d7c209088c
lorossi_expedia <rossilor95@outlook.it>
2026-06-01 15:18:47 +0200

    chown: restrict no-dereference symlink ctime test to Linux

--- tests/by-util/test_chown.rs
+++ tests/by-util/test_chown.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 // spell-checker:ignore (words) agroupthatdoesntexist auserthatdoesntexist cuuser groupname notexisting passgrp
-
+#[cfg(target_os = "linux")]
 use std::os::unix::fs::MetadataExt;
 use uutests::util::{CmdResult, TestScenario, is_ci, run_ucmd_as_root};
 use uutests::util_name;
@@ -900,7 +900,7 @@ fn test_chown_reference_file() {
 }
 
 #[test]
-#[cfg(unix)]
+#[cfg(target_os = "linux")]
 fn test_chown_no_dereference_symlink_to_dir() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
