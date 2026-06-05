f8ea2328f2744878cad1cfccae828fc5caae6141
Sylvestre Ledru <sylvestre@debian.org>
2026-06-05 15:12:38 +0200

    Add 'fgetfilecon' and 'fsetfilecon' to spell-checker ignore

--- src/uu/chcon/src/chcon.rs
+++ src/uu/chcon/src/chcon.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore (vars) RFILE RDONLY CLOEXEC
+// spell-checker:ignore (vars) RFILE RDONLY CLOEXEC fgetfilecon fsetfilecon
 
 #![cfg(any(target_os = "linux", target_os = "android"))]
 #![allow(clippy::upper_case_acronyms)]
