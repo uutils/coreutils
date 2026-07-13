b1524cab83d9a70c0bf13267905d5080d177de7a
Devel <Denislav08@proton.me>
2026-07-13 21:50:38 +0300

    Update test_od.rs

--- tests/by-util/test_od.rs
+++ tests/by-util/test_od.rs
@@ -1362,10 +1362,8 @@ fn test_hex_lowercase() {
 }
 
 #[test]
-#[cfg_attr(
-    any(target_os = "wasi", target_os = "windows"),
-    ignore = "Messages are different on windows and wasi"
-)]
+#[cfg(not(target_os = "windows"))]
+#[cfg_attr(wasi_runner, ignore)]
 fn test_is_a_directory() {
     let scene = TestScenario::new(util_name!());
     let fixtures = &scene.fixtures;
