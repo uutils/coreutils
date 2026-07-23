a1f3f816b42a908d5c3a127698ac9c4353d2e454
Eyüp Can Akman <eyupcanakman@gmail.com>
2026-07-23 12:23:52 +0300

    printf, seq: add the new words to the spell-checker comments
    unpadded and bigdecimal only appear in the new comments.

--- tests/by-util/test_printf.rs
+++ tests/by-util/test_printf.rs
@@ -3,7 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-// spell-checker:ignore fffffffffffffffc
+// spell-checker:ignore fffffffffffffffc bigdecimal unpadded
 use uutests::new_ucmd;
 
 #[test]
