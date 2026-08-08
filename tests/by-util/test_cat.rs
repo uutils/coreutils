dc64702a8097fe99a3f57e3495f7b1e27df5f078
Anthony DePasquale <anthony@depasquale.org>
2026-08-08 11:43:42 +0200

    tests/cat: preserve Unix test coverage

--- tests/by-util/test_cat.rs
+++ tests/by-util/test_cat.rs
@@ -788,7 +788,7 @@ fn test_cat_non_utf8_paths() {
 }
 
 #[test]
-#[cfg(target_os = "linux")]
+#[cfg(unix)]
 #[cfg_attr(wasi_runner, ignore = "WASI: cannot detect unsafe overwrite")]
 fn test_appending_same_input_output() {
     let (at, mut ucmd) = at_and_ucmd!();
