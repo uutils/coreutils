328cf8ba4a4f3e83d182d15cd35c79c95354b3b7
Markus Tervooren <mail@markustervooren.de>
2026-08-28 11:28:45 +0000

    change to saturating_mul

--- src/uu/dd/src/dd.rs
+++ src/uu/dd/src/dd.rs
@@ -160,7 +160,7 @@ impl Num {
 
     fn to_bytes(self, block_size: u64) -> u64 {
         match self {
-            Self::Blocks(n) => n.checked_mul(block_size).unwrap_or(u64::MAX),
+            Self::Blocks(n) => n.saturating_mul(block_size),
             Self::Bytes(n) => n,
         }
     }
