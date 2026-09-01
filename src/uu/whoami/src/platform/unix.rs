1554443636e1d9739492dd5016ba71f018e8b29d
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-01 10:50:14 +0100

    uucore: remove process functions

--- src/uu/whoami/src/platform/unix.rs
+++ src/uu/whoami/src/platform/unix.rs
@@ -6,10 +6,10 @@
 use std::ffi::OsString;
 use std::io;
 
+use rustix::process::geteuid;
 use uucore::entries::uid2usr;
-use uucore::process::geteuid;
 
 pub fn get_username() -> io::Result<OsString> {
     // uid2usr should arguably return an OsString but currently doesn't
-    uid2usr(geteuid()).map(Into::into)
+    uid2usr(geteuid().as_raw()).map(Into::into)
 }
