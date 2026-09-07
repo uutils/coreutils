d6efedd7b1ab5b8b852e4600893165eff89a30d8
oech3 <79379754+oech3@users.noreply.github.com>
2026-09-07 17:20:30 +0900

    hostid: remove unuseful lock()

--- src/uu/hostid/src/hostid.rs
+++ src/uu/hostid/src/hostid.rs
@@ -28,7 +28,7 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
     let mask = 0xffff_ffff;
 
     result &= mask;
-    writeln!(stdout().lock(), "{result:0>8x}")?;
+    writeln!(stdout(), "{result:0>8x}")?;
     Ok(())
 }
 
