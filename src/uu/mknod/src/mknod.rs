d2df53262b2338618e584020385b503036f83c98
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-09 15:07:21 +0100

    mknod: use dev_t type to avoid casting
    Use `nix::sys::stat::dev_t` type for the device ID to avoid unnecessary `as` conversions.

--- src/uu/mknod/src/mknod.rs
+++ src/uu/mknod/src/mknod.rs
@@ -7,7 +7,7 @@
 
 use clap::{Arg, ArgAction, Command, value_parser};
 use nix::libc::{S_IRGRP, S_IROTH, S_IRUSR, S_IWGRP, S_IWOTH, S_IWUSR, mode_t};
-use nix::sys::stat::{Mode, SFlag, mknod as nix_mknod, umask as nix_umask};
+use nix::sys::stat::{Mode, SFlag, dev_t, mknod as nix_mknod, umask as nix_umask};
 use std::ffi::OsString;
 use std::io::{self, Write as _};
 
@@ -56,7 +56,7 @@ struct Config {
     /// when false, the exact mode bits will be set
     use_umask: bool,
 
-    dev: u64,
+    dev: dev_t,
 
     /// Set security context (SELinux/SMACK).
     #[cfg(any(
@@ -104,7 +104,7 @@ fn mknod(file_name: &str, config: Config) -> i32 {
         file_name,
         config.file_type.as_sflag(),
         config.mode,
-        config.dev as _,
+        config.dev,
     )
     .err();
     let errno = if mknod_err.is_some() { -1 } else { 0 };
@@ -204,7 +204,7 @@ pub fn uumain(args: impl uucore::Args) -> UResult<()> {
                 translate!("mknod-error-fifo-no-major-minor"),
             ));
         }
-        (_, Some(&major), Some(&minor)) => makedev(major as _, minor as _) as u64,
+        (_, Some(&major), Some(&minor)) => makedev(major as _, minor as _),
         _ => {
             return Err(UUsageError::new(
                 1,
