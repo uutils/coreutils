09c7d9ec0728806d39100857c00983c7fefdd1e7
Zhiwei Liang <zhiwei.liang@zliang.me>
2026-09-14 02:43:18 -0400

    uptime: accept hour or minute in pretty-print tests
    Signed-off-by: Zhiwei Liang <zhiwei.liang@zliang.me>

--- tests/by-util/test_uptime.rs
+++ tests/by-util/test_uptime.rs
@@ -336,7 +336,7 @@ fn test_uptime_pretty_print() {
         .arg("-p")
         .succeeds()
         .stdout_contains("up")
-        .stdout_contains("minute");
+        .stdout_matches(&Regex::new(r"hour|minute").unwrap());
 }
 
 /// Test uptime reliability on macOS with sysctl kern.boottime fallback.
