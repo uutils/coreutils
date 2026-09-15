987097e27f961ab35c479ae08194935a524f1e57
Zhiwei Liang <zhiwei.liang@zliang.me>
2026-09-14 23:43:41 -0400

    ls: use name sort for -u/-c in long format
    Signed-off-by: Zhiwei Liang <zhiwei.liang@zliang.me>

--- tests/by-util/test_vdir.rs
+++ tests/by-util/test_vdir.rs
@@ -114,6 +114,30 @@ fn test_literal_quoting_on_terminal() {
 }
 
 #[test]
+fn test_time_selection_preserves_name_sort() {
+    let scene = TestScenario::new(util_name!());
+    for (name, seconds) in [("a", 978_307_200), ("b", 1_009_843_200)] {
+        scene.fixtures.touch(name);
+        filetime::set_file_atime(
+            scene.fixtures.plus(name),
+            filetime::FileTime::from_unix_time(seconds, 0),
+        )
+        .unwrap();
+    }
+    let name_order = Regex::new(r"(?s)2001 a\n.*2002 b\n").unwrap();
+    let zero_output = Regex::new(r"\Ab\x00a\x00\z").unwrap();
+    for (time, zero) in itertools::iproduct!(["-u", "--time=atime"], [false, true]) {
+        scene
+            .ucmd()
+            .args(&[time, "--time-style=+%Y", "a", "b"])
+            .args(if zero { &["--zero"] } else { &[] })
+            .succeeds()
+            .no_stderr()
+            .stdout_matches(if zero { &zero_output } else { &name_order });
+    }
+}
+
+#[test]
 fn test_column_output() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
