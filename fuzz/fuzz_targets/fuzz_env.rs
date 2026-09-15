858885c68f49e31d09778b2440d3c57d7c8a6ba8
oech3 <79379754+oech3@users.noreply.github.com>
2026-09-16 01:31:31 +0900

    fix cspell

--- fuzz/fuzz_targets/fuzz_env.rs
+++ fuzz/fuzz_targets/fuzz_env.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore chdir
+// spell-checker:ignore chdir putenv
 
 #![no_main]
 use libfuzzer_sys::fuzz_target;
