fabe6bc4638174a4aa40d6f0737386b41ca4e424
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-18 17:56:48 +0100

    build(rm): gate by target support
    The package now compiles successfully:
    
    ```sh
    RUSTC_BOOTSTRAP=1 cargo clippy -Zbuild-std=std,panic_abort --quiet --package=uu_rm --all-features --lib --target=powerpc64-ibm-aix --target=x86_64-linux-android --target=x86_64-pc-cygwin --target=aarch64-apple-darwin --target=x86_64-unknown-freebsd --target=x86_64-unknown-haiku --target=x86_64-unknown-hermit --target=x86_64-unknown-hurd-gnu --target=aarch64-unknown-illumos --target=x86_64-unknown-linux-gnu --target=i686-unknown-netbsd --target=x86_64-unknown-openbsd --target=x86_64-pc-solaris --target=wasm32-wasip1 --target=x86_64-pc-windows-gnu
    ```

--- src/uu/rm/benches/rm_bench.rs
+++ src/uu/rm/benches/rm_bench.rs
@@ -3,6 +3,8 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+#![cfg(any(unix, windows, target_os = "wasi"))]
+
 use divan::{Bencher, black_box};
 use tempfile::TempDir;
 use uu_rm::uumain;
