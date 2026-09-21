f164adaf767a1b310c0da04bbca5232b82d3c409
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-22 00:19:27 +0100

    style: reorder attributes and documentation comments

--- src/uucore/src/lib/features/fs.rs
+++ src/uucore/src/lib/features/fs.rs
@@ -430,8 +430,8 @@ pub fn canonicalize<P: AsRef<Path>>(
     Ok(result)
 }
 
-#[cfg(not(unix))]
 /// Display the permissions of a file
+#[cfg(not(unix))]
 pub fn display_permissions(metadata: &fs::Metadata, display_file_type: bool) -> String {
     let write = if metadata.permissions().readonly() {
         '-'
@@ -454,8 +454,8 @@ pub fn display_permissions(metadata: &fs::Metadata, display_file_type: bool) -> 
     }
 }
 
-#[cfg(unix)]
 /// Display the permissions of a file
+#[cfg(unix)]
 pub fn display_permissions(metadata: &fs::Metadata, display_file_type: bool) -> String {
     display_permissions_unix(metadata.mode(), display_file_type)
 }
@@ -522,10 +522,9 @@ fn get_file_display(mode: u32) -> char {
     }
 }
 
-// The logic below is more readable written this way.
-#[allow(clippy::if_not_else)]
-#[allow(clippy::cognitive_complexity)]
 /// Display the unix permissions of a file
+#[allow(clippy::if_not_else, reason = "more readable written this way")]
+#[allow(clippy::cognitive_complexity)]
 pub fn display_permissions_unix(mode: u32, display_file_type: bool) -> String {
     use mode::{
         S_IRGRP, S_IROTH, S_IRUSR, S_ISGID, S_ISUID, S_ISVTX, S_IWGRP, S_IWOTH, S_IWUSR, S_IXGRP,
